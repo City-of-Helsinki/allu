401211c8ff37ab6438200f750559e506cebd737f
perttu_taskinen <perttu.taskinen@vincit.fi>
2016-05-11 09:34:07 +0300

    Fixed tslint issues

--- frontend/src/service/map.service.ts
+++ frontend/src/service/map.service.ts
@@ -9,7 +9,7 @@ export class MapService {
     constructor() {
         this.baseMaps = {
             OpenStreetMap: new L.TileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            	attribution: `
+              attribution: `
                 &copy;
                 <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,
                 Tiles courtesy of
@@ -18,14 +18,14 @@ export class MapService {
               `
             }),
             Esri: new L.TileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-            	attribution: `
+              attribution: `
               Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap,
               iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey,
               Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community
               `
             }),
             CartoDB: new L.TileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-            	attribution: `
+              attribution: `
                 &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>
                 &copy; <a href="http://cartodb.com/attributions">CartoDB</a>
               `
