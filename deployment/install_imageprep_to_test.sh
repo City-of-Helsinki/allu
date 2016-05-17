aa77bb81876fa7c14edd533157123737105ea7a0
perttu_taskinen <perttu.taskinen@vincit.fi>
2016-05-17 10:16:51 +0300

    Adding --limit parameter support for imageprep deployment to test

--- deployment/install_imageprep_to_test.sh
+++ deployment/install_imageprep_to_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ansible-playbook -i test.inventory imageprep.yml --private-key $HOME/allu_keys/allu_id_rsa --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub"
+if [ -z $1 ]; then
+  echo "Usage: $0 <limit_hosts>"
+  echo "For example: $0 dbservers"
+else
+  ansible-playbook -i test.inventory imageprep.yml --limit $1 --private-key $HOME/allu_keys/allu_id_rsa --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub"
+fi
