abac36566aba926517b1bf64d039e557e9e706bb
perttu_taskinen <perttu.taskinen@vincit.fi>
2016-05-18 14:38:14 +0300

    Changed backend services to be restarted instead of started and added backend playbook as part of backend deployment

--- deployment/install_backend_to_test.sh
+++ deployment/install_backend_to_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh -e install_imageprep_to_test.sh dbservers
 ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
