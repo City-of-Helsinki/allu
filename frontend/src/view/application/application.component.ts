12043a10790feac70afef40ec3bfcf1a4d8bb9fa
Sadi Hossain <sadi.hossain@vincit.fi>
2016-05-23 09:47:09 +0300

    Fix lint issues

--- frontend/src/view/application/application.component.ts
+++ frontend/src/view/application/application.component.ts
@@ -23,50 +23,50 @@ import {WorkqueueComponent} from '../../component/workqueue/workqueue.component'
 })
 
 export class ApplicationComponent {
-  public application: any
+  public application: any;
 
   constructor() {
     this.application = {
-      "name": "Tapahtuma 1",
-      "type": "Ulkoilmatapahtuma",
-      "status": "Vireillä",
-      "information": "Suspendisse quis arcu dolor. Donec fringilla nunc mollis aliquet mollis. Donec commodo tempus erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis convallis sem tincidunt enim mattis eleifend eget eu ante",
-      "applicant": {
-        "companyName": 'Vincit Helsinki Oy',
-        "businessID": 'Y-VINCIT',
-        "name": 'Sadi Hossain',
-        "social": 'XXXXXX',
-        "address": 'Mikonkatu 15 A',
-        "postalCode": '00100',
-        "city": 'Helsinki',
-        "phone": '0505291920',
-        "email": 'sadi.hossain@vincit.fi'
+      'name': 'Tapahtuma 1',
+      'type': 'Ulkoilmatapahtuma',
+      'status': 'Vireillä',
+      'information': 'Suspendisse quis arcu dolor. Donec fringilla nunc mollis.',
+      'applicant': {
+        'companyName': 'Vincit Helsinki Oy',
+        'businessID': 'Y-VINCIT',
+        'name': 'Sadi Hossain',
+        'social': 'XXXXXX',
+        'address': 'Mikonkatu 15 A',
+        'postalCode': '00100',
+        'city': 'Helsinki',
+        'phone': '0505291920',
+        'email': 'sadi.hossain@vincit.fi'
       },
-      "contact": {
-        "name": 'Jan Nikander',
-        "social": 'XXYYZZ',
-        "address": 'Mikonkatu 15 A',
-        "postalCode": '00100',
-        "city": 'Helsinki',
-        "phone": '0505291920',
-        "email": 'jan.nikander@vincit.fi'
+      'contact': {
+        'name': 'Jan Nikander',
+        'social': 'XXYYZZ',
+        'address': 'Mikonkatu 15 A',
+        'postalCode': '00100',
+        'city': 'Helsinki',
+        'phone': '0505291920',
+        'email': 'jan.nikander@vincit.fi'
       },
-      "customer": {
-        "id": null,
-        "type": "Henkilöasiakas",
-        "address": "Jokutie",
-        "postOffice": "HELSINKI",
-        "zipCode": "00100",
-        "district": null,
-        "email": "mail@mail.com"
+      'customer': {
+        'id': undefined,
+        'type': 'Henkilöasiakas',
+        'address': 'Jokutie',
+        'postOffice': 'HELSINKI',
+        'zipCode': '00100',
+        'district': undefined,
+        'email': 'mail@mail.com'
       },
-      "createDate": "2016-05-18T10:24:06.565+03:00",
-      "startDate": null,
-      "project": {
-        "id": null,
-        "name": "Hanke1",
-        "type": "Sähkötyö",
-        "information": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis congue erat. Aenean eget suscipit neque. Quisque et tincidunt dui. Donec dictum tellus lectus, ut lobortis nulla mollis nec. Morbi ante est, tristique eu eros ut, cursus consectetur justo. Donec varius sodales arcu, a posuere velit porta quis. Aliquam erat volutpat. Aliquam bibendum in lectus ac ornare. Aenean lacus massa, maximus et metus eu, rutrum bibendum massa."
+      'createDate': '2016-05-18T10:24:06.565+03:00',
+      'startDate': undefined,
+      'project': {
+        'id': undefined,
+        'name': 'Hanke1',
+        'type': 'Sähkötyö',
+        'information': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       }
     };
   }
