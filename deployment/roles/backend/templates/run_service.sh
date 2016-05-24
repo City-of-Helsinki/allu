4249c8579ee5f3722acec83320e41955da26f989
perttu_taskinen <perttu.taskinen@vincit.fi>
2016-05-24 12:57:53 +0300

    added support for externalized Spring-Boot application configuration

--- deployment/roles/backend/templates/run_service.sh
+++ deployment/roles/backend/templates/run_service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/allu/{{ item }}/{{ item }}.jar &
+java -jar /home/allu/{{ item }}/{{ item }}.jar --spring.config.location=file:/home/allu/{{ item }}/{{ item }}.properties &
