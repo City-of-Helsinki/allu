6c8d8be86d97ae30db29b613fa78fc125f2b07fb
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2016-09-05 15:32:30 +0300

    Import es6-shim for IE11 compatibility

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -2,7 +2,7 @@
 // (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
 
 // import 'ie-shim'; // Internet Explorer
-// import 'es6-shim';
+import 'es6-shim';
 // import 'es6-promise';
 // import 'es7-reflect-metadata';
 
