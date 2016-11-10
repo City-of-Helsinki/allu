02afa0d8787471a1115049bb2782d933f80f18b7
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2016-11-10 16:25:38 +0200

    Fixed decision page modal

--- frontend/src/feature/decision/decision-modal.component.ts
+++ frontend/src/feature/decision/decision-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {ApplicationStatusChange, ApplicationStatus, translateStatus} from '../../model/application/application-status-change';
+import {MaterializeAction} from 'angular2-materialize';
 
 @Component({
   selector: 'decision-modal',
@@ -11,10 +12,18 @@ export class DecisionModalComponent {
   @Input() header: string;
   @Input() confirmText: string;
   @Output() statusChange = new EventEmitter<ApplicationStatusChange>();
+  modalActions = new EventEmitter<string|MaterializeAction>();
 
   public comment: string;
 
   confirm() {
     this.statusChange.emit(ApplicationStatusChange.withComment(undefined, ApplicationStatus[this.status], this.comment));
+  }
+
+  openModal() {
+    this.modalActions.emit({action: 'modal', params: ['open']});
+  }
+  closeModal() {
+    this.modalActions.emit({action: 'modal', params: ['close']});
   }
 }
