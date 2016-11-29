6fcc0cdd57c859584162bd8893ad938c85577c30
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2016-11-29 16:34:30 +0200

    Styles and colors tweaks - unified background color - align location card - pure white cards - HAKEMUKSET-tab is now old HAKU-tab (create new moved to search page similar to projects) - right align logout in main navbar

--- frontend/src/feature/search/search.module.ts
+++ frontend/src/feature/search/search.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import {SearchComponent} from './search.component';
 import {AlluCommonModule} from '../common/allu-common.module';
+
 
 @NgModule({
   imports: [
     ReactiveFormsModule,
     FormsModule,
-    AlluCommonModule
+    AlluCommonModule,
+    RouterModule.forChild([])
   ],
   declarations: [
     SearchComponent
