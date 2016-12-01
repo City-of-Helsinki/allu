9b0d92a28013991a7444f137c01784118dc9bbff
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2016-12-01 16:18:25 +0200

    Frontpage layout update - simplified searchbar as default on frontpage - sidebar with basic search fields

--- frontend/src/feature/mapsearch/mapsearch.module.ts
+++ frontend/src/feature/mapsearch/mapsearch.module.ts
@@ -1,16 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MdToolbarModule, MdCardModule} from '@angular/material';
+import {MdToolbarModule, MdCardModule, MdSidenavModule} from '@angular/material';
 import {MapModule} from '../map/map.module';
 import {MapSearchComponent} from './mapsearch.component';
 import {SearchBarModule} from '../searchbar/searchbar.module';
 import {ApplicationListComponent} from '../applicationlist/application-list.component';
+import {AlluCommonModule} from '../common/allu-common.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MdToolbarModule,
     MdCardModule,
+    MdSidenavModule,
+    AlluCommonModule,
     MapModule,
     SearchBarModule
   ],
