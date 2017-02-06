f17bbe18ad6903e67a23cc5a60e83a4ae381cd07
Perttu Taskinen <perttu.taskinen@vincit.fi>
2017-02-06 12:59:48 +0200

    Temporary traffic arrangements backend support

---
+++ backend/allu-ui-domain/src/main/java/fi/hel/allu/ui/domain/TrafficArrangementJson.java
@@ -0,0 +1,94 @@
+package fi.hel.allu.ui.domain;
+
+import fi.hel.allu.common.types.ApplicationType;
+
+import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
+
+/**
+ * Traffic arrangement (väliaikainen liikennejärjestely) specific data.
+ */
+public class TrafficArrangementJson extends ApplicationExtensionJson {
+  @NotNull(message = "{application.trafficarrangements.contractor}")
+  private ApplicantJson contractor;
+  @NotNull(message = "{application.trafficarrangements.responsiblePerson}")
+  private ContactJson responsiblePerson;
+  private Boolean pksCard;
+  private ZonedDateTime workFinished;
+  private String additionalInfo;
+  private String trafficArrangements;
+
+  @Override
+  public ApplicationType getApplicationType() {
+    return ApplicationType.TEMPORARY_TRAFFIC_ARRANGEMENTS;
+  }
+
+  /**
+   * In Finnish: Työn suorittaja (yritys yleensä).
+   * The company that does the actual work instead of the party that acts as customer.
+   */
+  public ApplicantJson getContractor() {
+    return contractor;
+  }
+
+  public void setContractor(ApplicantJson contractor) {
+    this.contractor = contractor;
+  }
+
+  /**
+   * In Finnish: vastuuhenkilö
+   * The person responsible of the actual work.
+   */
+  public ContactJson getResponsiblePerson() {
+    return responsiblePerson;
+  }
+
+  public void setResponsiblePerson(ContactJson responsiblePerson) {
+    this.responsiblePerson = responsiblePerson;
+  }
+
+  /**
+   * In Finnish: PKS-kortti (pääkaupunkiseudun katutöihin liittyvät koulutus).
+   * True, if the contractor doing the actual work has PKS-card.
+   */
+  public Boolean getPksCard() {
+    return pksCard;
+  }
+
+  public void setPksCard(Boolean pksCard) {
+    this.pksCard = pksCard;
+  }
+
+  /**
+   * In Finnish: Työ valmis.
+   */
+  public ZonedDateTime getWorkFinished() {
+    return workFinished;
+  }
+
+  public void setWorkFinished(ZonedDateTime workFinished) {
+    this.workFinished = workFinished;
+  }
+
+  /**
+   * In Finnish: lisätiedot.
+   */
+  public String getAdditionalInfo() {
+    return additionalInfo;
+  }
+
+  public void setAdditionalInfo(String additionalInfo) {
+    this.additionalInfo = additionalInfo;
+  }
+
+  /**
+   * In Finnish: suoritettavat liikennejärjestelytyöt.
+   */
+  public String getTrafficArrangements() {
+    return trafficArrangements;
+  }
+
+  public void setTrafficArrangements(String trafficArrangements) {
+    this.trafficArrangements = trafficArrangements;
+  }
+}
---
+++ backend/model-domain/src/main/java/fi/hel/allu/model/domain/TrafficArrangement.java
@@ -0,0 +1,91 @@
+package fi.hel.allu.model.domain;
+
+import fi.hel.allu.common.types.ApplicationType;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Traffic arrangement (väliaikainen liikennejärjestely) specific data.
+ */
+public class TrafficArrangement extends ApplicationExtension {
+  private Applicant contractor;
+  private Contact responsiblePerson;
+  private Boolean pksCard;
+  private ZonedDateTime workFinished;
+  private String additionalInfo;
+  private String trafficArrangements;
+
+  @Override
+  public ApplicationType getApplicationType() {
+    return ApplicationType.TEMPORARY_TRAFFIC_ARRANGEMENTS;
+  }
+
+  /**
+   * In Finnish: Työn suorittaja (yritys yleensä).
+   * The company that does the actual work instead of the party that acts as customer.
+   */
+  public Applicant getContractor() {
+    return contractor;
+  }
+
+  public void setContractor(Applicant contractor) {
+    this.contractor = contractor;
+  }
+
+  /**
+   * In Finnish: vastuuhenkilö
+   * The person responsible of the actual work.
+   */
+  public Contact getResponsiblePerson() {
+    return responsiblePerson;
+  }
+
+  public void setResponsiblePerson(Contact responsiblePerson) {
+    this.responsiblePerson = responsiblePerson;
+  }
+
+  /**
+   * In Finnish: PKS-kortti (pääkaupunkiseudun katutöihin liittyvät koulutus).
+   * True, if the contractor doing the actual work has PKS-card.
+   */
+  public Boolean getPksCard() {
+    return pksCard;
+  }
+
+  public void setPksCard(Boolean pksCard) {
+    this.pksCard = pksCard;
+  }
+
+  /**
+   * In Finnish: Työ valmis.
+   */
+  public ZonedDateTime getWorkFinished() {
+    return workFinished;
+  }
+
+  public void setWorkFinished(ZonedDateTime workFinished) {
+    this.workFinished = workFinished;
+  }
+
+  /**
+   * In Finnish: lisätiedot.
+   */
+  public String getAdditionalInfo() {
+    return additionalInfo;
+  }
+
+  public void setAdditionalInfo(String additionalInfo) {
+    this.additionalInfo = additionalInfo;
+  }
+
+  /**
+   * In Finnish: suoritettavat liikennejärjestelytyöt.
+   */
+  public String getTrafficArrangements() {
+    return trafficArrangements;
+  }
+
+  public void setTrafficArrangements(String trafficArrangements) {
+    this.trafficArrangements = trafficArrangements;
+  }
+}
