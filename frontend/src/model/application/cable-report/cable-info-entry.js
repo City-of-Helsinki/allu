8af1ce03f0b1047fa43fed34f4085b4c14b97b64
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2017-03-02 09:24:05 +0200

    Removed accidentally added js-files Added those files to .gitignore

--- frontend/src/model/application/cable-report/cable-info-entry.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CableInfoEntry = (function () {
-    function CableInfoEntry(type, additionalInfo) {
-        this.type = type;
-        this.additionalInfo = additionalInfo;
-    }
-    return CableInfoEntry;
-}());
-exports.CableInfoEntry = CableInfoEntry;
