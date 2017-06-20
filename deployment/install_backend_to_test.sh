58e9172abc7ccbe0ed307691089033b6e33d7c2e
Perttu Taskinen <perttu.taskinen@vincit.fi>
2017-06-20 10:12:59 +0000

    Merged in perttu_staging_deployment (pull request #562)
    Deployment to staging environment
    
    Approved-by: Eero Kirjavainen <eero.kirjavainen@vincit.fi>

--- deployment/install_backend_to_test.sh
+++ deployment/install_backend_to_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh -e install_imageprep_to_test.sh dbservers
 ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa search.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
---
+++ deployment/install_imageprep_to_staging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo "Usage: $0 <limit_hosts>"
+  echo "For example: $0 dbservers"
+else
+  ansible-playbook -i staging.inventory imageprep.yml --limit $1 --private-key $HOME/allu_keys/allu_id_rsa --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub"
+fi
