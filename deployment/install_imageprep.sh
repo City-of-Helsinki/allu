5b75086cc150434ee62a81d98dbc89b4b4f44182
Perttu Taskinen <perttu.taskinen@vincit.fi>
2017-06-26 06:29:12 +0000

    Merged in perttu_staging_with_fixed_user_ids (pull request #563)
    Perttu staging with fixed user ids
    
    * Staging deployment with fixed user and group ids
    
    * Changed deployment to use fixed uid/gid to support staging environment
    
    Approved-by: Eero Kirjavainen <eero.kirjavainen@vincit.fi>

--- deployment/install_imageprep.sh
+++ deployment/install_imageprep.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 if [ -z $1 ]; then
-  echo "Usage: $0 <allu_password> <target_host>"
-  echo "For example: $0 allu_secret_password 10.176.127.67"
+  echo "Usage: $0 <remote_user> <allu_password> <target_host>"
+  echo "For example: $0 root allu_secret_password 10.176.127.67"
+  echo "Use root as remote user for test env, others vincit"
 else
   # NOTE that password is read from command line as group_vars are not available for imageprep.yml,
   # because it is not specific to any host (not possible to define for example test:children in inventory file)
-  ALLU_PASSWORD=$( echo $1 | mkpasswd --method=SHA-512 --stdin )
+  ALLU_PASSWORD=$( echo $2 | mkpasswd --method=SHA-512 --stdin )
   # note that the $2, is intentional. If only $2 would be used, Ansible would not recognise the parameter correctly
-  ansible-playbook -i $2, imageprep.yml --private-key $HOME/allu_keys/allu_id_rsa --ask-pass --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub system_password_allu=$ALLU_PASSWORD"
+  ansible-playbook -i $3, imageprep.yml --private-key $HOME/allu_keys/allu_id_rsa --ask-pass --ask-become-pass --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub system_password_allu=$ALLU_PASSWORD deployment_user=$1"
 fi
