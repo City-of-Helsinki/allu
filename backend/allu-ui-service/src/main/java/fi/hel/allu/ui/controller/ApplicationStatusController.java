8d530f87a73f39fc6ca9679b87e27097eafc2787
Kimmo Surakka <kimmo.surakka@vincit.fi>
2017-08-15 10:38:56 +0300

    Generate final decision PDF only when moving to DECISION
    * Also changed the /applications/{id}/decision API to return
      a preview PDF if final decision doesn't exist, to allow simpler
      logic in the UI
    * Removed DecisionHub.fetchByStatus from UI

--- backend/allu-ui-service/src/main/java/fi/hel/allu/ui/controller/ApplicationStatusController.java
+++ backend/allu-ui-service/src/main/java/fi/hel/allu/ui/controller/ApplicationStatusController.java
@@ -3,11 +3,11 @@ package fi.hel.allu.ui.controller;
 import fi.hel.allu.common.domain.types.StatusType;
 import fi.hel.allu.servicecore.domain.ApplicationJson;
 import fi.hel.allu.servicecore.domain.StatusCommentJson;
-import fi.hel.allu.ui.security.DecisionSecurityService;
 import fi.hel.allu.servicecore.service.ApplicationServiceComposer;
 import fi.hel.allu.servicecore.service.CommentService;
-
 import fi.hel.allu.servicecore.service.DecisionService;
+import fi.hel.allu.ui.security.DecisionSecurityService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -50,17 +50,17 @@ public class ApplicationStatusController {
     @RequestMapping(value = "/{id}/status/decisionmaking", method = RequestMethod.PUT)
     @PreAuthorize("hasAnyRole('ROLE_PROCESS_APPLICATION')")
     public ResponseEntity<ApplicationJson> changeStatusToDecisionMaking(
-            @PathVariable int id, @RequestBody(required = false) StatusCommentJson comment) throws IOException {
-        ApplicationJson applicationJson = applicationServiceComposer.findApplicationById(id);
-        decisionService.generateDecision(id, applicationJson);
+            @PathVariable int id, @RequestBody(required = false) StatusCommentJson comment) {
         Optional.ofNullable(comment).ifPresent(c -> commentService.addDecisionProposalComment(id, c));
         return new ResponseEntity<>(applicationServiceComposer.changeStatus(id, StatusType.DECISIONMAKING), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}/status/decision", method = RequestMethod.PUT)
     @PreAuthorize("@decisionSecurityService.canMakeDecision(#id)")
-    public ResponseEntity<ApplicationJson> changeStatusToDecision(@PathVariable int id) {
-        return new ResponseEntity<>(applicationServiceComposer.changeStatus(id, StatusType.DECISION), HttpStatus.OK);
+    public ResponseEntity<ApplicationJson> changeStatusToDecision(@PathVariable int id) throws IOException {
+    ApplicationJson applicationJson = applicationServiceComposer.changeStatus(id, StatusType.DECISION);
+        decisionService.generateDecision(id, applicationJson);
+    return new ResponseEntity<>(applicationJson, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}/status/rejected", method = RequestMethod.PUT)
