721137c7c4aabb475127204bfb01cf9527018f87
Kimmo Surakka <kimmo.surakka@vincit.fi>
2017-08-31 15:39:34 +0300

    external api: added application update

--- backend/external-test/spec/application.spec.js
+++ backend/external-test/spec/application.spec.js
@@ -7,6 +7,10 @@ describe('Application', () => {
 
   let customerCreated;
   let contactCreated;
+  let applicationExtNew;
+  let extension;
+  let customerWithContactsExt;
+  let locationExt;
 
   beforeAll((done) => {
 
@@ -36,6 +40,140 @@ describe('Application', () => {
       'phone': '010 1010',
       'registryKey': '1234-123' 
     };
+    extension = {
+      'applicationType': 'EXCAVATION_ANNOUNCEMENT',
+      'terms': 'Some terms for application',
+      'pksCard': true,
+      'constructionWork': false,
+      'maintenanceWork': false,
+      'emergencyWork': null,
+      'propertyConnectivity': false,
+      'winterTimeOperation': null,
+      'workFinished': null,
+      'unauthorizedWorkStartTime': null,
+      'unauthorizedWorkEndTime': null,
+      'guaranteeEndTime': null,
+      'cableReportId': null,
+      'additionalInfo': 'Some additional info on extension',
+      'trafficArrangements': 'No problem with traffic arrangements',
+      'trafficArrangementImpedimentType': 'NO_IMPEDIMENT'
+    };
+    customerWithContactsExt = {
+      'customer': null,
+      'roleType': 'APPLICANT',
+      'contacts': null
+    };
+    let locationGeometry = {
+      "type": "GeometryCollection",
+      "crs": {
+        "properties": {
+          "name": "EPSG:3879"
+        },
+        "type": "name"
+      },
+      "bbox": null,
+      "geometries": [
+        {
+          "type": "Polygon",
+          "crs": null,
+          "bbox": [
+            2.549589275E7,
+            6674232.999998959,
+            2.549606025E7,
+            6674407.499998961
+          ],
+          "coordinates": [
+            [
+              [
+                2.549589975E7,
+                6674390.49999896
+              ],
+              [
+                2.549592575E7,
+                6674407.499998961
+              ],
+              [
+                2.549596925E7,
+                6674348.99999896
+              ],
+              [
+                2.549603675E7,
+                6674393.499998959
+              ],
+              [
+                2.549606025E7,
+                6674357.999998961
+              ],
+              [
+                2.549599975E7,
+                6674320.99999896
+              ],
+              [
+                2.549602225E7,
+                6674239.999998959
+              ],
+              [
+                2.549597825E7,
+                6674232.999998959
+              ],
+              [
+                2.549596175E7,
+                6674303.499998958
+              ],
+              [
+                2.549590825E7,
+                6674251.999998959
+              ],
+              [
+                2.549589275E7,
+                6674283.999998958
+              ],
+              [
+                2.549594775E7,
+                6674327.999998961
+              ],
+              [
+                2.549589975E7,
+                6674390.49999896
+              ]
+            ]
+          ]
+        }
+      ]
+    };
+    locationExt = {
+      'id': null,
+      'locationKey': null,
+      'locationVersion': null,
+      'startTime': '2017-07-17T10:42:48.315Z',
+      'endTime': '2017-08-17T10:42:48.315Z',
+      'additionalInfo': null,
+      'geometry': locationGeometry,
+      'area': null,
+      'areaOverride': null,
+      'postalAddress': null,
+      'paymentTariff': null,
+      'paymentTariffOverride': null,
+      'underpass': null
+    };
+    applicationExtNew = {
+      'id': null,
+      'projectId': null,
+      'customersWithContacts': [customerWithContactsExt],
+      'locations': [locationExt],
+      'status': 'PENDING',
+      'type': 'EXCAVATION_ANNOUNCEMENT',
+      'kindsWithSpecifiers': {
+        'STREET_AND_GREEN' : [],
+        'ELECTRICITY' : ['DISTRIBUTION_CABINET'],
+        'CONSTRUCTION' : ['GROUND_ROCK_ANCHOR', 'UNDERGROUND_SPACE', 'DRILL_PILE'] },
+      'applicationTags': null,
+      'name': 'test application',
+      'creationTime': null,
+      'startTime': null,
+      'endTime': null,
+      'extension': extension
+    };
 
     // create customer and related contact for the customer to be used in this spec
     TestUtil.swaggerClient()
@@ -44,155 +182,18 @@ describe('Application', () => {
         .then(customer => {
           customerCreated = customer.obj;
           contactNew.customerId = customerCreated.id;
-          return client.apis.contacts.contactsCreate({ body: contactNew}).then(contact => contactCreated = contact.obj);
+          return client.apis.contacts.contactsCreate({body: contactNew}).then(contact => contactCreated = contact.obj);
         })
     )
+    .then(() => {
+      customerWithContactsExt.customer = customerCreated.id;
+      customerWithContactsExt.contacts = [contactCreated.id];
+    })
     .then(() => done());
+
   });
 
   describe('Create Excavation Announcement', () => {
-    let applicationExtNew;
-    let extension;
-    let customerWithContactsExt;
-    let locationExt;
-
-    beforeEach(() => {
-
-      extension = {
-        'applicationType': 'EXCAVATION_ANNOUNCEMENT',
-        'terms': 'Some terms for application',
-        'pksCard': true,
-        'constructionWork': false,
-        'maintenanceWork': false,
-        'emergencyWork': null,
-        'propertyConnectivity': false,
-        'winterTimeOperation': null,
-        'workFinished': null,
-        'unauthorizedWorkStartTime': null,
-        'unauthorizedWorkEndTime': null,
-        'guaranteeEndTime': null,
-        'cableReportId': null,
-        'additionalInfo': 'Some additional info on extension',
-        'trafficArrangements': 'No problem with traffic arrangements',
-        'trafficArrangementImpedimentType': 'NO_IMPEDIMENT'
-      };
-      customerWithContactsExt = {
-        'customer': customerCreated.id,
-        'roleType': 'APPLICANT',
-        'contacts': [contactCreated.id]
-      };
-      locationGeometry = {
-        "type": "GeometryCollection",
-        "crs": {
-          "properties": {
-            "name": "EPSG:3879"
-          },
-          "type": "name"
-        },
-        "bbox": null,
-        "geometries": [
-          {
-            "type": "Polygon",
-            "crs": null,
-            "bbox": [
-              2.549589275E7,
-              6674232.999998959,
-              2.549606025E7,
-              6674407.499998961
-            ],
-            "coordinates": [
-              [
-                [
-                  2.549589975E7,
-                  6674390.49999896
-                ],
-                [
-                  2.549592575E7,
-                  6674407.499998961
-                ],
-                [
-                  2.549596925E7,
-                  6674348.99999896
-                ],
-                [
-                  2.549603675E7,
-                  6674393.499998959
-                ],
-                [
-                  2.549606025E7,
-                  6674357.999998961
-                ],
-                [
-                  2.549599975E7,
-                  6674320.99999896
-                ],
-                [
-                  2.549602225E7,
-                  6674239.999998959
-                ],
-                [
-                  2.549597825E7,
-                  6674232.999998959
-                ],
-                [
-                  2.549596175E7,
-                  6674303.499998958
-                ],
-                [
-                  2.549590825E7,
-                  6674251.999998959
-                ],
-                [
-                  2.549589275E7,
-                  6674283.999998958
-                ],
-                [
-                  2.549594775E7,
-                  6674327.999998961
-                ],
-                [
-                  2.549589975E7,
-                  6674390.49999896
-                ]
-              ]
-            ]
-          }
-        ]
-      };
-      locationExt = {
-        'id': null,
-        'locationKey': null,
-        'locationVersion': null,
-        'startTime': '2017-07-17T10:42:48.315Z',
-        'endTime': '2017-08-17T10:42:48.315Z',
-        'additionalInfo': null,
-        'geometry': locationGeometry,
-        'area': null,
-        'areaOverride': null,
-        'postalAddress': null,
-        'paymentTariff': null,
-        'paymentTariffOverride': null,
-        'underpass': null
-      };
-      applicationExtNew = {
-        'id': null,
-        'projectId': null,
-        'customersWithContacts': [customerWithContactsExt],
-        'locations': [locationExt],
-        'status': 'PENDING',
-        'type': 'EXCAVATION_ANNOUNCEMENT',
-        'kindsWithSpecifiers': {
-          'STREET_AND_GREEN' : [],
-          'ELECTRICITY' : ['DISTRIBUTION_CABINET'],
-          'CONSTRUCTION' : ['GROUND_ROCK_ANCHOR', 'UNDERGROUND_SPACE', 'DRILL_PILE'] },
-        'applicationTags': null,
-        'name': 'test application',
-        'creationTime': null,
-        'startTime': null,
-        'endTime': null,
-        'extension': extension
-      }
-    });
 
     it('should contain all swagger defined properties in application', (done) => {
       TestUtil.swaggerClient()
@@ -212,6 +213,36 @@ describe('Application', () => {
       TestUtil.swaggerClient()
       .then(client => client.apis.applications.applicationsCreate({body: applicationExtNew}))
       .then(application => expect(ComparisonUtil.deepCompareNonNull('', applicationExtNew, application.obj)).toEqual([]))
+      .then(done)
+      .catch(err => {
+        console.log('Error', err);
+        done.fail(err);
+      });
+    });
+  });
+
+  describe('Update ExcavationAnnouncement', () => {
+    let applicationExtUpdated;
+    let applicationId;
+    beforeEach((done) => {
+      TestUtil.swaggerClient()
+      .then(client => client.apis.applications.applicationsCreate({body: applicationExtNew}))
+      .then(application =>  {
+        applicationExtUpdated = application.obj;
+        applicationExtUpdated.name = 'Updated test application';
+        // TODO: changes into all editable fields
+        applicationId = application.obj.id;
+      })
+      .then(done)
+      .catch(err => {
+        console.log('Error', err);
+        done.fail(err);
+      });
+    });
+    it('should update existing application', (done) => {
+      TestUtil.swaggerClient()
+      .then(client => client.apis.applications.applicationsUpdate({id: applicationId, body: applicationExtUpdated}))
+      .then(application => expect(ComparisonUtil.deepCompareNonNull('', applicationExtUpdated, application.obj)).toEqual([]))
       .then(done)
       .catch(err => {
         console.log('Error', err);
