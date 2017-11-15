7446d216dacb68444090aded3ef9416833118cb0
Kimmo Surakka <kimmo.surakka@vincit.fi>
2017-11-15 17:45:26 +0200

    external-service: service user token
    * POST /token with pre-defined Basic auth token
      (see https://tools.ietf.org/html/rfc6749#section-4.4.2)
    * The received JWT token has role ROLE_SERVICE
    * The pre-defined Basic auth tokein is defined in
      application.properies as service.authkey

--- backend/common-domain/src/main/java/fi/hel/allu/common/domain/types/ExternalRoleType.java
+++ backend/common-domain/src/main/java/fi/hel/allu/common/domain/types/ExternalRoleType.java
@@ -5,12 +5,16 @@ package fi.hel.allu.common.domain.types;
  */
 public enum ExternalRoleType {
   /**
-   * External user belonging to "Allu ecosystem" i.e. is more like part of Allu than actual external user.
+   * System service
+   */
+  ROLE_SERVICE,
+  /**
+   * External user belonging to "Allu ecosystem" i.e. is more like part of Allu
+   * than actual external user.
    */
   ROLE_INTERNAL,
   /**
    * External user who is a trusted partner. Trusted partner is allowed to do many, but not all operations in the external interface.
    */
   ROLE_TRUSTED_PARTNER;
-
 }
