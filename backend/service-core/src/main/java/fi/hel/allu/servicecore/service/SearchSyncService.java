e508539627a01d882f676b40ae14c90be43e40e2
Kimmo Surakka <kimmo.surakka@vincit.fi>
2017-12-19 18:14:15 +0200

    service-core: search database syncing
    Added service to resync search database from model.

---
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/SearchSyncService.java
@@ -0,0 +1,223 @@
+package fi.hel.allu.servicecore.service;
+
+import fi.hel.allu.model.domain.Application;
+import fi.hel.allu.model.domain.Contact;
+import fi.hel.allu.model.domain.Customer;
+import fi.hel.allu.model.domain.Project;
+import fi.hel.allu.search.domain.ApplicationES;
+import fi.hel.allu.search.domain.ContactES;
+import fi.hel.allu.search.domain.CustomerES;
+import fi.hel.allu.search.domain.ProjectES;
+import fi.hel.allu.servicecore.config.ApplicationProperties;
+import fi.hel.allu.servicecore.mapper.ApplicationMapper;
+import fi.hel.allu.servicecore.mapper.ProjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+/**
+ * Service class to manage database syncing from model-service to
+ * search-service.
+ */
+@Service
+public class SearchSyncService {
+  private static final Logger logger = LoggerFactory.getLogger(SearchSyncService.class);
+  private static final int PAGE_SIZE = 100;
+
+  private RestTemplate restTemplate;
+  private ApplicationProperties applicationProperties;
+  private ApplicationMapper applicationMapper;
+  private ProjectMapper projectMapper;
+
+  @Autowired
+  public SearchSyncService(RestTemplate restTemplate, ApplicationProperties applicationProperties,
+      ApplicationMapper applicationMapper, ProjectMapper projectMapper) {
+    this.restTemplate = restTemplate;
+    this.applicationProperties = applicationProperties;
+    this.applicationMapper = applicationMapper;
+    this.projectMapper = projectMapper;
+  }
+
+  /**
+   * Sync application, project, customer, and contact data from model-service to
+   * search-service
+   */
+  public void sync() {
+    logger.debug("Database sync started");
+    startSync();
+    try {
+      syncApplications();
+      syncProjects();
+      syncCustomers();
+      syncContacts();
+      endSync();
+    } catch (SyncFailedException e) {
+      logger.error("Sync failure: " + e.getMessage() + ", canceling sync.");
+      cancelSync();
+      if (e.getCause() != null) {
+        throw (RuntimeException) e.getCause();
+      }
+    }
+  }
+
+  /*
+   * Generic way to talk to server. Executes the given operation and checks that
+   * the result is OK. If there's error, throws a SyncFailedException. Also, if
+   * any exception is thrown, wraps it inside a SyncFailedException.
+   */
+  private <T> T talkToServer(String operationDescription, Supplier<ResponseEntity<T>> restOperation) {
+    ResponseEntity<T> response;
+    try {
+      response = restOperation.get();
+    } catch (RuntimeException e) {
+      throw new SyncFailedException(operationDescription + ": Exception in REST", e);
+    }
+    if (!response.getStatusCode().is2xxSuccessful()) {
+      throw new SyncFailedException(operationDescription + " failed", response.getStatusCode());
+    }
+    return response.getBody();
+  }
+
+  private void startSync() {
+    talkToServer("Start sync",
+        () -> restTemplate.postForEntity(applicationProperties.getStartSearchSyncUrl(), null, Void.class));
+  }
+
+  private void endSync() {
+    talkToServer("End sync",
+        () -> restTemplate.postForEntity(applicationProperties.getCommitSearchSyncUrl(), null, Void.class));
+  }
+
+  private void cancelSync() {
+    // Failure to cancel shouldn't cause SyncFailed, so skip talkToServer.
+    restTemplate.postForEntity(applicationProperties.getCancelSearchSyncUrl(), null, Void.class);
+  }
+
+  /*
+   * Generic way to sync one type of data. Calls fetcher to retrieve pages of
+   * elements from model-service, then maps them to ES with mapper and calls
+   * sender to transmit the results to search-service.
+   */
+  <T, U> void syncData(IntFunction<Page<T>> fetcher, Consumer<List<U>> sender, Function<T, U> mapper) {
+    int page = 0;
+    Page<T> fromModel;
+    do {
+      fromModel = fetcher.apply(page);
+      if (fromModel.hasContent()) {
+        List<U> toSearch = fromModel.getContent().stream().map(mapper).collect(Collectors.toList());
+        sender.accept(toSearch);
+      }
+      page++;
+    } while (!fromModel.isLast());
+  }
+
+  void syncApplications() {
+    syncData(p -> fetchApplications(p), l -> sendApplications(l), a -> mapToES(a));
+  }
+
+  void syncProjects() {
+    syncData(p -> fetchProjects(p), l -> sendProjects(l), p -> mapToES(p));
+  }
+
+  void syncCustomers() {
+    syncData(p -> fetchCustomers(p), l -> sendCustomers(l), c -> mapToES(c));
+  }
+
+  void syncContacts() {
+    syncData(p -> fetchContacts(p), l -> sendContacts(l), c -> mapToES(c));
+  }
+
+  Page<Application> fetchApplications(int pageNum) {
+    ParameterizedTypeReference<Page<Application>> typeref = new ParameterizedTypeReference<Page<Application>>() {
+    };
+    return talkToServer("Fetch applications", () -> restTemplate
+        .exchange(applicationProperties.getAllApplicationsUrl(), HttpMethod.POST, null, typeref, pageNum, PAGE_SIZE));
+  }
+
+  void sendApplications(List<ApplicationES> apps) {
+    talkToServer("Send applications",
+        () -> restTemplate.postForEntity(applicationProperties.getSyncApplicationsUrl(), apps, Void.class));
+  }
+
+  Page<Project> fetchProjects(int pageNum) {
+    ParameterizedTypeReference<Page<Project>> typeref = new ParameterizedTypeReference<Page<Project>>() {
+    };
+    return talkToServer("Fetch projects", () -> restTemplate.exchange(applicationProperties.getAllProjectsUrl(),
+        HttpMethod.POST, null, typeref, pageNum, PAGE_SIZE));
+  }
+
+  void sendProjects(List<ProjectES> projects) {
+    talkToServer("Send projects",
+        () -> restTemplate.postForEntity(applicationProperties.getSyncProjectsUrl(), projects, Void.class));
+  }
+
+  Page<Customer> fetchCustomers(int pageNum) {
+    ParameterizedTypeReference<Page<Customer>> typeref = new ParameterizedTypeReference<Page<Customer>>() {
+    };
+    return talkToServer("Fetch customers", () -> restTemplate.exchange(applicationProperties.getAllCustomersUrl(),
+        HttpMethod.POST, null, typeref, pageNum, PAGE_SIZE));
+  }
+
+  void sendCustomers(List<CustomerES> customers) {
+    talkToServer("Send customers",
+        () -> restTemplate.postForEntity(applicationProperties.getSyncCustomersUrl(), customers, Void.class));
+  }
+
+  Page<Contact> fetchContacts(int pageNum) {
+    ParameterizedTypeReference<Page<Contact>> typeref = new ParameterizedTypeReference<Page<Contact>>() {
+    };
+    return talkToServer("Fetch contacts", () -> restTemplate.exchange(applicationProperties.getAllContactsUrl(),
+        HttpMethod.POST, null, typeref, pageNum, PAGE_SIZE));
+  }
+
+  void sendContacts(List<ContactES> contacts) {
+    talkToServer("Send contacts",
+        () -> restTemplate.postForEntity(applicationProperties.getSyncProjectsUrl(), contacts, Void.class));
+  }
+
+  @SuppressWarnings("serial")
+  private class SyncFailedException extends RuntimeException {
+
+    public SyncFailedException(String message, Throwable cause) {
+      super(message, cause);
+    }
+
+    public SyncFailedException(String message, HttpStatus statusCode) {
+      super(message + "(status=" + statusCode.value() + ", " + statusCode.name() + ")");
+      initCause(null);
+    }
+  }
+
+  ApplicationES mapToES(Application application) {
+    return applicationMapper.createApplicationESModel(applicationMapper.mapApplicationToJson(application));
+  }
+
+  ProjectES mapToES(Project project) {
+    return projectMapper.createProjectESModel(projectMapper.mapProjectToJson(project));
+  }
+
+  CustomerES mapToES(Customer customer) {
+    return new CustomerES(customer.getId(), customer.getName(), customer.getRegistryKey(), customer.getOvt(),
+        customer.getType(), customer.isActive());
+  }
+
+  ContactES mapToES(Contact contact) {
+    return new ContactES(contact.getId(), contact.getName(), contact.isActive());
+  }
+
+}
---
+++ backend/service-core/src/test/java/fi/hel/allu/servicecore/service/SearchSyncServiceSpec.java
@@ -0,0 +1,180 @@
+package fi.hel.allu.servicecore.service;
+
+import com.greghaskins.spectrum.Spectrum;
+
+import fi.hel.allu.model.domain.Application;
+import fi.hel.allu.model.domain.Contact;
+import fi.hel.allu.model.domain.Customer;
+import fi.hel.allu.model.domain.Project;
+import fi.hel.allu.servicecore.config.ApplicationProperties;
+import fi.hel.allu.servicecore.mapper.ApplicationMapper;
+import fi.hel.allu.servicecore.mapper.ProjectMapper;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.greghaskins.spectrum.dsl.specification.Specification.beforeEach;
+import static com.greghaskins.spectrum.dsl.specification.Specification.describe;
+import static com.greghaskins.spectrum.dsl.specification.Specification.it;
+import static org.mockito.Mockito.times;
+
+@SuppressWarnings("unchecked")
+@RunWith(Spectrum.class)
+public class SearchSyncServiceSpec {
+
+  @Mock
+  private RestTemplate restTemplate;
+  @Mock
+  private ApplicationProperties applicationProperties;
+  @Mock
+  private ApplicationMapper applicationMapper;
+  @Mock
+  private ProjectMapper projectMapper;
+
+  private SearchSyncService searchSyncService;
+
+  {
+    describe("Mail sending service", () -> {
+      beforeEach(() -> {
+        MockitoAnnotations.initMocks(this);
+        searchSyncService = new SearchSyncService(restTemplate, applicationProperties, applicationMapper,
+            projectMapper);
+        setupApplicationProperties();
+      });
+      it("Successfully syncs", () -> {
+        setupRestTemplate(2, 2, 2, 2);
+        searchSyncService.sync();
+        Mockito.verify(restTemplate).postForEntity(START_SEARCH_SYNC_URL, null, Void.class);
+        Mockito.verify(restTemplate).postForEntity(COMMIT_SEARCH_SYNC_URL, null, Void.class);
+        Mockito.verify(restTemplate, times(2)).exchange(Mockito.eq(ALL_CUSTOMERS_URL), Mockito.eq(HttpMethod.POST),
+            Mockito.any(), Mockito.any(ParameterizedTypeReference.class), Mockito.anyInt(), Mockito.anyInt());
+      });
+      it("Cancels on failure", () -> {
+        setupRestTemplate(2, 2, 0, 2);
+        searchSyncService.sync();
+        Mockito.verify(restTemplate).postForEntity(START_SEARCH_SYNC_URL, null, Void.class);
+        Mockito.verify(restTemplate).postForEntity(CANCEL_SEARCH_SYNC_URL, null, Void.class);
+      });
+    });
+  }
+
+  private static final String START_SEARCH_SYNC_URL = "startSearch";
+  private static final String COMMIT_SEARCH_SYNC_URL = "commitSearch";
+  private static final String CANCEL_SEARCH_SYNC_URL = "cancelSearch";
+  private static final String ALL_APPLICATIONS_URL = "allApplications";
+  private static final String ALL_PROJECTS_URL = "allProjects";
+  private static final String ALL_CUSTOMERS_URL = "allCustomers";
+  private static final String ALL_CONTACTS_URL = "allContacts";
+  private static final String SYNC_APPLICATIONS_URL = "syncApplications";
+  private static final String SYNC_PROJECTS_URL = "syncProjects";
+  private static final String SYNC_CUSTOMERS_URL = "syncCustomers";
+  private static final String SYNC_CONTACTS_URL = "syncContacts";
+
+  private void setupApplicationProperties() {
+    Mockito.when(applicationProperties.getStartSearchSyncUrl()).thenReturn(START_SEARCH_SYNC_URL);
+    Mockito.when(applicationProperties.getCommitSearchSyncUrl()).thenReturn(COMMIT_SEARCH_SYNC_URL);
+    Mockito.when(applicationProperties.getCancelSearchSyncUrl()).thenReturn(CANCEL_SEARCH_SYNC_URL);
+    Mockito.when(applicationProperties.getAllApplicationsUrl()).thenReturn(ALL_APPLICATIONS_URL);
+    Mockito.when(applicationProperties.getAllProjectsUrl()).thenReturn(ALL_PROJECTS_URL);
+    Mockito.when(applicationProperties.getAllCustomersUrl()).thenReturn(ALL_CUSTOMERS_URL);
+    Mockito.when(applicationProperties.getAllContactsUrl()).thenReturn(ALL_CONTACTS_URL);
+    Mockito.when(applicationProperties.getSyncApplicationsUrl()).thenReturn(SYNC_APPLICATIONS_URL);
+    Mockito.when(applicationProperties.getSyncProjectsUrl()).thenReturn(SYNC_PROJECTS_URL);
+    Mockito.when(applicationProperties.getSyncCustomersUrl()).thenReturn(SYNC_CUSTOMERS_URL);
+    Mockito.when(applicationProperties.getSyncContactsUrl()).thenReturn(SYNC_CONTACTS_URL);
+  }
+
+
+  private void setupRestTemplate(int numApplicationPages, int numProjectPages, int numCustomerPages,
+      int numContactPages) {
+    Mockito.when(restTemplate.postForEntity(START_SEARCH_SYNC_URL, null, Void.class))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(COMMIT_SEARCH_SYNC_URL, null, Void.class))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(CANCEL_SEARCH_SYNC_URL, null, Void.class))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(Mockito.eq(SYNC_APPLICATIONS_URL), Mockito.any(), Mockito.eq(Void.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(Mockito.eq(SYNC_PROJECTS_URL), Mockito.any(), Mockito.eq(Void.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(Mockito.eq(SYNC_CUSTOMERS_URL), Mockito.any(), Mockito.eq(Void.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    Mockito.when(restTemplate.postForEntity(Mockito.eq(SYNC_CONTACTS_URL), Mockito.any(), Mockito.eq(Void.class)))
+        .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+    // Setup mocking for data fetchers
+    Mockito
+        .when(restTemplate.exchange(Mockito.eq(ALL_APPLICATIONS_URL), Mockito.eq(HttpMethod.POST), Mockito.any(),
+            Mockito.any(ParameterizedTypeReference.class), Mockito.anyInt(), Mockito.anyInt()))
+        .then(new Answer<ResponseEntity<Page<Application>>>() {
+
+          @Override
+          public ResponseEntity<Page<Application>> answer(InvocationOnMock invocation) throws Throwable {
+            return generatePage(invocation, numApplicationPages, Application.class);
+          }
+        });
+    Mockito
+        .when(restTemplate.exchange(Mockito.eq(ALL_PROJECTS_URL), Mockito.eq(HttpMethod.POST), Mockito.any(),
+            Mockito.any(ParameterizedTypeReference.class), Mockito.anyInt(), Mockito.anyInt()))
+        .then(new Answer<ResponseEntity<Page<Project>>>() {
+
+          @Override
+          public ResponseEntity<Page<Project>> answer(InvocationOnMock invocation) throws Throwable {
+            return generatePage(invocation, numProjectPages, Project.class);
+          }
+        });
+    Mockito
+        .when(restTemplate.exchange(Mockito.eq(ALL_CUSTOMERS_URL), Mockito.eq(HttpMethod.POST), Mockito.any(),
+            Mockito.any(ParameterizedTypeReference.class), Mockito.anyInt(), Mockito.anyInt()))
+        .then(new Answer<ResponseEntity<Page<Customer>>>() {
+
+          @Override
+          public ResponseEntity<Page<Customer>> answer(InvocationOnMock invocation) throws Throwable {
+            return generatePage(invocation, numCustomerPages, Customer.class);
+          }
+        });
+    Mockito
+        .when(restTemplate.exchange(Mockito.eq(ALL_CONTACTS_URL), Mockito.eq(HttpMethod.POST), Mockito.any(),
+            Mockito.any(ParameterizedTypeReference.class), Mockito.anyInt(), Mockito.anyInt()))
+        .then(new Answer<ResponseEntity<Page<Contact>>>() {
+
+          @Override
+          public ResponseEntity<Page<Contact>> answer(InvocationOnMock invocation) throws Throwable {
+            return generatePage(invocation, numContactPages, Contact.class);
+          }
+        });
+  }
+
+  // Mock code to emulate model-service's paging "get all" api. The requested
+  // page number & page size are in invocation. Total number of pages to supply
+  // is in numPages. If the request is for non-existing page, return
+  // HttpStatus.NOT_FOUND, otherwise generate a page and return it.
+  private <T> ResponseEntity<Page<T>> generatePage(InvocationOnMock invocation, int numPages, Class<T> clazz)
+      throws InstantiationException, IllegalAccessException {
+    int pageNum = invocation.getArgumentAt(4, Integer.class);
+    int pageSize = invocation.getArgumentAt(5, Integer.class);
+    if (pageNum >= numPages) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    List<T> elems = new ArrayList<>();
+    for (int i = 0; i < pageSize; ++i) {
+      elems.add(clazz.newInstance());
+    }
+    PageImpl<T> page = new PageImpl<>(elems, new PageRequest(pageNum, pageSize), pageSize * numPages);
+    return new ResponseEntity<>(page, HttpStatus.OK);
+  }
+}
