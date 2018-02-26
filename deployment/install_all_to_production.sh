697157572d236d687fec3f01ea9fed597662ebb1
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2018-02-26 13:11:22 +0000

    Merged in eero_production_configuration (pull request #931)
    Eero production configuration
    
    * Production deployment configuration
        - jump hosts for production (since all installation is proxied via frontend server)
        - sap mail configuration
        - sudo no password only for allu user
        - more reasonable delay for sap scheduling
    
    * Fixes to product deployment configuration
    
    Approved-by: Mikko Mehtonen <mikko.mehtonen@vincit.fi>

--- deployment/install_all_to_production.sh
+++ deployment/install_all_to_production.sh
@@ -3,7 +3,7 @@
 sh -e install_imageprep_to_production.sh dbservers
 sh -e install_imageprep_to_production.sh backendservers
 sh -e install_imageprep_to_production.sh webservers
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/prod_allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/prod_allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/prod_allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/prod_allu_id_rsa frontend.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa frontend.yml --vault-password-file ~/allu_keys/vault_secret
