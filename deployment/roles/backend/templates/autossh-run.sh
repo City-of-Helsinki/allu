47dfa8bf18ebae42ca077aeb1b2f5f17aec6d779
Mikko Mehtonen <mikko.mehtonen@vincit.fi>
2018-03-26 04:22:02 +0000

    Merged in mikko_autossh_port_fix (pull request #999)
    Fix destination server address and port for autossh and add known_hosts file
    
    Approved-by: Janne Matihaldi <janne.matihaldi@vincit.fi>
    (cherry picked from commit daa76a517a9852430336d2ac7731b82aa1ee4721)

--- deployment/roles/backend/templates/autossh-run.sh
+++ deployment/roles/backend/templates/autossh-run.sh
@@ -3,7 +3,7 @@
 {% if 'production' in group_names %}
 
 AUTOSSH_GATETIME=0
-usr/bin/autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o StrictHostKeyChecking=no -N -L 8022:{{sap_ftp_invoice_host}}:{{sap_ftp_invoice_port}} allu@{{ ssh_tunnel_server_address | default('NA') }} -i {{ allu_ssh_key_file | default('NA') }}
+usr/bin/autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o StrictHostKeyChecking=no -N -L 8022:{{ ssh_tunnel_destination_address }}:22 allu@{{ ssh_tunnel_server_address | default('NA') }} -i {{ allu_ssh_key_file | default('NA') }}
 
 {% else %}
 
