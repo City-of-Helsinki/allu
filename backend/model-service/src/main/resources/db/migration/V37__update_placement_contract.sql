ff75b77603f282e99d09c6b980a6e6e15891993c
Mikko Mehtonen <mikko.mehtonen@vincit.fi>
2018-04-11 12:08:59 +0000

    Merged in mikko_fix_placement_contract_field_name_change (pull request #1027)
    Add new fields only to placement contract applications
    
    Approved-by: Eero Kirjavainen <eero.kirjavainen@vincit.fi>

--- backend/model-service/src/main/resources/db/migration/V37__update_placement_contract.sql
+++ backend/model-service/src/main/resources/db/migration/V37__update_placement_contract.sql
@@ -2,7 +2,7 @@ insert into allu.attribute_meta (structure_meta_id,name,ui_name,data_type) value
   (select id from allu.structure_meta where type_name='PLACEMENT_CONTRACT'),'propertyIdentificationNumber','Kiinteistötunnus','STRING');
 
 update allu.attribute_meta set ui_name='Sopimusteksti',name='contractText' where name='generalTerms' and structure_meta_id=(select id from allu.structure_meta where type_name='PLACEMENT_CONTRACT');
-update allu.application set extension = extension::jsonb - 'generalTerms' || jsonb_build_object('contractText', extension::jsonb->'generalTerms');
+update allu.application set extension = extension::jsonb - 'generalTerms' || jsonb_build_object('contractText', extension::jsonb->'generalTerms') where type='PLACEMENT_CONTRACT';
 
 update allu.attribute_meta set ui_name='Asiointitunnus',name='identificationNumber' where name='diaryNumber' and structure_meta_id=(select id from allu.structure_meta where type_name='PLACEMENT_CONTRACT');
-update allu.application set extension = extension::jsonb - 'diaryNumber' || jsonb_build_object('identificationNumber', extension::jsonb->'diaryNumber');
+update allu.application set extension = extension::jsonb - 'diaryNumber' || jsonb_build_object('identificationNumber', extension::jsonb->'diaryNumber') where type='PLACEMENT_CONTRACT';
