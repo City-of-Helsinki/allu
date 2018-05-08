82c3e6fd1a5775d583d2e2d8b0eba2ab09635a6a
Mikko Mehtonen <mikko.mehtonen@vincit.fi>
2018-05-08 06:33:05 +0000

    Merged in mikko_codeset_caching (pull request #1078)
    Add caching for codesets
    
    Approved-by: Eero Kirjavainen <eero.kirjavainen@vincit.fi>

--- backend/service-core/src/main/java/fi/hel/allu/servicecore/service/CodeSetService.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/CodeSetService.java
@@ -4,6 +4,7 @@ import fi.hel.allu.common.domain.types.CodeSetType;
 import fi.hel.allu.model.domain.CodeSet;
 import fi.hel.allu.servicecore.config.ApplicationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,18 +23,21 @@ public class CodeSetService {
     this.restTemplate = restTemplate;
   }
 
+  @Cacheable("codesets")
   public CodeSet findById(Integer id) {
     final ResponseEntity<CodeSet> result = restTemplate
         .getForEntity(applicationProperties.getCodeSetFindByIdUrl(), CodeSet.class, id);
     return result.getBody();
   }
 
+  @Cacheable("codesets")
   public List<CodeSet> findByType(CodeSetType type) {
     final ResponseEntity<CodeSet[]> result = restTemplate
         .getForEntity(applicationProperties.getCodeSetFindByTypeUrl(), CodeSet[].class, type);
     return Arrays.asList(result.getBody());
   }
 
+  @Cacheable("codesets")
   public CodeSet findByTypeAndCode(CodeSetType type, String code) {
     final ResponseEntity<CodeSet> result = restTemplate
         .getForEntity(applicationProperties.getCodeSetFindByTypeAndCodeUrl(), CodeSet.class, type, code);
