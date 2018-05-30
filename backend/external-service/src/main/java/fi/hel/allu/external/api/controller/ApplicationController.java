ac2b91509e119d24e9ad979b2cd3362238521616
Janne Matihaldi <janne.matihaldi@vincit.fi>
2018-05-30 06:06:51 +0000

    Merged in janne_ext_api_cancel (pull request #1145)
    Ext API: cancel application
    
    * Rename controller with more descriptive name.
    
    * Ext API - cancel application
    
    * Localized error messages
    
    Approved-by: Mikko Mehtonen <mikko.mehtonen@vincit.fi>
    (cherry picked from commit 055e376c6feb44a0cbc82af777e52704bb2f0586)

---
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/ApplicationController.java
@@ -0,0 +1,38 @@
+package fi.hel.allu.external.api.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.hel.allu.external.service.ApplicationServiceExt;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
+
+@RestController
+@RequestMapping("/v1/applications")
+@Api(value = "v1/applications")
+public class ApplicationController {
+
+  @Autowired
+  private ApplicationServiceExt applicationService;
+
+  @ApiOperation(value = "Sets Allu application cancelled",
+      produces = "application/json",
+      authorizations=@Authorization(value ="api_key"))
+  @RequestMapping(value = "/{id}/cancelled", method = RequestMethod.PUT)
+  @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
+  public ResponseEntity<Void> cancelApplication(@ApiParam(value = "Id of the application to cancel.") @PathVariable Integer id) {
+    applicationService.validateOwnedByExternalUser(id);
+    applicationService.cancelApplication(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
