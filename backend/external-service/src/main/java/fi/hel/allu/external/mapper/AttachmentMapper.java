35b54dd1b302b911c8d6471d401d71c34e6738d2
Janne Matihaldi <janne.matihaldi@vincit.fi>
2018-06-05 09:33:53 +0000

    Merged in janne_attachment_type (pull request #1167)
    Set type for ext API attachments
    
    Approved-by: Eero Kirjavainen <eero.kirjavainen@vincit.fi>

--- backend/external-service/src/main/java/fi/hel/allu/external/mapper/AttachmentMapper.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/mapper/AttachmentMapper.java
@@ -1,5 +1,6 @@
 package fi.hel.allu.external.mapper;
 
+import fi.hel.allu.common.types.AttachmentType;
 import fi.hel.allu.external.domain.AttachmentInfoExt;
 import fi.hel.allu.servicecore.domain.AttachmentInfoJson;
 
@@ -7,6 +8,7 @@ public class AttachmentMapper {
 
   public static AttachmentInfoJson toAttachmentInfoJson(AttachmentInfoExt attachmentInfoExt) {
     AttachmentInfoJson attachmentInfo = new AttachmentInfoJson();
+    attachmentInfo.setType(AttachmentType.ADDED_BY_CUSTOMER);
     attachmentInfo.setDescription(attachmentInfoExt.getDescription());
     attachmentInfo.setName(attachmentInfoExt.getName());
     attachmentInfo.setMimeType(attachmentInfoExt.getMimeType());
