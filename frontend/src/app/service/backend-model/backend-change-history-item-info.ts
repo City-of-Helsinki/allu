9aa3f3506fbcc952a22acb3182dc9a1a19620100
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-06-05 17:25:59 +0300

    Show an info about status changes on change history on UI

---
+++ frontend/src/app/service/backend-model/backend-change-history-item-info.ts
@@ -0,0 +1,8 @@
+export class BackendChangeHistoryItemInfo {
+  constructor(
+    public id: number,
+    public name?: string,
+    public applicationId?: string
+  ) {
+  }
+}
--- frontend/src/app/service/mapper/change-history-mapper.ts
+++ frontend/src/app/service/mapper/change-history-mapper.ts
@@ -1,5 +1,7 @@
 import {BackendChangeHistoryItem} from '../backend-model/backend-change-history-item';
+import {BackendChangeHistoryItemInfo} from '../backend-model/backend-change-history-item-info';
 import {ChangeHistoryItem} from '../../model/history/change-history-item';
+import {ChangeHistoryItemInfo} from '../../model/history/change-history-item-info';
 import {TimeUtil} from '../../util/time.util';
 import {FieldChange} from '../../model/history/field-change';
 import {BackendFieldChange} from '../backend-model/backend-field-change';
@@ -19,10 +21,19 @@ export class ChangeHistoryMapper {
   public static mapBackend(backendChange: BackendChangeHistoryItem): ChangeHistoryItem {
     return new ChangeHistoryItem(
       UserMapper.mapBackend(backendChange.user),
+      this.mapBackendInfo(backendChange.info),
       backendChange.changeType,
       backendChange.newStatus,
       TimeUtil.dateFromBackend(backendChange.changeTime),
       backendChange.fieldChanges ? backendChange.fieldChanges.map(fc => FieldChangeMapper.mapBackend(fc)) : []
+    );
+  }
+
+  private static mapBackendInfo(backendInfo: BackendChangeHistoryItemInfo): ChangeHistoryItemInfo {
+    return new ChangeHistoryItemInfo(
+      backendInfo.id,
+      backendInfo.name,
+      backendInfo.applicationId
     );
   }
 }
