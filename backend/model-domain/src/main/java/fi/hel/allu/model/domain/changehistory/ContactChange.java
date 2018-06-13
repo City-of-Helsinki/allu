1e17cf144efb85cdb44ef154b3fee684e2a6dbc0
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-06-13 14:57:11 +0300

    Rename contactName to name on ContactChange

--- backend/model-domain/src/main/java/fi/hel/allu/model/domain/changehistory/ContactChange.java
+++ backend/model-domain/src/main/java/fi/hel/allu/model/domain/changehistory/ContactChange.java
@@ -3,20 +3,20 @@ package fi.hel.allu.model.domain.changehistory;
 import fi.hel.allu.model.domain.Contact;
 
 public class ContactChange {
-  private final String contactName;
+  private final String name;
   private final Integer id;
   public ContactChange(Contact contact) {
     if (contact != null) {
-      this.contactName = contact.getName();
+      this.name = contact.getName();
       this.id = contact.getId();
     } else {
-      this.contactName = null;
+      this.name = null;
       this.id = null;
     }
   }
 
-  public String getContactName() {
-    return contactName;
+  public String getName() {
+    return name;
   }
 
   public Integer getId() {
