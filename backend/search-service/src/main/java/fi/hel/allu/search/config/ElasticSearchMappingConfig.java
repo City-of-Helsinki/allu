3a1885049a92c3afa8bbc47ec4c39cca78726de5
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-06-21 13:12:55 +0300

    Applicant can be searched by partial name

--- backend/search-service/src/main/java/fi/hel/allu/search/config/ElasticSearchMappingConfig.java
+++ backend/search-service/src/main/java/fi/hel/allu/search/config/ElasticSearchMappingConfig.java
@@ -41,7 +41,7 @@ public class ElasticSearchMappingConfig {
   private static final String FILTER_AUTOCOMPLETE = "autocomplete_filter";
 
   // Note! Change this version number if you edit mappings. Then changes will be updated to elastic on next startup.
-  private static final String MAPPINGS_VERSION_NUMBER = "8";
+  private static final String MAPPINGS_VERSION_NUMBER = "9";
 
   private static final String VERSION_INDEX_NAME = "versions";
   private static final String VERSION_TYPE_NAME = "version";
@@ -162,10 +162,7 @@ public class ElasticSearchMappingConfig {
                     .startObject("properties")
                       .startObject("customer")
                         .startObject("properties")
-                          .startObject("name")
-                            .field("type", "text")
-                            .field("fields").copyCurrentStructure(parser(alphasort()))
-                          .endObject()
+                          .field("name").copyCurrentStructure(parser(autocompleteWithAlphaSortingMappingAnalyzer()))
                         .endObject()
                       .endObject()
                     .endObject()
