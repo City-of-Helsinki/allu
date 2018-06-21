261f821e3b07ac40edc032ee5a4f0727e0672b1b
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-06-21 12:05:15 +0300

    Add "Add to basket" button to application search page

--- frontend/src/app/feature/search/search.component.ts
+++ frontend/src/app/feature/search/search.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/internal/operators';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {distinctUntilChanged, map, takeUntil} from 'rxjs/internal/operators';
 
 import {Application} from '../../model/application/application';
 import {ApplicationSearchQuery} from '../../model/search/ApplicationSearchQuery';
 import {ApplicationStatus, searchable} from '../../model/application/application-status';
+import {ApplicationWorkItemStore} from '../workqueue/application-work-item-store';
 import {EnumUtil} from '../../util/enum.util';
 import {ApplicationType} from '../../model/application/type/application-type';
 import {UserHub} from '../../service/user/user-hub';
@@ -12,10 +13,11 @@ import {User} from '../../model/user/user';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {CityDistrict} from '../../model/common/city-district';
 import {ApplicationService} from '../../service/application/application.service';
-import {MatPaginator, MatSort} from '@angular/material';
+import {MatCheckboxChange, MatPaginator, MatSort} from '@angular/material';
 import {ApplicationSearchDatasource} from '../../service/application/application-search-datasource';
 import {NotificationService} from '../../service/notification/notification.service';
 import {ArrayUtil} from '../../util/array-util';
+import {AddMultiple} from '../project/actions/application-basket-actions';
 import * as fromRoot from '../allu/reducers';
 import {Store} from '@ngrx/store';
 
@@ -23,10 +25,10 @@ import {Store} from '@ngrx/store';
   selector: 'search',
   templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
 
   displayedColumns = [
-    'owner.realName', 'applicationId', 'name', 'type', 'status', 'project.identifier',
+    'selected', 'owner.realName', 'applicationId', 'name', 'type', 'status', 'project.identifier',
     'customers.applicant.customer.name', 'locations.address', 'locations.cityDistrictId',
     'creationTime', 'startTime'
   ];
@@ -39,11 +41,17 @@ export class SearchComponent implements OnInit {
   applicationTypeStrings = EnumUtil.enumValues(ApplicationType)
     .sort(ArrayUtil.naturalSortTranslated(['application.type'], (type: string) => type));
   dataSource: ApplicationSearchDatasource;
+  allSelected = false;
+  noneSelected = true;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  private selectedItems: Array<number> = [];
+  private destroy = new Subject<boolean>();
+
   constructor(private applicationService: ApplicationService,
+              private itemStore: ApplicationWorkItemStore,
               private userHub: UserHub,
               private store: Store<fromRoot.State>,
               private fb: FormBuilder,
@@ -68,6 +76,29 @@ export class SearchComponent implements OnInit {
     this.owners = this.userHub.getActiveUsers().pipe(
       map(users => users.sort(ArrayUtil.naturalSort((user: User) => user.realName))));
     this.districts = this.store.select(fromRoot.getAllCityDistricts);
+
+    this.itemStore.changes.pipe(
+      map(state => state.selectedItems),
+      distinctUntilChanged(),
+      takeUntil(this.destroy)
+    ).subscribe(selected => this.selectedItems = selected);
+
+    this.itemStore.changes.pipe(
+      map(state => state.allSelected),
+      distinctUntilChanged(),
+      takeUntil(this.destroy)
+    ).subscribe(allSelected => this.allSelected = allSelected);
+
+    this.itemStore.changes.pipe(
+      map(state => state.selectedItems),
+      distinctUntilChanged(),
+      takeUntil(this.destroy),
+    ).subscribe(items => this.noneSelected = (items.length === 0));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
   }
 
   search(): void {
@@ -81,5 +112,23 @@ export class SearchComponent implements OnInit {
 
   trackById(index: number, item: Application) {
     return item.id;
+  }
+
+  selected(id: number): boolean {
+    return this.selectedItems.indexOf(id) >= 0;
+  }
+
+  checkAll(change: MatCheckboxChange): void {
+    this.itemStore.toggleAll(change.checked);
+  }
+
+  checkSingle(change: MatCheckboxChange, taskId: number) {
+    this.itemStore.toggleSingle(taskId, change.checked);
+  }
+
+  addToBasket(): void {
+    const selected = this.itemStore.snapshot.selectedItems;
+    this.store.dispatch(new AddMultiple(selected));
+    this.itemStore.selectedItemsChange([]);
   }
 }
