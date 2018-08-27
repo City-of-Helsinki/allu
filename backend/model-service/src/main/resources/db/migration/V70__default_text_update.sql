e1ee6d466a4d207aef0d3e9b7ea33c7871c318fa
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-08-27 16:20:45 +0300

    Add PLACEMENT_CONTRACT/rationale to attribute meta

--- backend/model-service/src/main/resources/db/migration/V70__default_text_update.sql
+++ backend/model-service/src/main/resources/db/migration/V70__default_text_update.sql
@@ -2,3 +2,6 @@ update allu.default_text set text_type = 'TERMS' where text_type = 'OTHER' and a
 
 insert into  allu.attribute_meta (name, ui_name, data_type, structure_meta_id)
   values ('TERMS', 'Ehdot', 'ENUM_VALUE', (select id from allu.structure_meta where type_name = 'CableInfoType'));
+
+insert into  allu.attribute_meta (name, ui_name, data_type, structure_meta_id)
+  values ('rationale', 'Päätöksen perustelu', 'STRING', (select id from allu.structure_meta where type_name = 'PLACEMENT_CONTRACT'));
