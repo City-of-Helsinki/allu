88aa9386d9356a9490facbe1af15aad1b8e27a58
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-09-11 13:54:56 +0300

    Add winter time start and end to the configuration

--- backend/model-service/src/main/resources/db/migration/V74__configuration_key.sql
+++ backend/model-service/src/main/resources/db/migration/V74__configuration_key.sql
@@ -3,3 +3,6 @@ update allu.configuration set key=type;
 update allu.configuration set type='EMAIL' where key='CUSTOMER_NOTIFICATION_RECEIVER_EMAIL';
 update allu.configuration set type='EMAIL' where key='INVOICE_NOTIFICATION_RECEIVER_EMAIL';
 alter table allu.configuration alter column key set not null;
+
+insert into allu.configuration (type,key,value) values ('DATE','WINTER_TIME_START','1972-12-01');
+insert into allu.configuration (type,key,value) values ('DATE','WINTER_TIME_END','1972-05-14');
