477cc7d6afb819f9b08623a3936601b7e655a0af
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-09-19 14:00:42 +0300

    Add missing value to enum ConfigurationType

---
+++ backend/model-service/src/main/resources/db/migration/V77__placement_contract_section_number_year.sql
@@ -0,0 +1,23 @@
+update allu.configuration set type='TEXT', key='PLACEMENT_CONTRACT_SECTION_NUMBER_YEAR' where key='placement_contract_section_number_year';
+
+CREATE OR REPLACE FUNCTION allu.get_placement_contract_section_number()
+RETURNS INT AS $$
+DECLARE
+  year TEXT;
+  cur_year TEXT;
+  conf_count INT;
+BEGIN
+  select date_part('year',(now())) into cur_year;
+  select value into year from allu.configuration where key='PLACEMENT_CONTRACT_SECTION_NUMBER_YEAR';
+  select count(*) into conf_count from allu.configuration where key='PLACEMENT_CONTRACT_SECTION_NUMBER_YEAR';
+  if (year is null or year <> cur_year) then
+    alter sequence allu.placement_contract_section_number restart with 1;
+    if (conf_count < 1) then
+      insert into allu.configuration (key, type, value) values ('PLACEMENT_CONTRACT_SECTION_NUMBER_YEAR', 'TEXT', cur_year);
+    else
+      update allu.configuration set value=cur_year where key='PLACEMENT_CONTRACT_SECTION_NUMBER_YEAR';
+    end if;
+  end if;
+  return nextval('allu.placement_contract_section_number');
+END;
+$$ LANGUAGE plpgsql;
