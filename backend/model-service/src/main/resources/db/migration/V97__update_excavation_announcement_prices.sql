e54bb3def9f17d9c48ad801b9cc64f4f2b0dfb33
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-10-23 16:52:15 +0300

    Update prices for excavation announcement

---
+++ backend/model-service/src/main/resources/db/migration/V97__update_excavation_announcement_prices.sql
@@ -0,0 +1,21 @@
+delete from allu.pricing where application_type='EXCAVATION_ANNOUNCEMENT';
+
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'HANDLING_FEE', null, 21000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'SMALL_AREA_DAILY_FEE', '1', 7500);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'SMALL_AREA_DAILY_FEE', '2', 5000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'SMALL_AREA_DAILY_FEE', '3', 2500);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'SMALL_AREA_DAILY_FEE', '4a', 1250);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'SMALL_AREA_DAILY_FEE', '4b', 1250);
+
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'MEDIUM_AREA_DAILY_FEE', '1', 9750);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'MEDIUM_AREA_DAILY_FEE', '2', 6500);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'MEDIUM_AREA_DAILY_FEE', '3', 3250);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'MEDIUM_AREA_DAILY_FEE', '4a', 1625);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'MEDIUM_AREA_DAILY_FEE', '4b', 1625);
+
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'LARGE_AREA_DAILY_FEE', '1', 12000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'LARGE_AREA_DAILY_FEE', '2', 8000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'LARGE_AREA_DAILY_FEE', '3', 4000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'LARGE_AREA_DAILY_FEE', '4a', 2000);
+insert into allu.pricing (application_type, key, payment_class, value) values ('EXCAVATION_ANNOUNCEMENT', 'LARGE_AREA_DAILY_FEE', '4b', 2000);
+
