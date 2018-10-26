b656c54b447d67f04c29d235577de2c81972d227
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2018-10-26 13:37:28 +0300

    Sort configurations by type and id

--- frontend/src/app/feature/allu/reducers/configuration-reducer.ts
+++ frontend/src/app/feature/allu/reducers/configuration-reducer.ts
@@ -2,12 +2,18 @@ import {Configuration} from '@model/config/configuration';
 import {ConfigurationActions, ConfigurationActionType} from '../actions/configuration-actions';
 import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
 
+export function sortConfiguration(left: Configuration, right: Configuration) {
+  return (left.type.toString().localeCompare(right.type.toString()))
+    || (left.id - right.id);
+}
+
 export interface State extends EntityState<Configuration> {
   loading: boolean;
 }
 
 export const adapter: EntityAdapter<Configuration> = createEntityAdapter<Configuration>({
-  selectId: (configuration: Configuration) => configuration.id
+  selectId: (configuration: Configuration) => configuration.id,
+  sortComparer: sortConfiguration
 });
 
 const initialState: State = adapter.getInitialState({
