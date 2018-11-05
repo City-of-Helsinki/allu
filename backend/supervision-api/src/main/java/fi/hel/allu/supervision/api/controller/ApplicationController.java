4a0b64fb7566609e2eaf0829bdc3ff610e562ff6
Janne Matihaldi <janne.matihaldi@vincit.fi>
2018-11-05 14:51:50 +0200

    Fix compiler error

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationController.java
@@ -1,37 +1,23 @@
 package fi.hel.allu.supervision.api.controller;
 
 
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fi.hel.allu.common.domain.types.ApplicationTagType;
-import fi.hel.allu.search.domain.*;
+import fi.hel.allu.search.domain.ApplicationES;
+import fi.hel.allu.search.domain.ApplicationQueryParameters;
 import fi.hel.allu.servicecore.domain.search.ApplicationSearchResult;
-import fi.hel.allu.servicecore.domain.search.LocationSearchResult;
 import fi.hel.allu.servicecore.mapper.ApplicationMapper;
 import fi.hel.allu.servicecore.service.ApplicationServiceComposer;
-import fi.hel.allu.supervision.api.domain.SearchParameterField;
-import fi.hel.allu.supervision.api.domain.SearchParameterType;
 import fi.hel.allu.supervision.api.domain.SearchParameters;
 import io.swagger.annotations.*;
 
@@ -59,8 +45,8 @@ public class ApplicationController {
   @RequestMapping(value = "/search", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Page<ApplicationSearchResult>> search(@RequestBody SearchParameters searchParameters) {
-    ApplicationQueryParameters queryParameters = mapToQueryParameters(searchParameters);
-    Pageable pageable = mapToPageRequest(searchParameters);
+    ApplicationQueryParameters queryParameters = SearchParameterMapper.mapToQueryParameters(searchParameters);
+    Pageable pageable = SearchParameterMapper.mapToPageRequest(searchParameters);
     Page<ApplicationES> result = applicationServiceComposer.search(queryParameters, pageable, Boolean.FALSE);
     Page<ApplicationSearchResult> response = result.map(a -> applicationMapper.mapToSearchResult(a));
     return ResponseEntity.ok(response);
