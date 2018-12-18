99084064988155212da7b5af2e131adfa385f813
Mikko Mehtonen <mikko.mehtonen@vincit.com>
2018-12-18 11:19:01 +0200

    Same permission for adding/removing/updating attachemnts for backend and frontend

--- backend/allu-ui-service/src/main/java/fi/hel/allu/ui/controller/ApplicationController.java
+++ backend/allu-ui-service/src/main/java/fi/hel/allu/ui/controller/ApplicationController.java
@@ -144,7 +144,7 @@ public class ApplicationController {
    * @throws IllegalArgumentException
    */
   @RequestMapping(value = "/{id}/attachments", method = RequestMethod.POST)
-  @PreAuthorize("hasAnyRole('ROLE_VIEW')")
+  @PreAuthorize("hasAnyRole('ROLE_CREATE_APPLICATION', 'ROLE_PROCESS_APPLICATION')")
   public ResponseEntity<List<AttachmentInfoJson>> addAttachments(
       @PathVariable int id,
       @RequestPart("meta") @Valid AttachmentInfoJson[] infos, @RequestPart("file") MultipartFile[] files)
@@ -185,7 +185,7 @@ public class ApplicationController {
    * @return
    */
   @RequestMapping(value = "/attachments/{id}", method = RequestMethod.PUT)
-  @PreAuthorize("hasAnyRole('ROLE_VIEW')")
+  @PreAuthorize("hasAnyRole('ROLE_CREATE_APPLICATION', 'ROLE_PROCESS_APPLICATION')")
   public ResponseEntity<AttachmentInfoJson> updateAttachmentInfo(@PathVariable int id,
       @Valid @RequestBody AttachmentInfoJson attachmentInfoJson) {
     return new ResponseEntity<>(attachmentService.updateAttachment(id, attachmentInfoJson), HttpStatus.OK);
@@ -199,7 +199,7 @@ public class ApplicationController {
    * @return
    */
   @RequestMapping(value = "/{applicationId}/attachments/{attachmentId}", method = RequestMethod.DELETE)
-  @PreAuthorize("hasAnyRole('ROLE_VIEW')")
+  @PreAuthorize("hasAnyRole('ROLE_CREATE_APPLICATION', 'ROLE_PROCESS_APPLICATION')")
   public ResponseEntity<Void> deleteAttachment(
       @PathVariable int applicationId,
       @PathVariable int attachmentId) {
