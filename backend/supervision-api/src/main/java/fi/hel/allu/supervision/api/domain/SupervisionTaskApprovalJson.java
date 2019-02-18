74cc04f888572afda3923a798d149473e21022c8
Janne Matihaldi <janne.matihaldi@vincit.fi>
2019-02-18 15:20:39 +0200

    Validate work finished / operational condition dates
    Work finished / operational condition date cannot be
    in future.

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/domain/SupervisionTaskApprovalJson.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/domain/SupervisionTaskApprovalJson.java
@@ -6,10 +6,18 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import fi.hel.allu.common.util.TimeUtil;
+import fi.hel.allu.common.validator.NotFalse;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel("Supervision task approval data")
+@NotFalse(rules = {
+    "operationalConditionDate, operationalConditionNotInFuture, {supervisiontask.operationalCondition.invalid}",
+    "workFinishedDate, workFinishedNotInFuture, {supervisiontask.workFinished.invalid}",
+ })
 public class SupervisionTaskApprovalJson {
 
   @NotNull(message = "{supervisiontask.id}")
@@ -37,7 +45,7 @@ public class SupervisionTaskApprovalJson {
     this.result = result;
   }
 
-  @ApiModelProperty(value = "Date when work was in operational condition. Required when approving operational condition supervision (otherwise ignored).")
+  @ApiModelProperty(value = "Date when work was in operational condition. Required when approving operational condition supervision (otherwise ignored). Cannot be in future.")
   public ZonedDateTime getOperationalConditionDate() {
     return operationalConditionDate;
   }
@@ -46,12 +54,27 @@ public class SupervisionTaskApprovalJson {
     this.operationalConditionDate = operationalConditionDate;
   }
 
-  @ApiModelProperty(value = "Date when work was finished. Required when approving final supervision of area rental or excavation announcement (otherwise ignored).")
+  @ApiModelProperty(value = "Date when work was finished. Required when approving final supervision of area rental or excavation announcement (otherwise ignored). Cannot be in future.")
   public ZonedDateTime getWorkFinishedDate() {
     return workFinishedDate;
   }
 
   public void setWorkFinishedDate(ZonedDateTime workFinishedDate) {
     this.workFinishedDate = workFinishedDate;
+  }
+
+  @JsonIgnore
+  public boolean getOperationalConditionNotInFuture() {
+    return getNotInFuture(operationalConditionDate);
+  }
+
+  @JsonIgnore
+  public boolean getWorkFinishedNotInFuture() {
+    return getNotInFuture(workFinishedDate);
+  }
+
+  @JsonIgnore
+  private boolean getNotInFuture(ZonedDateTime date) {
+    return date == null || date.isBefore(TimeUtil.startOfDay(TimeUtil.nextDay(ZonedDateTime.now())));
   }
 }
