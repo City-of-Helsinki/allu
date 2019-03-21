0550516a0abbf9bd920a4b24f8fc746740c2fc05
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2019-03-21 08:31:54 +0200

    Fixed wrong times terraces' and parklet's time configuration and duplicate summer terrace start key

--- backend/model-service/src/main/resources/db/migration/V141__terraces_parklets_time_period_configs.sql
+++ backend/model-service/src/main/resources/db/migration/V141__terraces_parklets_time_period_configs.sql
@@ -1,8 +1,8 @@
 insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-04-01', 'SUMMER_TERRACE_TIME_START');
-insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-10-31', 'SUMMER_TERRACE_TIME_START');
+insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-10-31', 'SUMMER_TERRACE_TIME_END');
 
-insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-04-01', 'WINTER_TERRACE_TIME_START');
-insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-10-31', 'WINTER_TERRACE_TIME_END');
+insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-11-01', 'WINTER_TERRACE_TIME_START');
+insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-03-31', 'WINTER_TERRACE_TIME_END');
 
-insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-11-01', 'PARKLET_TIME_START');
-insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-03-31', 'PARKLET_TIME_END');
+insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-04-01', 'PARKLET_TIME_START');
+insert into allu.configuration (type, value, key) values ('CALENDAR_DATE', '1973-10-31', 'PARKLET_TIME_END');
