7bf1b9b5a86761c11146562b0d2456da5c99140b
Janne Matihaldi <janne.matihaldi@vincit.fi>
2019-03-26 14:52:24 +0200

    Add terrace prices

---
+++ backend/model-service/src/main/resources/db/migration/V143__terrace_prices.sql
@@ -0,0 +1,6 @@
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'SUMMER_TERRACE', '1', 1223);
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'WINTER_TERRACE', '1', 612);
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'PARKLET', '1', 1223);
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'SUMMER_TERRACE', '2', 918);
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'WINTER_TERRACE', '2', 459);
+insert into allu.pricing (application_type, key, payment_class, value) values ('SHORT_TERM_RENTAL', 'PARKLET', '2', 918);
