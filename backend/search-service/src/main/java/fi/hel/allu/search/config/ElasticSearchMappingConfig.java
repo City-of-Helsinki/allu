c1110ca75229ed0f75707b403024cb00ca4d6fc3
Janne Matihaldi <janne.matihaldi@vincit.fi>
2019-05-06 16:40:51 +0300

    Fix search by user name returning incorrect results
    Standard analyzer tokenized usernames at dot and
    all users having at least one equal token with the
    searched one were returned.

--- backend/search-service/src/main/java/fi/hel/allu/search/config/ElasticSearchMappingConfig.java
+++ backend/search-service/src/main/java/fi/hel/allu/search/config/ElasticSearchMappingConfig.java
@@ -42,7 +42,7 @@ public class ElasticSearchMappingConfig {
   private static final String FILTER_AUTOCOMPLETE = "autocomplete_filter";
 
   // Note! Change this version number if you edit mappings. Then changes will be updated to elastic on next startup.
-  private static final String MAPPINGS_VERSION_NUMBER = "18";
+  private static final String MAPPINGS_VERSION_NUMBER = "19";
 
   private static final String VERSION_INDEX_NAME = "versions";
   private static final String VERSION_TYPE_NAME = "version";
@@ -147,10 +147,7 @@ public class ElasticSearchMappingConfig {
               .field("name").copyCurrentStructure(parser(autocompleteWithAlphaSortingMappingAnalyzer()))
               .startObject("owner") // alphabetical sorting for owner.userName
                 .startObject("properties")
-                  .startObject("userName")
-                    .field("type", "text")
-                    .field("fields").copyCurrentStructure(parser(alphasort()))
-                  .endObject()
+                    .field("userName").copyCurrentStructure(parser(autocompleteWithAlphaSortingMappingAnalyzerAndKeywordSearchAnalyzer()))
                   .startObject("realName")
                     .field("type", "text")
                     .field("fields").copyCurrentStructure(parser(alphasort()))
@@ -171,7 +168,6 @@ public class ElasticSearchMappingConfig {
                           .field("name").copyCurrentStructure(parser(autocompleteWithAlphaSortingMappingAnalyzer()))
                         .endObject()
                       .endObject()
-
                     .endObject()
                   .endObject()
                 .endObject()
