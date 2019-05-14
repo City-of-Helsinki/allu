9ab0e2b948fa03e435702f423a7348d8ed2763b6
Arto Seppa <arto.seppa@vincit.fi>
2019-05-14 13:45:11 +0300

    Add button to add/remove survey required tag
    This moves controlling the tag to a better place in user flow.

--- frontend/src/app/feature/application/info/cable-report/cable-report.component.ts
+++ frontend/src/app/feature/application/info/cable-report/cable-report.component.ts
@@ -2,11 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {FormGroup, Validators} from '@angular/forms';
 
 import {Application} from '@model/application/application';
+import {ApplicationTag} from '@model/application/tag/application-tag';
+import {ApplicationTagType} from '@model/application/tag/application-tag-type';
 import {ComplexValidator} from '@util/complex-validator';
 import {CableReportForm, createDefaultOrdererId, from, to} from './cable-report.form';
 import {ApplicationInfoBaseComponent} from '@feature/application/info/application-info-base.component';
 import {CableReport} from '@model/application/cable-report/cable-report';
 import {ApplicationStatus, isSameOrAfter} from '@model/application/application-status';
+import * as fromApplication from '@feature/application/reducers';
+import {Add, Remove} from "@feature/application/actions/application-tag-actions";
+import {select} from '@ngrx/store';
+import {Observable} from "rxjs/index";
 import {TimeUtil} from '@util/time.util';
 
 @Component({
@@ -18,6 +24,12 @@ import {TimeUtil} from '@util/time.util';
 export class CableReportComponent extends ApplicationInfoBaseComponent implements OnInit {
 
   showCableInfo = false;
+  isSurveyRequired: Observable<boolean>;
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.isSurveyRequired = this.store.pipe(select(fromApplication.hasTag(ApplicationTagType.SURVEY_REQUIRED)));
+  }
 
   protected createExtensionForm(): FormGroup {
     return this.fb.group({
@@ -55,5 +67,13 @@ export class CableReportComponent extends ApplicationInfoBaseComponent implement
     application.singleLocation.endTime = application.endTime;
 
     return application;
+  }
+
+  markSurveyRequired(): void {
+    this.store.dispatch(new Add(new ApplicationTag(ApplicationTagType.SURVEY_REQUIRED, undefined, new Date())));
+  }
+
+  markSurveyDone(): void {
+    this.store.dispatch(new Remove(new ApplicationTag(ApplicationTagType.SURVEY_REQUIRED, undefined, new Date())));
   }
 }
