5192a6e057cf265f1120513955bade77dafbed27
Janne Matihaldi <janne.matihaldi@vincit.fi>
2019-08-27 13:10:35 +0300

    Fix failing PDF-merge
    PDF-merge fails with some PDF-documents. Use merge-method
    instead of appendDocuments from PDFMergerUtility and update
    PDFBox version to fix merge failure.

--- backend/common/src/main/java/fi/hel/allu/common/util/PdfMerger.java
+++ backend/common/src/main/java/fi/hel/allu/common/util/PdfMerger.java
@@ -1,34 +1,22 @@
 package fi.hel.allu.common.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class PdfMerger {
 
   public static byte[] appendDocuments(byte[] destination, List<byte[]> sources) throws IOException {
     PDFMergerUtility pdfMerger = new PDFMergerUtility();
-    try (PDDocument destinationDoc = PDDocument.load(destination)) {
-      sources.forEach(s -> appendDocument(pdfMerger, destinationDoc, s));
-      return toByteArray(destinationDoc);
-    }
-  }
-
-  private static void appendDocument(PDFMergerUtility pdfMerger, PDDocument destination, byte[] source) {
-    try (PDDocument sourceDoc = PDDocument.load(source)) {
-      pdfMerger.appendDocument(destination, sourceDoc);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private static byte[] toByteArray(PDDocument document) throws IOException {
+    pdfMerger.addSource(new ByteArrayInputStream(destination));
+    sources.forEach(s -> pdfMerger.addSource(new ByteArrayInputStream(s)));
     ByteArrayOutputStream output = new ByteArrayOutputStream();
-    document.save(output);
+    pdfMerger.setDestinationStream(output);
+    pdfMerger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
     return output.toByteArray();
   }
-
 }
