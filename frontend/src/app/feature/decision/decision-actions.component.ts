938cfc585d8cb43e69636cbf9fa3f25c216d2b61
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2019-10-11 11:46:26 +0300

    Allow document resend on "OPERATIONAL_CONDITION" state

--- frontend/src/app/feature/decision/decision-actions.component.ts
+++ frontend/src/app/feature/decision/decision-actions.component.ts
@@ -24,6 +24,7 @@ import {validForDecision} from '@feature/application/application-util';
 
 const RESEND_ALLOWED = [
   ApplicationStatus.DECISION,
+  ApplicationStatus.OPERATIONAL_CONDITION,
   ApplicationStatus.FINISHED,
   ApplicationStatus.ARCHIVED,
 ];
