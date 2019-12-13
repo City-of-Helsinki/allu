0bb222a35e57addb7e574c97258b3b3a904db3ef
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2019-12-13 11:19:28 +0200

    Load TerracePeriods on demand since @PostConstruct causes app start to fail

--- backend/service-core/src/main/java/fi/hel/allu/servicecore/service/TerracePeriodService.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/TerracePeriodService.java
@@ -5,7 +5,6 @@ import fi.hel.allu.model.domain.ConfigurationKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 
 @Service
@@ -13,34 +12,28 @@ public class TerracePeriodService {
 
   private final ConfigurationService configurationService;
 
-  private AnnualTimePeriod summerTerracePeriod;
-  private AnnualTimePeriod winterTerracePeriod;
-
   @Autowired
   public TerracePeriodService(ConfigurationService configurationService) {
     this.configurationService = configurationService;
   }
 
-  @PostConstruct
-  public void init() {
-    summerTerracePeriod = new AnnualTimePeriod(
-      LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.SUMMER_TERRACE_TIME_START)),
-      LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.SUMMER_TERRACE_TIME_END)));
+  public AnnualTimePeriod getSummerTerracePeriod() {
+    return getSummerPeriod();
+  }
 
-    winterTerracePeriod = new AnnualTimePeriod(
+  public AnnualTimePeriod getWinterTerracePeriod() {
+    return new AnnualTimePeriod(
       LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.WINTER_TERRACE_TIME_START)),
       LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.WINTER_TERRACE_TIME_END)));
   }
 
-  public AnnualTimePeriod getSummerTerracePeriod() {
-    return summerTerracePeriod;
-  }
-
-  public AnnualTimePeriod getWinterTerracePeriod() {
-    return winterTerracePeriod;
-  }
-
   public AnnualTimePeriod getParkletPeriod() {
-    return summerTerracePeriod;
+    return getSummerPeriod();
+  }
+
+  private AnnualTimePeriod getSummerPeriod() {
+    return new AnnualTimePeriod(
+      LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.SUMMER_TERRACE_TIME_START)),
+      LocalDate.parse(configurationService.getSingleValue(ConfigurationKey.SUMMER_TERRACE_TIME_END)));
   }
 }
