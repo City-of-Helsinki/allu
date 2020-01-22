289c5623d702a0d124ffc66c15e3762aac977c45
Janne Matihaldi <janne.matihaldi@vincit.fi>
2020-01-22 11:53:04 +0200

    Fix additional property location configuration
    In Spring Boot 2.0 the behavior of the spring-config-location was
    changed, it now replaces the default locations instead of adding a
    location. Use spring.config.additional-location instead to
    add property location.

--- deployment/roles/backend/templates/run_service.sh
+++ deployment/roles/backend/templates/run_service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx{{ allu_service_memory_limit[item] | default('256m') }} -Dservice.home=/home/allu/{{ item }} -jar /home/allu/{{ item }}/{{ item }}.jar --spring.config.location=file:/home/allu/{{ item }}/{{ item }}.properties
+java -Xmx{{ allu_service_memory_limit[item] | default('256m') }} -Dservice.home=/home/allu/{{ item }} -jar /home/allu/{{ item }}/{{ item }}.jar --spring.config.additional-location=file:/home/allu/{{ item }}/{{ item }}.properties
--- deployment/roles/etl/files/etl/run_allu_etl.sh
+++ deployment/roles/etl/files/etl/run_allu_etl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx256m -Dservice.home=/home/allu/etl -jar /home/allu/etl/allu-etl.jar --spring.config.location=file:/home/allu/etl/allu-etl.properties
+java -Xmx256m -Dservice.home=/home/allu/etl -jar /home/allu/etl/allu-etl.jar --spring.config.additional-location=file:/home/allu/etl/allu-etl.properties
