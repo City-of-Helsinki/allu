dfedf2012dda3b1fdedfb6e17f21054206c8c93e
Antti Vuorinen <antti.vuorinen@vincit.fi>
2021-11-26 06:55:05 +0000

    adding docker-compose for local creation of allu database

--- deployment/roles/database/files/database_docker/init.sql
+++ deployment/roles/database/files/database_docker/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE model_service ENCODING utf8 LC_COLLATE 'fi_FI.utf8';
+CREATE DATABASE model_service ENCODING 'UTF8' LC_COLLATE 'fi_FI.utf8';
 CREATE USER model_service PASSWORD 'model_service';
 GRANT ALL PRIVILEGES ON DATABASE model_service TO model_service;
 
