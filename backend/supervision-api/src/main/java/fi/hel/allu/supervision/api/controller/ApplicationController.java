c2ac4b4b71f36d1aae4ac018242d9ce64309ee04
Antti Vuorinen <antti.vuorinen@vincit.fi>
2021-12-10 12:42:59 +0000

    #200 add returned to preparation validation

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationController.java
@@ -1,21 +1,6 @@
 package fi.hel.allu.supervision.api.controller;
 
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.validation.Valid;
-
-import org.apache.commons.lang3.BooleanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-
 import fi.hel.allu.common.domain.types.ApplicationTagType;
 import fi.hel.allu.common.domain.types.StatusType;
 import fi.hel.allu.common.exception.IllegalOperationException;
@@ -29,6 +14,18 @@ import fi.hel.allu.servicecore.service.CommentService;
 import fi.hel.allu.supervision.api.domain.DecisionInfo;
 import fi.hel.allu.supervision.api.validation.DecisionMakerValidator;
 import io.swagger.annotations.*;
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Common operations for all application types
@@ -38,14 +35,19 @@ import io.swagger.annotations.*;
 @Api(tags = "Applications")
 public class ApplicationController {
 
-  @Autowired
-  private ApplicationServiceComposer applicationServiceComposer;
-  @Autowired
-  private CommentService commentService;
-  @Autowired
-  private ChargeBasisService chargeBasisService;
-  @Autowired
-  private DecisionMakerValidator decisionMakerValidator;
+  private final ApplicationServiceComposer applicationServiceComposer;
+  private final CommentService commentService;
+  private final ChargeBasisService chargeBasisService;
+  private final DecisionMakerValidator decisionMakerValidator;
+
+
+  public ApplicationController(ApplicationServiceComposer applicationServiceComposer, CommentService commentService,
+                               ChargeBasisService chargeBasisService, DecisionMakerValidator decisionMakerValidator) {
+    this.applicationServiceComposer = applicationServiceComposer;
+    this.commentService = commentService;
+    this.chargeBasisService = chargeBasisService;
+    this.decisionMakerValidator = decisionMakerValidator;
+  }
 
   @InitBinder("decisionInfo")
   protected void initUpdateBinder(WebDataBinder binder) {
@@ -53,28 +55,28 @@ public class ApplicationController {
   }
 
   @ApiOperation(value = "Update application owner.",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
+    authorizations = @Authorization(value = "api_key"),
+    produces = "application/json"
   )
-  @ApiResponses( value = {
-      @ApiResponse(code = 200, message = "Application owner updated successfully"),
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Application owner updated successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/owner", method = RequestMethod.PUT)
+  @PutMapping(value = "/applications/{id}/owner")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> updateOwner(@PathVariable Integer id,
-      @ApiParam(value = "Id of the new owner") @RequestParam Integer ownerId) {
+                                          @ApiParam(value = "Id of the new owner") @RequestParam Integer ownerId) {
     applicationServiceComposer.updateApplicationOwner(ownerId, Collections.singletonList(id), true);
     return ResponseEntity.ok().build();
   }
 
   @ApiOperation(value = "Remove application owner.",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
+    authorizations = @Authorization(value = "api_key"),
+    produces = "application/json"
   )
-  @ApiResponses( value = {
-      @ApiResponse(code = 200, message = "Application owner removed successfully"),
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Application owner removed successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/owner", method = RequestMethod.DELETE)
+  @DeleteMapping(value = "/applications/{id}/owner")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> removeOwner(@PathVariable Integer id) {
     applicationServiceComposer.removeApplicationOwner(Collections.singletonList(id), true);
@@ -82,58 +84,57 @@ public class ApplicationController {
   }
 
   @ApiOperation(value = "Move application to handling. Allowed if current state is pending.",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
+    authorizations = @Authorization(value = "api_key"),
+    produces = "application/json"
   )
-  @ApiResponses( value = {
-      @ApiResponse(code = 200, message = "Application moved to handling successfully"),
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Application moved to handling successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/handling", method = RequestMethod.PUT)
+  @PutMapping(value = "/applications/{id}/handling"
+  )
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> moveToHandling(@PathVariable Integer id) {
-    validateApplicationStatus(id, StatusType.PENDING);
+    validateApplicationStatus(id, Collections.singletonList(StatusType.PENDING));
     applicationServiceComposer.changeStatus(id, StatusType.HANDLING);
     return ResponseEntity.ok().build();
   }
 
   @ApiOperation(value = "Cancel application. Allowed if current state is decision or earlier",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
+    authorizations = @Authorization(value = "api_key"),
+    produces = "application/json"
   )
-  @ApiResponses( value = {
-      @ApiResponse(code = 200, message = "Application cancelled successfully"),
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Application cancelled successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/cancelled", method = RequestMethod.PUT)
+  @PutMapping(value = "/applications/{id}/cancelled")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> cancel(@PathVariable Integer id) {
     List<StatusType> cancellationAllowedStatuses = Stream.of(StatusType.values())
-        .filter(s -> s.isBeforeDecision() || s == StatusType.DECISION)
-        .collect(Collectors.toList());
+      .filter(s -> s.isBeforeDecision() || s == StatusType.DECISION)
+      .collect(Collectors.toList());
     validateApplicationStatus(id, cancellationAllowedStatuses);
     applicationServiceComposer.changeStatus(id, StatusType.CANCELLED);
     return ResponseEntity.ok().build();
   }
 
-  @ApiOperation(value = "Move application to decision making. Allowed if current state is handling.",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
+  @ApiOperation(value = "Move application to decision making. " +
+    "Allowed if current state is handling or returned to preparation.",
+    authorizations = @Authorization(value = "api_key"),
+    produces = "application/json"
   )
-  @ApiResponses( value = {
-      @ApiResponse(code = 200, message = "Application moved to decision making successfully"),
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Application moved to decision making successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/decisionmaking", method = RequestMethod.PUT, consumes = "application/json")
+  @PutMapping(value = "/applications/{id}/decisionmaking", consumes = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> moveToDecisionMaking(@PathVariable Integer id, @RequestBody @Valid DecisionInfo decisionInfo) {
-    validateApplicationStatus(id, StatusType.HANDLING);
+    validateApplicationStatus(id,  Stream.of(StatusType.HANDLING, StatusType.RETURNED_TO_PREPARATION).collect(Collectors.toList()));
     validateDecisionMakingAllowed(id);
-    StatusChangeInfoJson statusChangeInfo = new StatusChangeInfoJson(CommentType.PROPOSE_APPROVAL, decisionInfo.getDecisionNote(), decisionInfo.getDecisionMakerId());
+    StatusChangeInfoJson statusChangeInfo = new StatusChangeInfoJson(
+      CommentType.PROPOSE_APPROVAL, decisionInfo.getDecisionNote(), decisionInfo.getDecisionMakerId());
     commentService.addDecisionProposalComment(id, statusChangeInfo);
     applicationServiceComposer.changeStatus(id, StatusType.DECISIONMAKING, statusChangeInfo);
     return ResponseEntity.ok().build();
-  }
-
-  private void validateApplicationStatus(Integer id, StatusType allowedStatus) {
-    validateApplicationStatus(id, Collections.singletonList(allowedStatus));
   }
 
   private void validateApplicationStatus(Integer id, List<StatusType> allowedStatuses) {
@@ -158,7 +159,7 @@ public class ApplicationController {
   private boolean hasValidInvoicing(Integer id) {
     ApplicationJson application = applicationServiceComposer.findApplicationById(id);
     return BooleanUtils.isTrue(application.getNotBillable())
-        || (application.getInvoiceRecipientId() != null && hasInvoicing(id));
+      || (application.getInvoiceRecipientId() != null && hasInvoicing(id));
   }
 
   private boolean hasInvoicing(Integer applicationId) {
@@ -168,7 +169,7 @@ public class ApplicationController {
   private boolean hasDecisionBlockingTags(Integer id) {
     return applicationServiceComposer.findTags(id)
       .stream()
-      .anyMatch(t -> blocksDecisionMaking(t));
+      .anyMatch(this::blocksDecisionMaking);
   }
 
   private boolean blocksDecisionMaking(ApplicationTagJson t) {
