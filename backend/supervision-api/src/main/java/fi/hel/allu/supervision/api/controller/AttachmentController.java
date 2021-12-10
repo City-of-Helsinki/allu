5c64bf660fc3242d2db3648c3ac6254849220055
Antti Vuorinen <antti.vuorinen@vincit.fi>
2021-12-10 13:04:53 +0000

    #152 add decision attachment to api

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/AttachmentController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/AttachmentController.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +22,11 @@ import io.swagger.annotations.*;
 @Api(tags = "Application attachments")
 public class AttachmentController {
 
-  @Autowired
-  private AttachmentService attachmentService;
+  private final AttachmentService attachmentService;
+
+  public AttachmentController(AttachmentService attachmentService) {
+    this.attachmentService = attachmentService;
+  }
 
   @ApiOperation(value = "Add new attachment for an application with given ID.",
       produces = "application/json",
@@ -35,7 +37,7 @@ public class AttachmentController {
       @ApiResponse(code = 200, message = "Attachment added successfully", response = AttachmentInfoJson.class),
       @ApiResponse(code = 400, message = "Invalid request data", response = ErrorInfo.class)
   })
-  @RequestMapping(value = "/applications/{id}/attachments", method = RequestMethod.POST)
+  @PostMapping(value = "/applications/{id}/attachments")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<AttachmentInfoJson> create(@ApiParam(value = "Application ID to add attachment for") @PathVariable Integer id,
                                      @ApiParam(value = "Attachment info in JSON", required = true) @Valid @RequestPart(value="metadata",required=true) AttachmentInfoJson metadata,
@@ -50,7 +52,7 @@ public class AttachmentController {
   @ApiResponses(value =  {
       @ApiResponse(code = 200, message = "Default attachments added successfully"),
   })
-  @RequestMapping(value = "/applications/{id}/attachments/default", method = RequestMethod.POST)
+  @PostMapping(value = "/applications/{id}/attachments/default")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> addDefaultAttachments(@ApiParam(value = "Application ID to add default attachment for") @PathVariable Integer id,
                                      @ApiParam(value = "Default attachment ID", required = true) @RequestBody List<Integer> defaultAttachmentIds) {
@@ -60,7 +62,7 @@ public class AttachmentController {
 
   @ApiOperation(value = "Delete attachment from application.",
       authorizations = @Authorization(value ="api_key"))
-  @RequestMapping(value = "/applications/{id}/attachments/{attachmentid}", method = RequestMethod.DELETE)
+  @DeleteMapping(value = "/applications/{id}/attachments/{attachmentid}")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Void> delete(@ApiParam(value = "Application ID") @PathVariable Integer id,
                                      @ApiParam(value = "Attachment ID to delete", required = true) @PathVariable(value = "attachmentid") Integer attachmentId) {
@@ -76,10 +78,28 @@ public class AttachmentController {
   @ApiResponses(value =  {
       @ApiResponse(code = 200, message = "Attachments fetched successfully", response = AttachmentInfoJson.class, responseContainer="List"),
   })
-  @RequestMapping(value = "/applications/{id}/attachments", method = RequestMethod.GET)
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
+  @GetMapping(value = "/applications/{id}/attachments")
   public ResponseEntity<List<AttachmentInfoJson>> getAttachments(@ApiParam(value = "Application ID to fetch attachments for") @PathVariable Integer id) {
     return ResponseEntity.ok(attachmentService.findAttachmentsForApplication(id));
+  }
+
+  @ApiOperation(value = "Update decision attachment",
+    produces = "application/json",
+    response = AttachmentInfoJson.class,
+    responseContainer = "List",
+    authorizations=@Authorization(value ="api_key"))
+  @ApiResponses(value =  {
+    @ApiResponse(code = 200, message = "Decision updated succesfully",
+      response = AttachmentInfoJson.class, responseContainer="List"),
+  })
+  @PutMapping(value = "/applications/{id}/decisionAttachment")
+  @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
+  public ResponseEntity<AttachmentInfoJson> updateDecisionAttachment(@ApiParam(value = "ID of updated attachment") @PathVariable Integer id,
+                                                                     @ApiParam(value = "New value for decision attachment") @RequestParam Boolean decisionAttachment) {
+    AttachmentInfoJson attachment = attachmentService.getAttachment(id);
+    attachment.setDecisionAttachment(decisionAttachment);
+   return ResponseEntity.ok(attachmentService.updateAttachment(id, attachment));
   }
 
   @ApiOperation(value = "Get attachment data for attachment with given ID.",
@@ -89,7 +109,7 @@ public class AttachmentController {
   @ApiResponses(value =  {
       @ApiResponse(code = 200, message = "Attachment data fetched successfully", response = byte.class, responseContainer="Array"),
   })
-  @RequestMapping(value = "/attachments/{attachmentId}/data", method = RequestMethod.GET)
+  @GetMapping(value = "/attachments/{attachmentId}/data")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
   public ResponseEntity<byte[]> getAttachmentData(@PathVariable int attachmentId) {
     AttachmentInfoJson info = attachmentService.getAttachment(attachmentId);
@@ -111,7 +131,7 @@ public class AttachmentController {
   @ApiResponses(value =  {
       @ApiResponse(code = 200, message = "Attachments fetched successfully", response = AttachmentInfoJson.class, responseContainer="List"),
   })
-  @RequestMapping(value = "/attachments/default/{applicationType}", method = RequestMethod.GET)
+  @GetMapping(value = "/attachments/default/{applicationType}")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
   public ResponseEntity<List<AttachmentInfoJson>> readAttachmentInfos(@PathVariable ApplicationType applicationType) {
     List<AttachmentInfoJson> result = attachmentService.getDefaultAttachmentsByApplicationType(applicationType).stream()
