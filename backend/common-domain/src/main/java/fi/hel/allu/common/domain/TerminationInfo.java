040e6e2b57291a42d487bf6b9475174105d59f60
Patrik Hillner <patrik.hillner@vincit.fi>
2022-07-13 11:14:14 +0300

    Revert unnecessary change for TerminationInfo

--- backend/common-domain/src/main/java/fi/hel/allu/common/domain/TerminationInfo.java
+++ backend/common-domain/src/main/java/fi/hel/allu/common/domain/TerminationInfo.java
@@ -1,16 +1,14 @@
 package fi.hel.allu.common.domain;
 
-
-
 import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
 
 public class TerminationInfo {
 
   private Integer id;
-
-
+  @NotNull
   private Integer applicationId;
   private ZonedDateTime creationTime;
   @NotNull
