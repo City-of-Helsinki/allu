ee559eaa473e032e8d90a9117d54faa1914c1e0c
Patrik Hillner <patrik.hillner@vincit.fi>
2022-07-18 11:03:14 +0300

    Move pdf stylesheets from deployment to backend

--- backend/copy_pdf_stylesheets_for_docker-compose.sh
+++ backend/copy_pdf_stylesheets_for_docker-compose.sh
@@ -2,7 +2,7 @@
 
 ## Fix stylesheet directories
 mkdir -p /home/allu/allu-backend/services/pdf-service
-cp -r ../deployment/roles/backend/files/pdf-stylesheets /home/allu/allu-backend/services/pdf-service/stylesheets
+cp -r pdf-service/stylesheets /home/allu/allu-backend/services/pdf-service/stylesheets
 chown -R 1214:7898 /home/allu/allu-backend
 chmod -R 744 /home/allu/allu-backend/*
 chmod 644 /home/allu/allu-backend/services/pdf-service/stylesheets/*
