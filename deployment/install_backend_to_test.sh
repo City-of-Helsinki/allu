23bde910bf0185c7757d8988358b85fa8487f95c
Patrik Hillner <patrik.hillner@vincit.fi>
2022-07-29 10:25:50 +0300

    Enable backend Docker container versioning
    Default "branch" is master, as it would be by
    default used for the test environment.
    Other envs will have the CD-pipeline provide
    the "branch" variable.

--- deployment/install_backend_to_test.sh
+++ deployment/install_backend_to_test.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 sh -e install_imageprep_to_test.sh dbservers
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa sftpserver.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa reporting_database.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa etl.yml --vault-password-file ~/allu_keys/vault_secret
+if [ -z $1 ]; then
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa backend.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa sftpserver.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa reporting_database.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa etl.yml --vault-password-file ~/allu_keys/vault_secret
+else
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa database.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa elasticsearch.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="branch="$1 backend.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa sftpserver.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa reporting_database.yml --vault-password-file ~/allu_keys/vault_secret
+  ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa etl.yml --vault-password-file ~/allu_keys/vault_secret
+
+fi
