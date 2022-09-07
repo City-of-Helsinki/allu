45ba1cf15126f7061147741bc5ad26123bfa13cd
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-07 10:06:03 +0300

    refactoring ApplicationHistoryController

--- backend/external-service/src/main/java/fi/hel/allu/external/api/controller/ApplicationHistoryController.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/ApplicationHistoryController.java
@@ -10,14 +10,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import fi.hel.allu.external.domain.ApplicationHistoryExt;
 import fi.hel.allu.external.domain.ApplicationHistorySearchExt;
@@ -29,8 +25,11 @@ import fi.hel.allu.external.service.ApplicationServiceExt;
 @Tag(name = "Application history")
 public class ApplicationHistoryController {
 
-  @Autowired
-  private ApplicationServiceExt applicationService;
+  private final ApplicationServiceExt applicationService;
+
+  public ApplicationHistoryController(ApplicationServiceExt applicationService) {
+    this.applicationService = applicationService;
+  }
 
   @Operation(summary = "Get Allu application history. Returns result containing application status changes and supervision events.")
   @ApiResponses(value = {
@@ -38,9 +37,10 @@ public class ApplicationHistoryController {
                   content = @Content(schema = @Schema(implementation = ApplicationHistoryExt.class))),
           @ApiResponse(responseCode = "404", description = "Data not found", content = @Content)
   })
-  @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+  @PostMapping( produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
-  public ResponseEntity<List<ApplicationHistoryExt>> searchApplicationHistory(@Parameter(description = "Application history search parameters.") @RequestBody ApplicationHistorySearchExt searchParameters) {
+  public ResponseEntity<List<ApplicationHistoryExt>> searchApplicationHistory(
+          @Parameter(description = "Application history search parameters.") @RequestBody ApplicationHistorySearchExt searchParameters) {
     return new ResponseEntity<>(applicationService.searchApplicationHistory(searchParameters), HttpStatus.OK);
   }
 
