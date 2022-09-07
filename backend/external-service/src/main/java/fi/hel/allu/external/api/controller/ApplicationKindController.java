266008f8966b7352d95f5d9b4c31e738648df68e
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-07 10:21:03 +0300

    remove deprecated rest path from ApplicationKindController

--- backend/external-service/src/main/java/fi/hel/allu/external/api/controller/ApplicationKindController.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/ApplicationKindController.java
@@ -1,7 +1,6 @@
 package fi.hel.allu.external.api.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -18,37 +17,12 @@ import org.springframework.web.bind.annotation.*;
 
 import fi.hel.allu.common.domain.types.ApplicationKind;
 import fi.hel.allu.common.domain.types.ApplicationType;
-import fi.hel.allu.external.config.ApplicationProperties;
 
 @RestController
 @SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Application kinds")
 public class ApplicationKindController {
 
-  private final ApplicationProperties applicationProperties;
-
-  public ApplicationKindController(ApplicationProperties applicationProperties) {
-    this.applicationProperties = applicationProperties;
-  }
-
-  @Operation(summary = "Get Allu application kinds")
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "Application kinds retrieved",
-                  content = @Content(schema = @Schema(implementation = ApplicationKind.class))),
-          @ApiResponse(responseCode = "404", description = "Application kinds not found", content = @Content)
-  })
-  @GetMapping(value = {"/v1/applicationkinds"}, produces = "application/json")
-  @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
-  @Deprecated
-  public ResponseEntity<List<ApplicationKind>> getAllSupportedByV1(
-    @Parameter(description = "Application type of the kinds to get", required = true)
-    @RequestParam ApplicationType applicationType) {
-    List<ApplicationKind> result = ApplicationKind.forApplicationType(applicationType)
-        .stream()
-        .filter(k -> !applicationProperties.getV1ExcludedApplicationKinds().contains(k.name()))
-        .collect(Collectors.toList());
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 
   @Operation(summary = "Get Allu application kinds")
   @ApiResponses(value = {
--- backend/external-service/src/main/java/fi/hel/allu/external/config/ApplicationProperties.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/config/ApplicationProperties.java
@@ -1,59 +1,50 @@
 package fi.hel.allu.external.config;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotEmpty;
+
 @Component
 public class ApplicationProperties {
 
-  private final String jwtSecret;
-  private final String serviceAuth;
-  private final Integer jwtExpirationTime;
-  private final List<String> v1ExcludedApplicationKinds;
+    private final String jwtSecret;
+    private final String serviceAuth;
+    private final Integer jwtExpirationTime;
 
-  @Autowired
-  public ApplicationProperties(@Value("${jwt.secret}") @NotEmpty String jwtSecret,
-      @Value("${service.authkey}") @NotEmpty String serviceAuth,
-      @Value("${jwt.expirationtime}") @NotEmpty Integer jwtExpirationTime,
-      @Value("#{'${v1.api.kinds.excluded:}'.split(',')}") List<String> v1ExcludedApplicationKinds) {
-    this.jwtSecret = jwtSecret;
-    this.serviceAuth = serviceAuth;
-    this.jwtExpirationTime = jwtExpirationTime;
-    this.v1ExcludedApplicationKinds = v1ExcludedApplicationKinds;
-  }
+    @Autowired
+    public ApplicationProperties(@Value("${jwt.secret}") @NotEmpty String jwtSecret,
+                                 @Value("${service.authkey}") @NotEmpty String serviceAuth,
+                                 @Value("${jwt.expirationtime}") @NotEmpty Integer jwtExpirationTime) {
+        this.jwtSecret = jwtSecret;
+        this.serviceAuth = serviceAuth;
+        this.jwtExpirationTime = jwtExpirationTime;
+    }
 
-  /**
-   * Returns JWT secret key used to sign tokens.
-   *
-   * @return  JWT secret key used to sign tokens.
-   */
-  public String getJwtSecret() {
-    return jwtSecret;
-  }
+    /**
+     * Returns JWT secret key used to sign tokens.
+     *
+     * @return JWT secret key used to sign tokens.
+     */
+    public String getJwtSecret() {
+        return jwtSecret;
+    }
 
-  /**
-   * Return the auth token for the service user
-   *
-   * @return auth token
-   */
-  public String getServiceAuth() {
-    return serviceAuth;
-  }
+    /**
+     * Return the auth token for the service user
+     *
+     * @return auth token
+     */
+    public String getServiceAuth() {
+        return serviceAuth;
+    }
 
-  /**
-   * Gets JWT expiration time in minutes
-   */
-  public Integer getJwtExpirationTime() {
-    return jwtExpirationTime;
-  }
-
-  public List<String> getV1ExcludedApplicationKinds() {
-    return v1ExcludedApplicationKinds != null ? v1ExcludedApplicationKinds : Collections.emptyList();
-  }
+    /**
+     * Gets JWT expiration time in minutes
+     */
+    public Integer getJwtExpirationTime() {
+        return jwtExpirationTime;
+    }
 
 }
