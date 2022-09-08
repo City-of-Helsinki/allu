629e65960b638632e1ddace0b1b92b57434b4035
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-08 10:14:36 +0300

    refactor InformationRequestController

--- backend/external-service/src/main/java/fi/hel/allu/external/api/controller/InformationRequestController.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/InformationRequestController.java
@@ -1,9 +1,11 @@
 package fi.hel.allu.external.api.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import fi.hel.allu.external.domain.FixedLocationExt;
+import fi.hel.allu.external.domain.InformationRequestExt;
+import fi.hel.allu.external.domain.InformationRequestFieldExt;
+import fi.hel.allu.external.service.ApplicationServiceExt;
+import fi.hel.allu.servicecore.domain.InformationRequestFieldJson;
+import fi.hel.allu.servicecore.domain.InformationRequestJson;
+import fi.hel.allu.servicecore.service.InformationRequestService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,21 +14,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import fi.hel.allu.external.domain.InformationRequestExt;
-import fi.hel.allu.external.domain.InformationRequestFieldExt;
-import fi.hel.allu.external.service.ApplicationServiceExt;
-import fi.hel.allu.servicecore.domain.InformationRequestFieldJson;
-import fi.hel.allu.servicecore.domain.InformationRequestJson;
-import fi.hel.allu.servicecore.service.InformationRequestService;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -35,37 +29,45 @@ import fi.hel.allu.servicecore.service.InformationRequestService;
 @Tag(name = "Information requests")
 public class InformationRequestController {
 
-  @Autowired
-  private InformationRequestService informationRequestService;
+		private final InformationRequestService informationRequestService;
 
-  @Autowired
-  private ApplicationServiceExt applicationService;
+		private final ApplicationServiceExt applicationService;
 
-  @Operation(summary = "Fetch open information request for given application.")
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "Information request retrieved successfully",
-                  content = @Content(schema = @Schema(implementation = InformationRequestExt.class))),
-          @ApiResponse(responseCode = "404", description = "No information request found for given application",
-                  content = @Content)
-  })
-  @RequestMapping(value = "/applications/{id}/informationrequests", method = RequestMethod.GET, produces = "application/json")
-  @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
-  public ResponseEntity<InformationRequestExt> findByApplicationId(@Parameter(description = "Application ID to get information request for") @PathVariable Integer id) {
-    Integer applicationId = applicationService.getApplicationIdForExternalId(id);
-    applicationService.validateOwnedByExternalUser(applicationId);
-    InformationRequestJson request = informationRequestService.findOpenByApplicationId(applicationId);
-    return new ResponseEntity<>(toInformationRequestExt(request, id), HttpStatus.OK);
-  }
+		public InformationRequestController(InformationRequestService informationRequestService,
+																				ApplicationServiceExt applicationService) {
+				this.informationRequestService = informationRequestService;
+				this.applicationService = applicationService;
+		}
 
-  private InformationRequestExt toInformationRequestExt(InformationRequestJson request, Integer externalApplicationId) {
-    if (request == null) {
-      return null;
-    }
-    return new InformationRequestExt(request.getId(), externalApplicationId, toInformationRequestExtFields(request.getFields()));
-  }
+		@Operation(summary = "Fetch open information request for given application.")
+		@ApiResponses(value = {
+						@ApiResponse(responseCode = "200", description = "Information request retrieved successfully",
+										content = @Content(schema = @Schema(implementation = InformationRequestExt.class))),
+						@ApiResponse(responseCode = "404", description = "No information request found for given application",
+										content = @Content)
+		})
+		@GetMapping(value = "/applications/{id}/informationrequests", produces = "application/json")
+		@PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
+		public ResponseEntity<InformationRequestExt> findByApplicationId(
+						@Parameter(description = "Application ID to get information request for") @PathVariable Integer id) {
+				Integer applicationId = applicationService.getApplicationIdForExternalId(id);
+				applicationService.validateOwnedByExternalUser(applicationId);
+				InformationRequestJson request = informationRequestService.findOpenByApplicationId(applicationId);
+				return new ResponseEntity<>(toInformationRequestExt(request, id), HttpStatus.OK);
+		}
 
-  private List<InformationRequestFieldExt> toInformationRequestExtFields(List<InformationRequestFieldJson> fields) {
-    return fields.stream().map(f -> new InformationRequestFieldExt(f.getFieldKey(), f.getDescription())).collect(Collectors.toList());
-  }
+		private InformationRequestExt toInformationRequestExt(InformationRequestJson request,
+																													Integer externalApplicationId) {
+				if (request == null) {
+						return null;
+				}
+				return new InformationRequestExt(request.getId(), externalApplicationId,
+																				 toInformationRequestExtFields(request.getFields()));
+		}
+
+		private List<InformationRequestFieldExt> toInformationRequestExtFields(List<InformationRequestFieldJson> fields) {
+				return fields.stream().map(f -> new InformationRequestFieldExt(f.getFieldKey(), f.getDescription()))
+								.collect(Collectors.toList());
+		}
 
 }
