562f53a5734c4a4e72ffbd8dad333ec4a4f42de7
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-08 10:21:59 +0300

    update swagger on NoteController

--- backend/external-service/src/main/java/fi/hel/allu/external/api/controller/NoteController.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/NoteController.java
@@ -2,6 +2,14 @@ package fi.hel.allu.external.api.controller;
 
 import javax.validation.Valid;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,11 +21,11 @@ import fi.hel.allu.external.domain.NoteExt;
 import fi.hel.allu.external.mapper.NoteExtMapper;
 import fi.hel.allu.external.validation.ApplicationExtGeometryValidator;
 import fi.hel.allu.servicecore.service.ApplicationServiceComposer;
-import io.swagger.annotations.*;
 
 @RestController
 @RequestMapping({"/v2/notes"})
-@Api(tags = "Notes")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Notes")
 public class NoteController {
 
   @Autowired
@@ -34,26 +42,24 @@ public class NoteController {
     binder.addValidators(geometryValidator);
   }
 
-  @ApiOperation(value = "Adds new note and returns ID of the created note. Applicant of note is set according to Allu configuration (default contact).",
-      produces = "application/json",
-      response = Integer.class,
-      authorizations=@Authorization(value ="api_key"))
+  @Operation(summary = "Adds new note and returns ID of the created note. Applicant of note is set according to Allu configuration (default contact).")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Note added successfully", response = Integer.class),
-      @ApiResponse(code = 400, message = "Invalid input data", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Note added successfully",
+              content = @Content(schema = @Schema(implementation = Integer.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid input data",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
-  @RequestMapping(method = RequestMethod.POST)
+  @RequestMapping(method = RequestMethod.POST, produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_INTERNAL')")
-  public ResponseEntity<Integer> create(@ApiParam(value = "Application data", required = true)
+  public ResponseEntity<Integer> create(@Parameter(description = "Application data", required = true)
                                         @Valid @RequestBody NoteExt noteExt) {
     Integer id = applicationServiceComposer.createApplication(noteMapper.mapExtNote(noteExt)).getId();
     return ResponseEntity.ok(id);
   }
 
-  @ApiOperation(value = "Delete note with given ID.",
-      authorizations=@Authorization(value ="api_key"))
+  @Operation(summary = "Delete note with given ID.")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Note deleted successfully")
+      @ApiResponse(responseCode = "200", description = "Note deleted successfully")
   })
   @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
   @PreAuthorize("hasAnyRole('ROLE_INTERNAL')")
