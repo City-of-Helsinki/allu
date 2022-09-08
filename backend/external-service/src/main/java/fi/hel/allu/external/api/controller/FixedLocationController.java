767e885443e75b70641f2f8a9bb0bcfc79e08c3c
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-08 10:03:59 +0300

    refactoring FixedLocationController

--- backend/external-service/src/main/java/fi/hel/allu/external/api/controller/FixedLocationController.java
+++ backend/external-service/src/main/java/fi/hel/allu/external/api/controller/FixedLocationController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,20 +27,23 @@ import fi.hel.allu.servicecore.service.LocationService;
 @Tag(name = "Fixed locations")
 public class FixedLocationController {
 
-  @Autowired
-  private LocationService locationService;
+  private final LocationService locationService;
+
+  public FixedLocationController(LocationService locationService) {
+    this.locationService = locationService;
+  }
 
   @Operation(description = "Get fixed locations")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "Approval document retrieved successfully",
+          @ApiResponse(responseCode = "200", description = "Fixed locations retrieved successfully",
                   content = @Content(schema = @Schema(implementation = FixedLocationExt.class))),
-          @ApiResponse(responseCode = "404", description = "No approval document found for given application",
+          @ApiResponse(responseCode = "404", description = "No fixed locations found",
                   content = @Content)
   })
-  @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+  @GetMapping(produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
   public ResponseEntity<List<FixedLocationExt>> getAll(@Parameter(description = "Application kind of the fixed locations to get", required = true)
-                                                       @RequestParam(required = true) ApplicationKind applicationKind,
+                                                       @RequestParam ApplicationKind applicationKind,
                                                        @Parameter(description = "Spatial reference system ID of the geometry.")
                                                        @RequestParam(required = false) Integer srId) {
     return new ResponseEntity<>(FixedLocationMapper.mapToExt(locationService.getFixedLocationList(applicationKind, srId)), HttpStatus.OK);
@@ -49,12 +51,12 @@ public class FixedLocationController {
 
   @Operation(description = "Get fixed location by ID")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "Approval document retrieved successfully",
+          @ApiResponse(responseCode = "200", description = "Fixed location retrieved successfully",
                   content = @Content(schema = @Schema(implementation = FixedLocationExt.class))),
-          @ApiResponse(responseCode = "404", description = "No approval document found for given application",
+          @ApiResponse(responseCode = "404", description = "No fixed locations found for given  ID",
                   content = @Content)
   })
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+  @GetMapping(value = "/{id}", produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_INTERNAL','ROLE_TRUSTED_PARTNER')")
   public ResponseEntity<FixedLocationExt> findById(@Parameter(description = "Id of the fixed location") @PathVariable Integer id,
                                                    @Parameter(description = "Spatial reference system ID of the geometry." )
