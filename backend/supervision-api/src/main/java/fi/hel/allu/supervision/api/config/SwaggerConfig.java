a579a4fa2d76f1d74ebf3b599c8047446d7b44dc
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-09 13:20:55 +0300

    updating messageConverter

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/config/SwaggerConfig.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/config/SwaggerConfig.java
@@ -1,13 +1,16 @@
 package fi.hel.allu.supervision.api.config;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.servlet.ServletContext;
-
-import org.geolatte.geom.Geometry;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import fi.hel.allu.servicecore.domain.ApplicationExtensionJson;
+import fi.hel.allu.servicecore.domain.ClientApplicationDataJson;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
@@ -15,132 +18,72 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.classmate.TypeResolver;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-
-import fi.hel.allu.servicecore.domain.*;
-import fi.hel.allu.supervision.api.domain.BaseApplication;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Configuration of Swagger i.e. the REST documentation tool.
  */
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
 
-  private static final Class<?>[] IGNORED_CLASSES = {
-      ApplicationExtensionJson.class,
-      Page.class,
-      ClientApplicationDataJson.class,
-      Sort.class
-  };
+    private static final Class<?>[] IGNORED_CLASSES = {
+            ApplicationExtensionJson.class,
+            Page.class,
+            ClientApplicationDataJson.class,
+            Sort.class
+    };
 
-  private static final String[] FILTERED_APPLICATION_FIELDS = { "extension", "metadataVersion",
-      "clientApplicationData", "externalOwnerId", "externalApplicationId", "invoicingChanged", "targetState" };
+    private static final String[] FILTERED_APPLICATION_FIELDS = {"extension", "metadataVersion",
+            "clientApplicationData", "externalOwnerId", "externalApplicationId", "invoicingChanged", "targetState"};
 
-  @Value("${supervision.api.basepath}")
-  private String apiBasePath;
 
-  @Autowired
-  private TypeResolver typeResolver;
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize("Allu REST"));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                                    new SecurityScheme()
+                                                            .name(securitySchemeName)
+                                                            .type(SecurityScheme.Type.HTTP)
+                                                            .scheme("bearer")
+                                                            .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info().title(apiTitle).version("1"));
+    }
 
-  @Bean
-  public Docket api(ServletContext servletContext) {
-      List<SecurityScheme> schemeList = Collections.singletonList(new ApiKey("api_key", "Authorization", "Bearer"));
 
-      return new Docket(DocumentationType.SWAGGER_2)
-         .directModelSubstitute(Geometry.class, String.class) // Prevent Swagger from generating documentation for third party geometry classes
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("fi.hel.allu.supervision.api"))
-        .paths(PathSelectors.any())
-        .build()
-        .pathProvider(
-            new RelativePathProvider(servletContext) {
-              @Override
-              public String getApplicationBasePath() {
-                return apiBasePath + super.getApplicationBasePath();
-              }
-            }
-        )
-        .securitySchemes(schemeList)
-        .apiInfo(apiInfo())
-        .tags(
-              new Tag("Applications", "API to search and manage applications"),
-              new Tag("Supervision tasks", "API to search and manage supervision tasks"),
-              new Tag("Projects", "Project API"),
-              new Tag("Authentication", "Authentication API"),
-              new Tag("Charge basis entries", "API to list and manage charge basis entries"),
-              new Tag("Invoices", "API to list application's invoices"),
-              new Tag("Users", "User API"),
-              new Tag("Codes", "API providing translations for Allu codes"),
-              new Tag("Application tags", "API to add and remove application tags"),
-              new Tag("Comments", "API to list and add/remove application comments"),
-              new Tag("Application attachments", "API to list and add application attachments"),
-              new Tag("City districts", "API to list city districts"),
-              new Tag("Customers", "API to search and manage customers"),
-              new Tag("Locations", "API to manage locations"),
-              new Tag("Payment classes", "API to fetch payment classes")
-              )
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Find jackson message converter and add custom object mapper
+        SimpleBeanPropertyFilter applicationFilter = SimpleBeanPropertyFilter
+                .serializeAllExcept(FILTERED_APPLICATION_FIELDS);
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+                                   SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS)
+                .filters(new SimpleFilterProvider().addFilter("applicationFilter", applicationFilter))
+                .build();
 
-        .additionalModels(
-            typeResolver.resolve(BaseApplication.class),
-            typeResolver.resolve(ExcavationAnnouncementJson.class),
-            typeResolver.resolve(AreaRentalJson.class),
-            typeResolver.resolve(CableReportJson.class),
-            typeResolver.resolve(PlacementContractJson.class),
-            typeResolver.resolve(ShortTermRentalJson.class),
-            typeResolver.resolve(TrafficArrangementJson.class),
-            typeResolver.resolve(EventJson.class),
-            typeResolver.resolve(NoteJson.class))
-        .ignoredParameterTypes(IGNORED_CLASSES);
-  }
+        Optional<HttpMessageConverter<?>> converter = converters.stream()
+                .filter(m -> m.getClass().isAssignableFrom(MappingJackson2HttpMessageConverter.class))
+                .findFirst();
+        if (converter.isPresent()) {
+            MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter.get();
+            jacksonConverter.setObjectMapper(mapper);
+        }
 
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-        "Allu Supervision REST API",
-        "Allu public interface.",
-        "1.0.0",
-        "TODO",
-        new Contact("TODO", "TODO", "TODO"),
-        "License (MIT)", "https://bitbucket.org/vincit/allu/raw/HEAD/LICENSE",
-        Collections.emptyList());
-  }
 
-  @Override
-  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    // Find jackson message converter and add custom object mapper
-    SimpleBeanPropertyFilter applicationFilter = SimpleBeanPropertyFilter
-        .serializeAllExcept(FILTERED_APPLICATION_FIELDS);
-    ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
-      .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS)
-      .filters(new SimpleFilterProvider().addFilter("applicationFilter", applicationFilter))
-      .build();
-    getMessageConverters().stream()
-        .filter(m -> m.getClass().isAssignableFrom(MappingJackson2HttpMessageConverter.class))
-        .findFirst()
-        .ifPresent(j -> ((MappingJackson2HttpMessageConverter)j).setObjectMapper(mapper));
-  }
+    }
 
-  @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/swagger-ui.html")
-              .addResourceLocations("classpath:/META-INF/resources/");
-
-      registry.addResourceHandler("/webjars/**")
-              .addResourceLocations("classpath:/META-INF/resources/webjars/");
-  }
 
 }
