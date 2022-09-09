d29c360789b3344d02f623e4c158d072615c3827
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-09 16:55:35 +0300

    update swagger on ApplicationReplacementController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationReplacementController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/ApplicationReplacementController.java
@@ -1,5 +1,12 @@
 package fi.hel.allu.supervision.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,21 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fi.hel.allu.common.exception.ErrorInfo;
 import fi.hel.allu.servicecore.service.ApplicationServiceComposer;
-import io.swagger.annotations.*;
 
 @RestController
 @RequestMapping("/v1/applications")
-@Api(tags = "Applications")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Applications")
 public class ApplicationReplacementController {
 
   @Autowired
   private ApplicationServiceComposer applicationServiceComposer;
 
-  @ApiOperation(value = "Creates replacing application for application with given ID and returns id of the replacing application",
-      authorizations = @Authorization(value = "api_key"))
+  @Operation(summary = "Creates replacing application for application with given ID and returns id of the replacing application")
   @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "Application replaced successfully"),
-      @ApiResponse(code = 400, message = "Application cannot be replaced", response = ErrorInfo.class), })
+      @ApiResponse(responseCode = "200", description = "Application replaced successfully"),
+      @ApiResponse(responseCode = "400", description = "Application cannot be replaced",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))})
   @RequestMapping(value = "/{id}/replace", method = RequestMethod.POST)
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<Integer> replaceApplication(@PathVariable Integer id) {
