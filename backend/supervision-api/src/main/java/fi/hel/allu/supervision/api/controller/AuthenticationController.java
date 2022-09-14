324dc8fa01a29a0b20c21c36d38bb2990e4c9d89
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-14 14:03:18 +0300

    refactor AuthenticationController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/AuthenticationController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/AuthenticationController.java
@@ -3,12 +3,8 @@ package fi.hel.allu.supervision.api.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import fi.hel.allu.supervision.api.security.TokenAuthenticationService;
 
@@ -17,11 +13,14 @@ import fi.hel.allu.supervision.api.security.TokenAuthenticationService;
 @Tag(name = "Authentication")
 public class AuthenticationController {
 
-  @Autowired
-  private TokenAuthenticationService tokenAuthenticationService;
+  private final TokenAuthenticationService tokenAuthenticationService;
+
+  public AuthenticationController(TokenAuthenticationService tokenAuthenticationService) {
+    this.tokenAuthenticationService = tokenAuthenticationService;
+  }
 
   @Operation(summary = "Authenticate with AD token")
-  @RequestMapping(method = RequestMethod.POST)
+  @PostMapping
   public ResponseEntity<String> login(@RequestParam @Parameter(description = "AD JWT", required = true) String adToken) {
     return ResponseEntity.ok(tokenAuthenticationService.loginWithAdToken(adToken));
   }
