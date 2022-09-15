daf5362c9a4f4f176e440b9b2279900c083793de
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-15 12:01:50 +0300

    update swagger on PaymentClassController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/PaymentClassController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/PaymentClassController.java
@@ -2,6 +2,10 @@ package fi.hel.allu.supervision.api.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,28 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 import fi.hel.allu.common.domain.types.ApplicationKind;
 import fi.hel.allu.common.domain.types.ApplicationType;
 import fi.hel.allu.servicecore.service.ApplicationService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.Authorization;
 
 @RestController
 @RequestMapping("/v1")
-@Api(tags = "Payment classes")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Payment classes")
 public class PaymentClassController {
 
   @Autowired
   private ApplicationService applicationService;
 
-  @ApiOperation(value = "Gets payment classes available for given application type and kind.",
-      authorizations = @Authorization(value ="api_key"),
-      produces = "application/json"
-  )
+  @Operation(summary = "Gets payment classes available for given application type and kind.")
   @RequestMapping(value = "/paymentclasses", method = RequestMethod.GET, produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<List<String>> getPaymentClasses(
-      @ApiParam(value = "Application type") @RequestParam(value = "type") ApplicationType type,
-      @ApiParam(value = "Application kind") @RequestParam(value = "kind") ApplicationKind kind) {
+      @Parameter(description = "Application type") @RequestParam(value = "type") ApplicationType type,
+      @Parameter(description = "Application kind") @RequestParam(value = "kind") ApplicationKind kind) {
     return ResponseEntity.ok(applicationService.getPricelistPaymentClasses(type, kind));
   }
 }
