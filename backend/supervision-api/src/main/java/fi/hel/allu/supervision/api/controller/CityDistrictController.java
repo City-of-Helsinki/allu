449ad67b09716e749ce8c8a1d0f938927f9797b0
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-15 08:49:05 +0300

    refactor CityDistrictController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/CityDistrictController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/CityDistrictController.java
@@ -1,20 +1,18 @@
 package fi.hel.allu.supervision.api.controller;
 
-import java.util.List;
-
+import fi.hel.allu.servicecore.domain.CityDistrictInfoJson;
+import fi.hel.allu.servicecore.service.LocationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fi.hel.allu.servicecore.domain.CityDistrictInfoJson;
-import fi.hel.allu.servicecore.service.LocationService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/citydistricts")
@@ -22,20 +20,23 @@ import fi.hel.allu.servicecore.service.LocationService;
 @Tag(name = "City districts")
 public class CityDistrictController {
 
-  @Autowired
-  private LocationService locationService;
+    private final LocationService locationService;
 
-  @Operation(summary = "List all city districts")
-  @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-  @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
-  public ResponseEntity<List<CityDistrictInfoJson>> getAllCityDistricts() {
-    return ResponseEntity.ok(locationService.getCityDistrictList());
-  }
+    public CityDistrictController(LocationService locationService) {
+        this.locationService = locationService;
+    }
 
-  @Operation(summary = "Get city district by id")
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-  @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
-  public ResponseEntity<CityDistrictInfoJson> getCityDistrictById(@PathVariable Integer id) {
-    return ResponseEntity.ok(locationService.getCityDistrictById(id));
-  }
+    @Operation(summary = "List all city districts")
+    @GetMapping(produces = "application/json")
+    @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
+    public ResponseEntity<List<CityDistrictInfoJson>> getAllCityDistricts() {
+        return ResponseEntity.ok(locationService.getCityDistrictList());
+    }
+
+    @Operation(summary = "Get city district by id")
+    @GetMapping(value = "/{id}", produces = "application/json")
+    @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
+    public ResponseEntity<CityDistrictInfoJson> getCityDistrictById(@PathVariable Integer id) {
+        return ResponseEntity.ok(locationService.getCityDistrictById(id));
+    }
 }
