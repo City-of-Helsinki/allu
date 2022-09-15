e6b083cc47d8b3949842fb8a064df66553f74393
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-15 09:43:32 +0300

    update swagger on CommentController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/CommentController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/CommentController.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,111 +21,100 @@ import fi.hel.allu.common.exception.ErrorInfo;
 import fi.hel.allu.servicecore.domain.CommentJson;
 import fi.hel.allu.servicecore.service.CommentService;
 import fi.hel.allu.supervision.api.domain.CommentCreateJson;
-import io.swagger.annotations.*;
 
 @RestController
 @RequestMapping("/v1")
-@Api(tags = "Comments")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Comments")
 public class CommentController {
 
   @Autowired
   private CommentService commentService;
 
-  @ApiOperation(value = "Add new comment for an application with given ID.",
-      notes = "User is allowed to add comments with following types:"
+  @Operation(summary = "Add new comment for an application with given ID.",
+          description = "User is allowed to add comments with following types:"
       + "<ul>"
       + " <li>INTERNAL</li>"
       + " <li>INVOICING</li>"
       + " <li>EXTERNAL_SYSTEM</li>"
-      + "</ul>",
-      produces = "application/json",
-      consumes = "application/json",
-      response = CommentJson.class,
-      authorizations = @Authorization(value ="api_key"))
+      + "</ul>")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comment added successfully", response = CommentJson.class),
-      @ApiResponse(code = 400, message = "Invalid request data", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Comment added successfully",
+               content = @Content(schema = @Schema(implementation = CommentJson.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request data",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
-  @RequestMapping(value = "/applications/{id}/comments", method = RequestMethod.POST)
+  @RequestMapping(value = "/applications/{id}/comments", method = RequestMethod.POST,  produces = "application/json", consumes = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<CommentJson> addApplicationComment(@PathVariable Integer id, @RequestBody @Valid CommentCreateJson comment) {
     return ResponseEntity.ok(commentService.addApplicationComment(id, new CommentJson(comment.getType(), comment.getText())));
   }
 
-  @ApiOperation(value = "Add new comment for an project with given ID.",
-      notes = "User is allowed to add comments with following types:"
+  @Operation(summary = "Add new comment for an project with given ID.",
+          description = "User is allowed to add comments with following types:"
       + "<ul>"
       + " <li>INTERNAL</li>"
       + " <li>INVOICING</li>"
       + " <li>EXTERNAL_SYSTEM</li>"
-      + "</ul>",
-      produces = "application/json",
-      consumes = "application/json",
-      response = CommentJson.class,
-      authorizations = @Authorization(value ="api_key"))
+      + "</ul>")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comment added successfully", response = CommentJson.class),
-      @ApiResponse(code = 400, message = "Invalid request data", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Comment added successfully",
+              content = @Content(schema = @Schema(implementation = CommentJson.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid request data",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
-  @RequestMapping(value = "/projects/{id}/comments", method = RequestMethod.POST)
+  @RequestMapping(value = "/projects/{id}/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
   public ResponseEntity<CommentJson> addProjectComment(@PathVariable Integer id, @RequestBody @Valid CommentCreateJson comment) {
     return ResponseEntity.ok(commentService.addProjectComment(id, new CommentJson(comment.getType(), comment.getText())));
   }
 
-  @ApiOperation(value = "Get all comments for application with given ID. ",
-      produces = "application/json",
-      response = CommentJson.class,
-      responseContainer = "List",
-      authorizations = @Authorization(value ="api_key"))
+  @Operation(summary = "Get all comments for application with given ID. ")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comments fetched successfully", response = CommentJson.class, responseContainer = "List")
+      @ApiResponse(responseCode = "200", description = "Comments fetched successfully",
+              content = @Content(schema = @Schema(implementation = CommentJson.class)))
   })
-  @RequestMapping(value = "/applications/{id}/comments", method = RequestMethod.GET)
+  @RequestMapping(value = "/applications/{id}/comments", method = RequestMethod.GET, produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
   public ResponseEntity<List<CommentJson>> findByApplication(@PathVariable Integer id) {
     return ResponseEntity.ok(commentService.findByApplicationId(id));
   }
 
-  @ApiOperation(value = "Get all comments for project with given ID. ",
-      produces = "application/json",
-      response = CommentJson.class,
-      responseContainer = "List",
-      authorizations = @Authorization(value ="api_key"))
+  @Operation(summary = "Get all comments for project with given ID. ")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comments fetched successfully", response = CommentJson.class, responseContainer = "List")
+      @ApiResponse(responseCode = "200", description = "Comments fetched successfully",
+              content = @Content(schema = @Schema(implementation = CommentJson.class)))
   })
-  @RequestMapping(value = "/projects/{id}/comments", method = RequestMethod.GET)
+  @RequestMapping(value = "/projects/{id}/comments", method = RequestMethod.GET, produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
   public ResponseEntity<List<CommentJson>> findByProject(@PathVariable Integer id) {
     return ResponseEntity.ok(commentService.findByProjectId(id));
   }
 
-  @ApiOperation(value = "Get comment by ID. ",
-      produces = "application/json",
-      response = CommentJson.class,
-      authorizations = @Authorization(value ="api_key"))
+  @Operation(summary = "Get comment by ID.")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comment fetched successfully", response = CommentJson.class),
-      @ApiResponse(code = 404, message = "Comment with given ID not found", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Comment fetched successfully",
+              content = @Content(schema = @Schema(implementation = CommentJson.class))),
+      @ApiResponse(responseCode = "404", description = "Comment with given ID not found",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.GET)
+  @RequestMapping(value = "/comments/{id}", method = RequestMethod.GET, produces = "application/json")
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE', 'ROLE_VIEW')")
   public ResponseEntity<CommentJson> findById(@PathVariable Integer id) {
     return ResponseEntity.ok(commentService.mapToJson(commentService.findById(id)));
   }
 
-  @ApiOperation(value = "Remove comment with given ID.",
-      notes = "User is allowed to remove <b>own</b> comments with following types:"
+  @Operation(summary = "Remove comment with given ID.",
+          description = "User is allowed to remove <b>own</b> comments with following types:"
       + "<ul>"
       + " <li>INTERNAL</li>"
       + " <li>INVOICING</li>"
       + " <li>EXTERNAL_SYSTEM</li>"
-      + "</ul>",
-      authorizations = @Authorization(value ="api_key"))
+      + "</ul>")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comment removed successfully"),
-      @ApiResponse(code = 400, message = "Invalid comment type", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Comment removed successfully"),
+      @ApiResponse(responseCode = "400", description = "Invalid comment type",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
   @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE)
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
@@ -128,12 +124,12 @@ public class CommentController {
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
-  @ApiOperation(value = "Update comment with given ID.",
-      notes = "User is allowed to update only <b>own</b> comments",
-      authorizations = @Authorization(value ="api_key"))
+  @Operation(summary = "Update comment with given ID.",
+      description = "User is allowed to update only <b>own</b> comments")
   @ApiResponses(value =  {
-      @ApiResponse(code = 200, message = "Comment updated successfully"),
-      @ApiResponse(code = 400, message = "Invalid comment type", response = ErrorInfo.class)
+      @ApiResponse(responseCode = "200", description = "Comment updated successfully"),
+      @ApiResponse(responseCode = "400", description = "Invalid comment type",
+              content = @Content(schema = @Schema(implementation = ErrorInfo.class)))
   })
   @RequestMapping(value = "/comments/{id}", method = RequestMethod.PUT)
   @PreAuthorize("hasAnyRole('ROLE_SUPERVISE')")
