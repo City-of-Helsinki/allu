32c8c6b0a32653c54642f6c2a83a6e9193610b4a
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-09-15 11:00:34 +0300

    update swagger on EventController

--- backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/EventController.java
+++ backend/supervision-api/src/main/java/fi/hel/allu/supervision/api/controller/EventController.java
@@ -9,7 +9,8 @@ import fi.hel.allu.servicecore.service.ChargeBasisService;
 import fi.hel.allu.servicecore.service.LocationService;
 import fi.hel.allu.supervision.api.mapper.ApplicationMapperCollector;
 import fi.hel.allu.supervision.api.service.ApplicationUpdateService;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import fi.hel.allu.common.domain.types.ApplicationType;
 import fi.hel.allu.servicecore.domain.ApplicationJson;
 import fi.hel.allu.supervision.api.domain.EventApplication;
-import io.swagger.annotations.Api;
 
 @RestController
 @RequestMapping("/v1/events")
-@Api(tags = "Applications")
+@Tag(name = "Applications")
 public class EventController extends BaseApplicationDetailsController<EventApplication, CreateEventApplicationJson> {
 
   public EventController(ApprovalDocumentService approvalDocumentService,
@@ -50,14 +50,14 @@ public class EventController extends BaseApplicationDetailsController<EventAppli
   @Override
   @RequestMapping(value = "/{applicationId}/applicant", method = RequestMethod.PUT, produces = "application/json")
   public ResponseEntity<CustomerWithContactsJson> updateCustomerApplicant(@PathVariable Integer applicationId,
-                                                                          @RequestBody @ApiParam("The new customer with contacts") CreateCustomerWithContactsJson customer) {
+                                                                          @RequestBody @Parameter(description = "The new customer with contacts") CreateCustomerWithContactsJson customer) {
     return super.updateCustomerApplicant(applicationId, customer);
   }
 
   @Override
   @RequestMapping(value = "/{applicationId}/representative", method = RequestMethod.PUT, produces = "application/json")
   public ResponseEntity<CustomerWithContactsJson> updateCustomerRepresentative(@PathVariable Integer applicationId,
-                                                                               @RequestBody @ApiParam("The new customer with contacts") CreateCustomerWithContactsJson customer) {
+                                                                               @RequestBody @Parameter(description = "The new customer with contacts") CreateCustomerWithContactsJson customer) {
     return super.updateCustomerRepresentative(applicationId, customer);
   }
 
