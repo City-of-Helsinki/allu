c4034b07da166638c5b1343673b89be865dc709b
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-10-07 11:11:58 +0300

    change jwt libraty
    -needed to change used jwt library. Maintenance of old was stopped.
    -spring-security-jw -> nimbus-jose-jwt

--- backend/service-core/src/main/java/fi/hel/allu/servicecore/security/JwksService.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/security/JwksService.java
@@ -1,31 +1,37 @@
 package fi.hel.allu.servicecore.security;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.PublicKey;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.jwt.JwtHelper;
-import org.springframework.stereotype.Service;
-
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkException;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.UrlJwkProvider;
+import com.nimbusds.jwt.SignedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.PublicKey;
+import java.text.ParseException;
 
 @Service
 public class JwksService {
 
-  @Lazy
-  @Autowired
-  private AdAuthenticationProperties properties;
+    @Lazy
+    @Autowired
+    private AdAuthenticationProperties properties;
 
 
-  public PublicKey getAdPublicKey(String accessToken) throws MalformedURLException, JwkException {
-    JwkProvider provider = new UrlJwkProvider(new URL(properties.getOauth2JwksUri()));
-    String kid = JwtHelper.headers(accessToken).get("kid");
-    Jwk jwk = provider.get(kid);
-    return jwk.getPublicKey();
-  }
+    public PublicKey getAdPublicKey(String accessToken) throws MalformedURLException, JwkException {
+        JwkProvider provider = new UrlJwkProvider(new URL(properties.getOauth2JwksUri()));
+        SignedJWT signedJWT;
+        try {
+            signedJWT = SignedJWT.parse(accessToken);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        String kid = signedJWT.getHeader().getKeyID();
+        Jwk jwk = provider.get(kid);
+        return jwk.getPublicKey();
+    }
 }
