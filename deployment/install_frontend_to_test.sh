8fcc6c413c85a8e438f5c5680f9111426c084337
Patrik Hillner <patrik.hillner@vincit.fi>
2022-10-13 13:56:15 +0300

    Push frontend Docker image to registry

--- deployment/install_frontend_to_test.sh
+++ deployment/install_frontend_to_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sh -e install_imageprep_to_test.sh webservers
 ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa docker_network/web_docker_network.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa frontend.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i test.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" frontend.yml --vault-password-file ~/allu_keys/vault_secret
