bc731cd5551495703a4bea33c69923da501968f6
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-12-27 12:13:54 +0200

    update pdf service controller annotations

--- backend/pdf-service/src/main/java/fi/hel/allu/pdfcreator/controller/PdfController.java
+++ backend/pdf-service/src/main/java/fi/hel/allu/pdfcreator/controller/PdfController.java
@@ -1,21 +1,18 @@
 package fi.hel.allu.pdfcreator.controller;
 
-import java.io.IOException;
-
-import javax.xml.transform.TransformerException;
-
+import fi.hel.allu.pdfcreator.service.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import fi.hel.allu.pdfcreator.service.PdfService;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
 @RestController
 public class PdfController {
@@ -38,7 +35,7 @@ public class PdfController {
    * @throws IOException
    * @throws TransformerException
    */
-  @RequestMapping(value = "/generate", method = RequestMethod.POST)
+  @PostMapping(value = "/generate")
   public ResponseEntity<byte[]> generatePdf(@RequestParam String stylesheet, @RequestBody String contents)
       throws IOException, TransformerException {
       HttpHeaders httpHeaders = new HttpHeaders();
