51a2377161d80a913943be16d6564bb930e1fb6c
Antti Vuorinen <antti.vuorinen@vincit.fi>
2023-01-18 16:46:45 +0200

    Checking after transformation of geometry that it is inside the boundaries
    Geometry points will show infinite if coordinates are outside of boundary so checking if one coordinate is finite we make sure that there is no problem with transformation. Empty geometry is okay because that does not crash other programs

--- backend/model-service/src/main/java/fi/hel/allu/model/coordinates/CoordinateTransformation.java
+++ backend/model-service/src/main/java/fi/hel/allu/model/coordinates/CoordinateTransformation.java
@@ -1,11 +1,10 @@
 package fi.hel.allu.model.coordinates;
 
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.sql.SQLQueryFactory;
 import org.geolatte.geom.Geometry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.sql.SQLQueryFactory;
 
 @Repository
 public class CoordinateTransformation {
@@ -17,8 +16,11 @@ public class CoordinateTransformation {
     if (geometry == null || geometry.isEmpty() || targetReferenceSystemId == null || targetReferenceSystemId.intValue() == geometry.getSRID()) {
       return geometry;
     }
-    return queryFactory
-        .select(Expressions.simpleTemplate(Geometry.class, "st_transform({0}, {1})", geometry, targetReferenceSystemId))
-        .fetchFirst();
+    Geometry transformedCoord = queryFactory
+            .select(Expressions.simpleTemplate(Geometry.class, "st_transform({0}, {1})", geometry,
+                                               targetReferenceSystemId))
+            .fetchFirst();
+    // Spring jackson binds can't handle infinite mark when converting rest call between containers
+    return Double.isFinite(transformedCoord.getPoints().getX(0)) ? transformedCoord : null;
   }
 }
