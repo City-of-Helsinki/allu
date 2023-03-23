2d7298a544ee41674489b3c2244c4784a94d6146
Antti Vuorinen <antti.vuorinen@vincit.fi>
2023-03-23 16:43:45 +0200

    fixing metada linking of GEO_HEATING enum

--- backend/model-service/src/main/resources/db/migration/V183__add_attribute_metadata.sql
+++ backend/model-service/src/main/resources/db/migration/V183__add_attribute_metadata.sql
@@ -1,2 +1,4 @@
 INSERT INTO allu.attribute_meta (structure_meta_id, name, ui_name, data_type)
-VALUES (currval('allu.structure_meta_id_seq'),  'GEO_HEATING', 'Maalämpö', 'ENUM_VALUE');
+VALUES (4,  'GEO_HEATING', 'Maalämpö', 'ENUM_VALUE');
+INSERT INTO allu.attribute_meta (structure_meta_id, name, ui_name, data_type)
+VALUES (8,  'GEO_HEATING', 'Maalämpö', 'ENUM_VALUE');
