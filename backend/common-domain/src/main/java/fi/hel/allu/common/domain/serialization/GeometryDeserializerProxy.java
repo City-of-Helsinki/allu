566aa614d4c37b9f604ace376ff42cd7866f6b65
Antti Vuorinen <antti.vuorinen@vincit.fi>
2023-03-24 15:01:14 +0200

    updating geolatte-common
    needed to upda also jackson-mapper-asl to jackson-databing. Because older one could not handel the change of geolatte library. Updated unit tests also because used it to locate problem.

--- backend/common-domain/src/main/java/fi/hel/allu/common/domain/serialization/GeometryDeserializerProxy.java
+++ backend/common-domain/src/main/java/fi/hel/allu/common/domain/serialization/GeometryDeserializerProxy.java
@@ -1,10 +1,8 @@
 package fi.hel.allu.common.domain.serialization;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import org.codehaus.jackson.*;
 import org.geolatte.common.dataformats.json.jackson.GeometryDeserializer;
 import org.geolatte.common.dataformats.json.jackson.JsonMapper;
 import org.geolatte.geom.Geometry;
@@ -15,25 +13,24 @@ import java.math.BigInteger;
 
 public class GeometryDeserializerProxy extends JsonDeserializer<Geometry> {
 
-  private JsonMapper jsonMapper = new JsonMapper();
-  private GeometryDeserializer<Geometry> proxy = new GeometryDeserializer<>(jsonMapper, Geometry.class);
+  private final JsonMapper jsonMapper = new JsonMapper();
+  private final GeometryDeserializer<Geometry> proxy = new GeometryDeserializer<>(jsonMapper, Geometry.class);
 
   @Override
-  public Geometry deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-    Geometry geo = proxy.deserialize(new JsonParserAdaptor(p), null);
-    return geo;
+  public Geometry deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    return proxy.deserialize(p, null);
   }
 
-  private class JsonParserAdaptor extends org.codehaus.jackson.JsonParser {
+  private class JsonParserAdaptor extends JsonParser {
 
-    private JsonParser parser;
+    private final JsonParser parser;
 
     public JsonParserAdaptor(JsonParser parser) {
       this.parser = parser;
     }
 
     @Override
-    public <T> T readValueAs(Class<T> valueType) throws IOException, JsonProcessingException {
+    public <T> T readValueAs(Class<T> valueType) throws IOException {
       return parser.readValueAs(valueType);
     }
 
@@ -44,8 +41,13 @@ public class GeometryDeserializerProxy extends JsonDeserializer<Geometry> {
     }
 
     @Override
-    public void setCodec(ObjectCodec c) {
+    public void setCodec(ObjectCodec objectCodec) {
       throw new UnsupportedOperationException("Not implemented in the adapter class");
+    }
+
+    @Override
+    public Version version() {
+      return null;
     }
 
     @Override
@@ -59,8 +61,53 @@ public class GeometryDeserializerProxy extends JsonDeserializer<Geometry> {
     }
 
     @Override
-    public org.codehaus.jackson.JsonParser skipChildren() throws IOException, JsonParseException {
+    public JsonToken nextValue() throws IOException {
+      return null;
+    }
+
+    @Override
+    public JsonParser skipChildren() throws IOException, JsonParseException {
       throw new UnsupportedOperationException("Not implemented in the adapter class");
+    }
+
+    @Override
+    public JsonToken getCurrentToken() {
+      return null;
+    }
+
+    @Override
+    public int getCurrentTokenId() {
+      return 0;
+    }
+
+    @Override
+    public boolean hasCurrentToken() {
+      return false;
+    }
+
+    @Override
+    public boolean hasTokenId(int i) {
+      return false;
+    }
+
+    @Override
+    public boolean hasToken(JsonToken jsonToken) {
+      return false;
+    }
+
+    @Override
+    public void clearCurrentToken() {
+
+    }
+
+    @Override
+    public JsonToken getLastClearedToken() {
+      return null;
+    }
+
+    @Override
+    public void overrideCurrentName(String s) {
+
     }
 
     @Override
@@ -109,6 +156,11 @@ public class GeometryDeserializerProxy extends JsonDeserializer<Geometry> {
     }
 
     @Override
+    public boolean hasTextCharacters() {
+      return false;
+    }
+
+    @Override
     public Number getNumberValue() throws IOException, JsonParseException {
       throw new UnsupportedOperationException("Not implemented in the adapter class");
     }
@@ -151,6 +203,11 @@ public class GeometryDeserializerProxy extends JsonDeserializer<Geometry> {
     @Override
     public byte[] getBinaryValue(Base64Variant b64variant) throws IOException, JsonParseException {
       throw new UnsupportedOperationException("Not implemented in the adapter class");
+    }
+
+    @Override
+    public String getValueAsString(String s) throws IOException {
+      return null;
     }
 
   }
