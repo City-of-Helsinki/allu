e6e263ed69b7907658e43c1fc70787f894cc06a2
Patrik Hillner <patrik.hillner@vincit.fi>
2023-05-02 10:41:13 +0300

    Fix order of production deployment
    elasticsearch_deploy.yml uses searchservers,
    which is the same as backendserver endpoint.
    Move yml script after backend network has been
    started.

--- deployment/deploy_all_to_production.sh
+++ deployment/deploy_all_to_production.sh
@@ -7,8 +7,8 @@ sh -e install_imageprep_to_production.sh webservers
 
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa docker_network/db_docker_network.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" database_deploy.yml --vault-password-file ~/allu_keys/vault_secret
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" elasticsearch_deploy.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa docker_network/backend_docker_network.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" elasticsearch_deploy.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" backend_deploy.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa docker_network/web_docker_network.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" frontend_deploy.yml --vault-password-file ~/allu_keys/vault_secret
