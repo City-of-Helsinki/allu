afaac4d5074f49513311013ee1525f87b0680c95
Antti Vuorinen <antti.vuorinen@vincit.fi>
2023-06-01 12:37:10 +0300

    updating SFTP connection
    removing old jsch and vfs2. Both are old and not updated. Changed to use forked version of jsch that is frquently updated.
    
    SFTP servers use some old algorithms. Needed to add them on configuration. Added them on properties file so they can be changed on production if situation changes.

--- backend/scheduler-service/src/main/java/fi/hel/allu/scheduler/service/SftpService.java
+++ backend/scheduler-service/src/main/java/fi/hel/allu/scheduler/service/SftpService.java
@@ -1,61 +1,57 @@
 package fi.hel.allu.scheduler.service;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.apache.commons.vfs2.AllFileSelector;
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.FileSystemOptions;
-import org.apache.commons.vfs2.Selectors;
-import org.apache.commons.vfs2.impl.StandardFileSystemManager;
-import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
+import com.jcraft.jsch.*;
+import fi.hel.allu.scheduler.domain.SFTPSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @Service
 public class SftpService {
 
-  private static final Integer SFTP_TIMEOUT = Integer.valueOf(10000);
   private static final Logger logger = LoggerFactory.getLogger(SftpService.class);
-
-  private  FileSystemOptions sftpOptions;
-  private StandardFileSystemManager manager;
 
   /**
    * Uploads all files from given local directory to SFTP server directory. Moves
    * uploaded files to local archive directory.
    *
-   * @param host SFTP server host
-   * @param port SFTP port
-   * @param user SFTP username
-   * @param password SFTP password
+   * @param sftpSettings needed settings to create sftp connection
    * @param localDirectory Local source directory
    * @param localArchiveDirectory directory where to move files on local machine
    *        after successful upload
    * @param remoteDirectory Target directory on remote server
    * @return true if files uploaded successfully; otherwise, false
    */
-  public boolean uploadFiles(String host, int port, String user, String password, String localDirectory, String localArchiveDirectory,
-      String remoteDirectory) {
+  public boolean uploadFiles(SFTPSettings sftpSettings, String localDirectory, String localArchiveDirectory,
+                             String remoteDirectory) {
+    logger.info("Start uploading sftp");
     try {
-      initialize();
-      FileObject localDirectoryObject  = createLocalDirectoryObject(localDirectory);
-      FileObject localArchiveDirectoryObject = createLocalDirectoryObject(localArchiveDirectory);
-      FileObject remoteDirectoryObject = createRemoteDirectoryObject(host, port, user, password, remoteDirectory);
-      moveFiles(localDirectoryObject, remoteDirectoryObject, localArchiveDirectoryObject);
-    } catch (IOException | URISyntaxException ex) {
-      logger.warn("Failed to upload files.", ex);
+      ChannelSftp channelSftp = createSession(sftpSettings);
+      File sourceDir = new File(localDirectory);
+      for (File file : Objects.requireNonNull(sourceDir.listFiles())){
+        if(file.isFile()) {
+          channelSftp.put(file.getAbsolutePath(), remoteDirectory + "/" + file.getName());
+          boolean renaming = file.renameTo(new File(localArchiveDirectory + "/" + file.getName()));
+          if (!renaming) {
+            logger.warn("renaming local file failed");
+          }
+        }
+      }
+      channelSftp.exit();
+    }  catch (JSchException e) {
+      logger.error("Failed jsch", e);
       return false;
-    }
-    finally {
-      manager.close();
+    } catch (SftpException e) {
+      logger.error("Failed connection", e);
+      return false;
+    }finally {
+      logger.info("Uploading file through SFTP ended");
     }
     return true;
   }
@@ -64,98 +60,53 @@ public class SftpService {
    * Downloads all files from given SFTP server directory. Moves downloaded files
    * to SFTP server's archive directory
    *
-   * @param host SFTP server host
-   * @param port SFTP port
-   * @param user SFTP username
-   * @param password SFTP password
+   * @param sftpSettings needed settings to create sftp connection
    * @param remoteDirectory Directory in SFTP server where to download from
    * @param remoteArchiveDirectory Archive directory where to move files on
    *        server after successful download
    * @param localDirectory Local target directory
    * @return true if files downloaded successfully; otherwise, false
    */
-  public boolean downloadFiles(String host, int port, String user, String password, String remoteDirectory, String remoteArchiveDirectory,
-      String localDirectory) {
+  public boolean downloadFiles(SFTPSettings sftpSettings, String remoteDirectory, String remoteArchiveDirectory,
+                               String localDirectory) {
+    logger.info("Start downloading sftp");
     try {
-      initialize();
-      FileObject remoteDirectoryObject = createRemoteDirectoryObject(host, port, user, password, remoteDirectory);
-      FileObject remoteArchiveDirectoryObject = createRemoteDirectoryObject(host, port, user, password, remoteArchiveDirectory);
-      FileObject localDirectoryObject  = createLocalDirectoryObject(localDirectory);
-      moveFiles(remoteDirectoryObject, localDirectoryObject, remoteArchiveDirectoryObject);
-    } catch (IOException | URISyntaxException ex) {
-      logger.warn("Failed to download files.", ex);
-    }
-    finally {
-      manager.close();
+      ChannelSftp channelSftp = createSession(sftpSettings);
+      List<String> list = channelSftp.ls(remoteDirectory).stream()
+          .filter(e -> !e.getAttrs().isDir())
+          .map(ChannelSftp.LsEntry::getFilename)
+          .collect(Collectors.toList());
+      for (String file : list){
+        channelSftp.get(remoteDirectory+file, localDirectory+"/"+file);
+        channelSftp.rename(channelSftp.getHome()+file, channelSftp.getHome() + remoteArchiveDirectory
+            + "/" + file);
+      }
+      channelSftp.exit();
+    }  catch (JSchException e) {
+      logger.error("Failed jsch", e);
+      return false;
+    } catch (SftpException e) {
+      logger.error("Failed connection", e);
+      return false;
+    }finally {
+      logger.info("Downloading file through SFTP ended");
     }
     return true;
   }
 
-  private void initialize() throws FileSystemException {
-    manager = new StandardFileSystemManager();
-    manager.init();
-    initializeSftpOptions();
-  }
-
-  /**
-   * Copy files from given source directory to target directory. After file is copied
-   * moves file from source directory to given archive directory.
-   */
-  private void moveFiles(FileObject sourceDirectory, FileObject targetDirectory, FileObject archiveDirectory) throws IOException {
-    List<FileObject> files = Arrays.asList(sourceDirectory.getChildren()).stream().filter(f -> isFile(f)).collect(Collectors.toList());
-    for (FileObject file : files) {
-      FileObject targetFile = manager.resolveFile(targetDirectory.getName().getURI() + "/" + file.getName().getBaseName());
-      targetFile.copyFrom(file, Selectors.SELECT_SELF);
-      archiveFile(file, archiveDirectory);
-    }
-  }
-
-  private void archiveFile(FileObject file, FileObject archiveDirectory) throws FileSystemException {
-    FileObject targetFile = manager.resolveFile(archiveDirectory.getName().getURI() + "/" + file.getName().getBaseName());
-    targetFile.copyFrom(file, new AllFileSelector());
-    file.delete();
-  }
-
-  private boolean isFile(FileObject file) {
-    try {
-      return file.isFile();
-    } catch (FileSystemException ex) {
-      logger.warn("Error occurred when processing file {}.", file.getName().getBaseName(), ex);
-      return false;
-    }
-  }
-
-  private FileObject createLocalDirectoryObject(String localDirectory) throws IOException {
-    FileObject localDirectoryObject = manager.resolveFile(localDirectory);
-    if (!directoryExists(localDirectoryObject)) {
-      throw new FileNotFoundException("Local directory not found");
-    }
-    return localDirectoryObject;
-  }
-
-  private FileObject createRemoteDirectoryObject(String host, int port, String user, String password,
-      String directory) throws IOException, URISyntaxException {
-    String connectionString = buildConnectionString(host, port, user, password, directory);
-    FileObject remoteDirectoryObject = manager.resolveFile(connectionString, sftpOptions);
-    if (!directoryExists(remoteDirectoryObject)) {
-      throw new FileNotFoundException("Remote directory not found");
-    }
-    return remoteDirectoryObject;
-  }
-
-  private boolean directoryExists(FileObject remoteDirectoryObject) throws FileSystemException {
-    return remoteDirectoryObject.exists() && remoteDirectoryObject.isFolder();
-  }
-
-  private String buildConnectionString(String host, int port, String user, String password, String remoteDirectory) throws URISyntaxException {
-    return new URI("sftp", user + ":" + password, host, port, remoteDirectory, null, null).toString();
-  }
-
-  private void initializeSftpOptions() throws FileSystemException {
-    sftpOptions = new FileSystemOptions();
-    SftpFileSystemConfigBuilder configBuilder = SftpFileSystemConfigBuilder.getInstance();
-    configBuilder.setStrictHostKeyChecking(sftpOptions, "no");
-    configBuilder.setUserDirIsRoot(sftpOptions, true);
-    configBuilder.setTimeout(sftpOptions, SFTP_TIMEOUT);
+  private ChannelSftp createSession(SFTPSettings sftpSettings) throws JSchException {
+    JSch jsch = new JSch();
+    jsch.setKnownHosts(new ByteArrayInputStream(sftpSettings.getKnownHosts().getBytes()));
+    Session jschSession = jsch.getSession(sftpSettings.getUser(), sftpSettings.getHost(), sftpSettings.getPort());
+    jschSession.setConfig("server_host_key", jschSession.getConfig("server_host_key") + sftpSettings.getSignatureAlgorithm());
+    jschSession.setConfig("PubkeyAcceptedAlgorithms", jschSession.getConfig("PubkeyAcceptedAlgorithms") + sftpSettings.getSignatureAlgorithm());
+    jschSession.setConfig("kex", jschSession.getConfig("kex") + sftpSettings.getKeyAlgorithm());
+    jschSession.setPassword(sftpSettings.getPassword());
+    jschSession.setTimeout(sftpSettings.getTimeout());
+    jschSession.connect();
+    logger.info("Is connected: {}", jschSession.isConnected());
+    ChannelSftp channelSftp = (ChannelSftp) jschSession.openChannel("sftp");
+    channelSftp.connect();
+    return channelSftp;
   }
 }
