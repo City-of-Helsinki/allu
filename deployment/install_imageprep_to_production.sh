1d53a9cbeff4f1e953c85ea330a10fb93b79fa4d
Patrik Hillner <patrik.hillner@vincit.fi>
2023-11-15 10:20:49 +0200

    Change deployment_user to jenkins
    As jenkins is the one setting up the environment,
    it's only natural that jenkins user is to run
    imageprep to servers.
    Fix usersetup task name.

--- deployment/install_imageprep_to_production.sh
+++ deployment/install_imageprep_to_production.sh
@@ -3,5 +3,5 @@ if [ -z $1 ]; then
   echo "Usage: $0 <limit_hosts>"
   echo "For example: $0 dbservers"
 else
-    ansible-playbook -i production.inventory imageprep.yml --limit $1 --private-key $HOME/allu_keys/allu_id_rsa --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub deployment_user=allu"
+  ansible-playbook -i production.inventory imageprep.yml --limit $1 --private-key $HOME/allu_keys/allu_id_rsa --vault-password-file ~/allu_keys/vault_secret --extra-vars "root_public_key=$HOME/allu_keys/allu_id_rsa.pub allu_public_key=$HOME/allu_keys/allu_id_rsa.pub deployment_user=jenkins"
 fi
