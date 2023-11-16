13d31a7abe5cdbd5a81ab8e5059ad484acc3c2dd
Patrik Hillner <patrik.hillner@vincit.fi>
2023-11-16 15:50:37 +0200

    Remove unused Ansible build and deployment scripts

--- deployment/roles/backend/templates/run_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xmx{{ allu_service_memory_limit[item] | default('256m') }} -Dservice.home=/home/allu/{{ item }} -jar /home/allu/{{ item }}/{{ item }}.jar --spring.config.additional-location=file:/home/allu/{{ item }}/{{ item }}.properties
--- deployment/roles/frontend/files/frontend_docker/apache_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# remove pid file, which may prevent Apache to start during Docker container restart
-rm -rf /var/run/apache2/apache2.pid
-apachectl -DFOREGROUND
