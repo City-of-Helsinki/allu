6c53be7754606c7cc6ff936caaee93b8e5371d94
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2024-07-04 02:08:09 +0300

    Make HighLevelRestClientSockets have timeout of 10 minutes and use TCP keep alive

--- backend/search-service/src/main/java/fi/hel/allu/search/config/AppConfig.java
+++ backend/search-service/src/main/java/fi/hel/allu/search/config/AppConfig.java
@@ -3,6 +3,8 @@ package fi.hel.allu.search.config;
 import fi.hel.allu.common.controller.handler.ControllerExceptionHandlerConfig;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -34,9 +36,14 @@ public class AppConfig {
                             public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder builder) {
                                 return builder
                                         .setConnectTimeout(5000)
-                                        .setSocketTimeout(80000);
+                                        .setSocketTimeout(600000);
                             }
-                        }));
+                        }).setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                            .setDefaultIOReactorConfig(IOReactorConfig.custom()
+                                .setSoKeepAlive(true)
+                                .build())
+                        )
+        );
     }
 
     @Bean
