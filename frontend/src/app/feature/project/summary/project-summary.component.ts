4df56b164bf65f08a11f64f4b716b09e6be7340e
Olli Immonen <olli.immonen@gofore.com>
2024-09-17 10:58:33 +0300

    revert fix attemp

--- frontend/src/app/feature/project/summary/project-summary.component.ts
+++ frontend/src/app/feature/project/summary/project-summary.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {combineLatest, Observable, Subject} from 'rxjs';
 import {select, Store} from '@ngrx/store';
 
@@ -39,7 +39,7 @@ export class ProjectSummaryComponent implements OnInit, OnDestroy, AfterViewInit
   private destroy$ = new Subject<boolean>();
   @ViewChild(MapComponent) private map: MapComponent;
 
-  constructor(private store: Store<fromRoot.State>, private cdr: ChangeDetectorRef) {}
+  constructor(private store: Store<fromRoot.State>) {}
 
   ngOnInit(): void {
     this.districts$ = this.store.pipe(select(fromProject.getProjectDistricts));
@@ -54,8 +54,7 @@ export class ProjectSummaryComponent implements OnInit, OnDestroy, AfterViewInit
     // fixes the map not always being displayed
     setTimeout(() => {
       this.displayMap = true;
-      this.cdr.detectChanges();
-    }, 100);
+    });
   }
 
   ngAfterViewInit(): void {
