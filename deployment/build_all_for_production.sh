d79e729ce0b05bea0d6a4ba5621b87403e53893b
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2024-09-18 15:18:52 +0300

    Increase ansible verbosity to investigate ssh connection problem

--- deployment/build_all_for_production.sh
+++ deployment/build_all_for_production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" database_build.yml --vault-password-file ~/allu_keys/vault_secret
+ansible-playbook -vvvv -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" database_build.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" elasticsearch_build.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" backend_build.yml --vault-password-file ~/allu_keys/vault_secret
 ansible-playbook -i production.inventory --private-key=$HOME/allu_keys/allu_id_rsa --extra-vars="var_branch=$1" frontend_build.yml --vault-password-file ~/allu_keys/vault_secret
