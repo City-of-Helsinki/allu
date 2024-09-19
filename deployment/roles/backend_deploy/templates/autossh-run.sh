71640ada8174c1eeaf0d2e62093a2600d0ad9aae
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2024-09-19 18:56:52 +0300

    Fix configuration errors that crept in with commit 89aed164

--- deployment/roles/backend_deploy/templates/autossh-run.sh
+++ deployment/roles/backend_deploy/templates/autossh-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-{% if 'production' in group_names %}
+{% if 'staging' in group_names %}
 
 AUTOSSH_GATETIME=0
 usr/bin/autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o StrictHostKeyChecking=no -N -L 8022:{{ ssh_tunnel_destination_address }}:22 allu@{{ ssh_tunnel_server_address | default('NA') }} -i {{ allu_ssh_key_file | default('NA') }}
