1ed3d8221477498da3416891f1b262733a7a65a4
ilindhol <ira.lindholm@gofore.com>
2024-11-26 18:02:40 +0200

    ALLU-15: Hienosäätöä logiikkaan, jossa deaktivoidaan kontaktit ja alustavat testit

--- backend/service-core/src/test/java/fi/hel/allu/servicecore/service/CustomerServiceTest.java
+++ backend/service-core/src/test/java/fi/hel/allu/servicecore/service/CustomerServiceTest.java
@@ -1,5 +1,8 @@
 package fi.hel.allu.servicecore.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -7,6 +10,8 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import fi.hel.allu.servicecore.domain.ContactJson;
+import fi.hel.allu.servicecore.domain.CustomerWithContactsJson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -22,7 +27,7 @@ import fi.hel.allu.servicecore.domain.UserJson;
 import fi.hel.allu.servicecore.mapper.ChangeHistoryMapper;
 import fi.hel.allu.servicecore.mapper.CustomerMapper;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CustomerServiceTest extends MockServices {
   private static Validator validator;
@@ -33,6 +38,8 @@ public class CustomerServiceTest extends MockServices {
   private CodeSetService codeSetService;
   @Mock
   private ChangeHistoryMapper changeHistoryMapper;
+  @Mock
+  private ContactService contactService;
 
   protected CustomerService customerService;
 
@@ -86,5 +93,66 @@ public class CustomerServiceTest extends MockServices {
     Assert.assertNotNull(customerJson);
     Assert.assertNotNull(customerJson.getId());
     Assert.assertEquals(103, customerJson.getId().intValue());
+  }
+
+  //@Test
+  public void updateCustomerWithContacts_customerInactive_emptyContactList_setRelatedContactsInactive() {
+    List<ContactJson> mockContacts = Arrays.asList(
+      new ContactJson(1, 1, "Contact 1", "", "", "", "", "", true, false),
+      new ContactJson(2, 1, "Contact 2", "", "", "", "", "", true, false)
+    );
+    when(contactService.findByCustomer(1)).thenReturn(mockContacts);
+
+    CustomerJson inactiveCustomer = new CustomerJson();
+    inactiveCustomer.setId(1);
+    //inactiveCustomer.setType();
+    //inactiveCustomer.setName("Customer 1");
+    inactiveCustomer.setActive(false);
+    //inactiveCustomer.setCountry("Suomi");
+
+    CustomerWithContactsJson customerWithContactsJson = new CustomerWithContactsJson();
+    customerWithContactsJson.setCustomer(inactiveCustomer);
+    customerWithContactsJson.setContacts(new ArrayList<>());
+
+    CustomerWithContactsJson updatedCustomerWithContactsJson = customerService.updateCustomerWithContacts(inactiveCustomer.getId(), customerWithContactsJson);
+
+    Assert.assertNotNull(updatedCustomerWithContactsJson.getContacts());
+    Assert.assertEquals(2, updatedCustomerWithContactsJson.getContacts().size());
+    updatedCustomerWithContactsJson.getContacts().forEach(contact -> Assert.assertFalse(contact.isActive()));
+
+    verify(contactService, times(1)).findByCustomer(1);
+    verify(contactService, times(1)).updateContacts(any());
+    verify(contactService, never()).createContacts(any());
+  }
+
+  //@Test
+  public void updateCustomerWithContacts_customerInactive_withContactList_setRelatedContactsInactive() {
+    List<ContactJson> contacts = Arrays.asList(
+      new ContactJson(1, 1, "Contact 1", "", "", "", "", "", true, false),
+      new ContactJson(2, 1, "Contact 2", "", "", "", "", "", true, false)
+    );
+
+    CustomerJson inactiveCustomer = new CustomerJson();
+    inactiveCustomer.setId(1);
+    //inactiveCustomer.setType();
+    //inactiveCustomer.setName("Customer 1");
+    inactiveCustomer.setActive(false);
+    //inactiveCustomer.setCountry("Suomi");
+
+    CustomerWithContactsJson customerWithContactsJson = new CustomerWithContactsJson();
+    customerWithContactsJson.setCustomer(inactiveCustomer);
+    customerWithContactsJson.setContacts(contacts);
+
+    CustomerWithContactsJson updatedCustomerWithContactsJson = customerService.updateCustomerWithContacts(inactiveCustomer.getId(), customerWithContactsJson);
+
+    Assert.assertNotNull(updatedCustomerWithContactsJson.getContacts());
+    Assert.assertEquals(2, updatedCustomerWithContactsJson.getContacts().size());
+    updatedCustomerWithContactsJson.getContacts().forEach(contact -> Assert.assertFalse(contact.isActive()));
+
+    Assert.assertEquals(contacts.size(), updatedCustomerWithContactsJson.getContacts().size());
+
+    verify(contactService, never()).findByCustomer(anyInt());
+    verify(contactService, times(1)).updateContacts(any());
+    verify(contactService, never()).createContacts(any());
   }
 }
