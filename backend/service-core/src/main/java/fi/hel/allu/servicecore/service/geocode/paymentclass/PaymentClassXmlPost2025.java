7672dff1e3aab4a2d08eb6f892e75d92a6ccdfd5
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-01-16 15:45:52 +0200

    ALLU-112: Add the post-2025 alternatives to they payment class code
    PaymentClassXmlPost2025.java is just a placeholder for now

---
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/paymentclass/PaymentClassXmlPost2025.java
@@ -0,0 +1,22 @@
+package fi.hel.allu.servicecore.service.geocode.paymentclass;
+
+
+import fi.hel.allu.servicecore.service.geocode.featuremember.FeatureClassMember;
+import fi.hel.allu.servicecore.service.geocode.featuremember.FeatureClassMemberPost2022;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@XmlRootElement(name = "FeatureCollection", namespace = "http://www.opengis.net/wfs")
+public class PaymentClassXmlPost2025 implements PaymentClassXml {
+
+  @XmlElement(name = "featureMember", namespace = "http://www.opengis.net/gml")
+  public List<FeatureClassMemberPost2022> featureMember;
+
+  @Override
+  public List<FeatureClassMember> getFeatureMemeber() {
+    return featureMember.stream().map(FeatureClassMember.class::cast).collect(Collectors.toList());
+  }
+}
