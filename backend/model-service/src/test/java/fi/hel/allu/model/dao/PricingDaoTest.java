886a3dfba939136e83e48e6fc0040a4ed68f5050
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-02-06 08:28:36 +0200

    ALLU-109/110: New and fixed PricingDao unit tests
    Add unit tests for PricingDao.findValue() functions. Also fix the
    test for getPaymentClasses().

--- backend/model-service/src/test/java/fi/hel/allu/model/dao/PricingDaoTest.java
+++ backend/model-service/src/test/java/fi/hel/allu/model/dao/PricingDaoTest.java
@@ -5,8 +5,11 @@ import fi.hel.allu.common.domain.types.ApplicationKind;
 import fi.hel.allu.common.domain.types.ApplicationType;
 import fi.hel.allu.common.domain.types.SurfaceHardness;
 import fi.hel.allu.common.types.EventNature;
+import fi.hel.allu.common.util.TimeUtil;
 import fi.hel.allu.model.ModelApplication;
+import fi.hel.allu.model.domain.PricingKey;
 import fi.hel.allu.model.pricing.OutdoorPricingConfiguration;
+import fi.hel.allu.model.testUtils.SqlRunner;
 import fi.hel.allu.model.testUtils.TestCommon;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +20,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +29,6 @@ import static fi.hel.allu.QCityDistrict.cityDistrict;
 import static fi.hel.allu.QFixedLocation.fixedLocation;
 import static fi.hel.allu.QLocationArea.locationArea;
 import static fi.hel.allu.QOutdoorPricing.outdoorPricing;
-import static fi.hel.allu.QPricing.pricing;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -41,12 +45,16 @@ public class PricingDaoTest {
   @Autowired
   private SQLQueryFactory queryFactory;
 
+  @Autowired
+  private SqlRunner sqlRunner;
+
   private static final int TEST_ID = 1337;
   private static final long TEST_BASE_CHARGE = 6660000L;
 
   @Before
   public void setUp() throws Exception {
     testCommon.deleteAllData();
+    testCommon.deleteFrom("pricing");
     // Insert one fixed location and a pricing config for it:
     int areaId = queryFactory.insert(locationArea).set(locationArea.name, "Turbofolkstraße")
         .executeWithKey(locationArea.id);
@@ -94,33 +102,75 @@ public class PricingDaoTest {
 
   @Test
   public void testGetPaymentClassesForUnionResult() {
+    List<String> expectedResult = List.of("1", "2", "3", "4", "4a", "4b", "5");
+
+    insertPricings();
+
+    List<String> result = pricingDao.getPaymentClasses(ApplicationType.EXCAVATION_ANNOUNCEMENT, null);
+    assertEquals(expectedResult.size(), result.size());
+    for (int i = 0; i< result.size(); i++)
+      assertEquals(expectedResult.get(i), result.get(i));
+  }
+
+  @Test
+  public void testWithValidityDateWithoutPaymentClass() {
+    insertPricings();
+
+    ZonedDateTime februaryDate = ZonedDateTime.of(2025, 2, 14, 12, 0, 0, 0, TimeUtil.HelsinkiZoneId);
+    ZonedDateTime marchDate = ZonedDateTime.of(2025, 3, 10, 10, 27, 15, 0, TimeUtil.HelsinkiZoneId);
+
+    assertEquals(28000, pricingDao.findValue(ApplicationType.AREA_RENTAL, PricingKey.MAJOR_DISTURBANCE_HANDLING_FEE, februaryDate));
+    assertEquals(30000, pricingDao.findValue(ApplicationType.AREA_RENTAL, PricingKey.MAJOR_DISTURBANCE_HANDLING_FEE, marchDate));
+
+    assertEquals(10000, pricingDao.findValue(ApplicationType.AREA_RENTAL, PricingKey.MINOR_DISTURBANCE_HANDLING_FEE, februaryDate));
+    assertEquals(10000, pricingDao.findValue(ApplicationType.AREA_RENTAL, PricingKey.MINOR_DISTURBANCE_HANDLING_FEE, marchDate));
+  }
+
+  @Test
+  public void testWithValidityDateWithPaymentClass () {
+    insertPricings();
+
+    ZonedDateTime februaryDate = ZonedDateTime.of(2025, 2, 14, 12, 0, 0, 0, TimeUtil.HelsinkiZoneId);
+    ZonedDateTime marchDate = ZonedDateTime.of(2025, 3, 10, 10, 27, 15, 0, TimeUtil.HelsinkiZoneId);
+
+    assertEquals(7500, pricingDao.findValue(ApplicationType.EXCAVATION_ANNOUNCEMENT, PricingKey.MEDIUM_AREA_DAILY_FEE, "3", februaryDate));
+    assertEquals(9000, pricingDao.findValue(ApplicationType.EXCAVATION_ANNOUNCEMENT, PricingKey.FROM_121_TO_250M2, "3", marchDate));
+  }
+
+  private void insertPricings() {
+
+    // wasn't able to figure out how to insert Postgres dateranges with QueryDSL
 
     List<String> oldClasses = List.of("1", "2", "3", "4a", "4b");
     List<String> newClasses = List.of("1", "2", "3", "4", "5");
 
-    List<String> expectedResult = List.of("1", "2", "3", "4", "4a", "4b", "5");
+    String insertBody = "INSERT INTO allu.pricing (application_type, key, payment_class, value, validity) VALUES (";
+    String excavationBody = insertBody + "'" + ApplicationType.EXCAVATION_ANNOUNCEMENT.toString() + "', '";
+    String excavationOldBody = excavationBody + PricingKey.MEDIUM_AREA_DAILY_FEE.toString() + "', '";
+    String excavationNewBody = excavationBody + PricingKey.FROM_121_TO_250M2.toString() + "', '";
+    String areaRentalBody = insertBody + "'" + ApplicationType.AREA_RENTAL.toString() + "', '";
+    String areaRentalUnitPriceBody = areaRentalBody + PricingKey.UNIT_PRICE.toString() + "', '";
+    String oldValidity = "daterange(NULL, '2025-02-28', '(]')";
+    String newValidity = "daterange('2025-03-01', NULL, '[)')";
 
-    for (int i = 0; i < oldClasses.size(); i++) {
-      queryFactory.insert(pricing)
-        .set(pricing.applicationType, ApplicationType.AREA_RENTAL)
-        .set(pricing.key, "UNIT_PRICE")
-        .set(pricing.validity, "daterange(NULL, '2025-02-28', '(]'")
-        .set(pricing.paymentClass, oldClasses.get(i))
-        .set(pricing.value, (i+1) * 2500);
+    try {
+      for (int i = 0; i < oldClasses.size(); i++) {
+        String insertQuery = excavationOldBody + oldClasses.get(i) + "', " + (i + 1) * 2500 + ", " + oldValidity + ")";
+        System.out.println(insertQuery);
+        sqlRunner.runSql(insertQuery);
+      }
+      for (int i = 0; i < newClasses.size(); i++) {
+        String insertQuery = excavationNewBody + newClasses.get(i) + "', " + (i + 1) * 3000 + ", " + newValidity + ")";
+        System.out.println(insertQuery);
+        sqlRunner.runSql(insertQuery);
+      }
+
+      sqlRunner.runSql(areaRentalBody + PricingKey.MAJOR_DISTURBANCE_HANDLING_FEE.toString() + "', NULL, 28000, " + oldValidity + ')');
+      sqlRunner.runSql(areaRentalBody + PricingKey.MAJOR_DISTURBANCE_HANDLING_FEE.toString() + "', NULL, 30000, " + newValidity + ')');
+      sqlRunner.runSql(areaRentalBody + PricingKey.MINOR_DISTURBANCE_HANDLING_FEE.toString() + "', NULL, 10000, daterange(NULL, NULL,'()'))");
     }
-
-    for (int i = 0; i < newClasses.size(); i++) {
-      queryFactory.insert(pricing)
-        .set(pricing.applicationType, ApplicationType.AREA_RENTAL)
-        .set(pricing.key, "UNIT_PRICE")
-        .set(pricing.validity, "daterange('2025-03-01', NULL, [)")
-        .set(pricing.paymentClass, newClasses.get(i))
-        .set(pricing.value, (i+1) * 30000);
+    catch (SQLException e) {
+      System.out.println("Caught SQLException: " + e.getMessage());
     }
-
-    List<String> result = pricingDao.getPaymentClasses(ApplicationType.AREA_RENTAL, null);
-    assertEquals(expectedResult.size(), result.size());
-    for (int i = 0; i< result.size(); i++)
-      assertEquals(expectedResult.get(i), result.get(i));
   }
 }
