5d1d7b798697f6a1ddc582547478b113bdfbb371
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-02-12 13:53:32 +0200

    ALLU-125: Put anonymized application to the ANONYMIZED status

---
+++ backend/model-service/src/test/java/fi/hel/allu/model/service/AnonymizationTest.java
@@ -0,0 +1,50 @@
+package fi.hel.allu.model.service;
+
+import fi.hel.allu.common.domain.types.StatusType;
+import fi.hel.allu.model.ModelApplication;
+import fi.hel.allu.model.dao.ApplicationDao;
+import fi.hel.allu.model.dao.CustomerDao;
+import fi.hel.allu.model.dao.InvoiceRecipientDao;
+import fi.hel.allu.model.dao.UserDao;
+import fi.hel.allu.model.domain.Application;
+import fi.hel.allu.model.service.chargeBasis.ChargeBasisService;
+import fi.hel.allu.model.testUtils.TestCommon;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ModelApplication.class)
+@WebAppConfiguration
+@Transactional
+public class AnonymizationTest {
+  @Autowired
+  private ApplicationService applicationService;
+
+  @Autowired
+  private TestCommon testCommon;
+
+  @Test
+  public void shouldSetStatusToAnonymized() {
+    Application app1 = testCommon.dummyExcavationAnnouncementApplication("Application1", "Client1");
+    app1 = applicationService.insert(app1, 3);
+    Application app2 = testCommon.dummyExcavationAnnouncementApplication("Application2", "Client2");
+    app2 = applicationService.insert(app2, 3);
+
+    applicationService.anonymizeApplications(List.of(app1.getId(), app2.getId()));
+
+    Application anonApp1 = applicationService.findById(app1.getId());
+    assertEquals(StatusType.ANONYMIZED, anonApp1.getStatus());
+    Application anonApp2 = applicationService.findById(app2.getId());
+    assertEquals(StatusType.ANONYMIZED, anonApp2.getStatus());
+  }
+}
