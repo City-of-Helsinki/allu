6b15ec6803bba08a94de8bc45d2d7aee8fc12ccf
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-02-12 14:06:13 +0200

    ALLU-125: Remove all tags from anonymized applications

--- backend/model-service/src/test/java/fi/hel/allu/model/service/AnonymizationTest.java
+++ backend/model-service/src/test/java/fi/hel/allu/model/service/AnonymizationTest.java
@@ -1,5 +1,6 @@
 package fi.hel.allu.model.service;
 
+import fi.hel.allu.common.domain.types.ApplicationTagType;
 import fi.hel.allu.common.domain.types.StatusType;
 import fi.hel.allu.model.ModelApplication;
 import fi.hel.allu.model.dao.ApplicationDao;
@@ -7,6 +8,7 @@ import fi.hel.allu.model.dao.CustomerDao;
 import fi.hel.allu.model.dao.InvoiceRecipientDao;
 import fi.hel.allu.model.dao.UserDao;
 import fi.hel.allu.model.domain.Application;
+import fi.hel.allu.model.domain.ApplicationTag;
 import fi.hel.allu.model.service.chargeBasis.ChargeBasisService;
 import fi.hel.allu.model.testUtils.TestCommon;
 import java.util.List;
@@ -47,4 +49,25 @@ public class AnonymizationTest {
     Application anonApp2 = applicationService.findById(app2.getId());
     assertEquals(StatusType.ANONYMIZED, anonApp2.getStatus());
   }
+
+  @Test
+  public void shouldRemoveAllTags() {
+    Application app1 = testCommon.dummyExcavationAnnouncementApplication("Application1", "Client1");
+    app1.setApplicationTags(List.of(
+      new ApplicationTag(3, ApplicationTagType.DEPOSIT_PAID),
+      new ApplicationTag(3, ApplicationTagType.OTHER_CHANGES)
+    ));
+    app1 = applicationService.insert(app1, 3);
+    Application app2 = testCommon.dummyExcavationAnnouncementApplication("Application2", "Client2");
+    app2.setApplicationTags(List.of(
+      new ApplicationTag(3, ApplicationTagType.ADDITIONAL_INFORMATION_REQUESTED)
+    ));
+    app2 = applicationService.insert(app2, 3);
+
+    applicationService.anonymizeApplications(List.of(app1.getId(), app2.getId()));
+
+    Application anonApp1 = applicationService.findById(app1.getId());
+    assertEquals(0, anonApp1.getApplicationTags().size());
+    Application anonApp2 = applicationService.findById(app2.getId());
+    assertEquals(0, anonApp2.getApplicationTags().size());  }
 }
