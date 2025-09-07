e56636cebda1d77d16960341bd9490c4d1c54e85
Ira Lindholm <ira.lindholm@gofore.com>
2025-09-07 20:40:13 +0300

    ALLU-142: korjattu virheellinen endpoint ja virheenkäsittely hakemuksen anonymisoinnissa

--- frontend/src/app/feature/admin/prune-data/store/prune-data.effects.ts
+++ frontend/src/app/feature/admin/prune-data/store/prune-data.effects.ts
@@ -5,13 +5,14 @@ import { of } from 'rxjs';
 import { catchError, map, mergeMap, switchMap, withLatestFrom } from 'rxjs/operators';
 import * as PruneDataActions from './prune-data.actions';
 import { Store } from '@ngrx/store';
-import { PruneDataItem } from '../models/prude-data-item.model';
 import { selectCurrentTab } from './prune-data.selectors';
 
 @Injectable()
 export class PruneDataEffects {
-  private applicationEndPoint = '/api/applications/anonymizable';
-  private userEndpoint = '/api/user/anonymizable';
+  private applicationsAnonymizableEndpoint  = '/api/applications/anonymizable';
+  private applicationsAnonymizeEndpoint  = '/api/applications/anonymize';
+  private usersAnonymizableEndpoint  = '/api/user/anonymizable';
+  private usersAnonymizeEndpoint  = '/api/user/anonymize';
 
   constructor(
     private actions$: Actions,
@@ -24,31 +25,31 @@ export class PruneDataEffects {
       ofType(PruneDataActions.fetchAllData),
       switchMap(action => {
         const endpoint =
-        action.tab === 'user_data' ? this.userEndpoint : this.applicationEndPoint;
-    
+        action.tab === 'user_data' ? this.usersAnonymizableEndpoint : this.applicationsAnonymizableEndpoint;
+
         // pagination
         const params: any = {};
-        if (action.page !== undefined) params.page = action.page;
-        if (action.size !== undefined) params.size = action.size;
-    
+        if (action.page !== undefined) { params.page = action.page; }
+        if (action.size !== undefined) { params.size = action.size; }
+
          // sort
         if (action.sortField && action.sortDirection) {
           params.sort = `${action.sortField},${action.sortDirection}`;
         }
-    
+
         params.type = action.tab;
 
         return this.http.get<any>(endpoint, { params }).pipe(
           map(response => {
             if (response.content) {
-              return PruneDataActions.fetchAllDataSuccess({ 
-                  data: response.content, 
-                  totalItems: response.totalElements 
+              return PruneDataActions.fetchAllDataSuccess({
+                  data: response.content,
+                  totalItems: response.totalElements
                 });
               } else {
-                  return PruneDataActions.fetchAllDataSuccess({ 
-                    data: response, 
-                    totalItems: response.length 
+                  return PruneDataActions.fetchAllDataSuccess({
+                    data: response,
+                    totalItems: response.length
                   });
                 }
            }),
@@ -64,10 +65,10 @@ export class PruneDataEffects {
       withLatestFrom(this.store.select(selectCurrentTab)),
       mergeMap(([action, currentTab]) => {
         const endpoint =
-          currentTab === 'user_data' ? this.userEndpoint : this.applicationEndPoint;
+          currentTab === 'user_data' ? this.usersAnonymizeEndpoint : this.applicationsAnonymizeEndpoint;
         return this.http.patch<void>(`${endpoint}`, action.ids).pipe(
           map(() => PruneDataActions.deleteDataSuccess({ ids: action.ids })),
-          catchError(error => of(PruneDataActions.deleteDataSuccess({ ids: action.ids })))
+          catchError(error => of(PruneDataActions.deleteDataFailure({ ids: action.ids, error })))
         );
       })
     )
