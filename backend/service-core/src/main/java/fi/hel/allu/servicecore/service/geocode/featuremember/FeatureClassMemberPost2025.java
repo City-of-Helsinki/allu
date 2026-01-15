062cb17bbe750d29cf5386dc4010ae2689cb90ff
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2026-01-15 15:46:42 +0200

    Refactor: Unify payment level classes

--- backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/featuremember/FeatureClassMemberPost2025.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/featuremember/FeatureClassMemberPost2025.java
@@ -1,23 +1,22 @@
 package fi.hel.allu.servicecore.service.geocode.featuremember;
 
 import fi.hel.allu.servicecore.service.geocode.VariablesUtils;
-import fi.hel.allu.servicecore.service.geocode.paymentlevel.PaymentLevelClass2025;
-import fi.hel.allu.servicecore.service.geocode.paymentlevel.PaymentLevelInfo;
+import fi.hel.allu.servicecore.service.geocode.paymentlevel.PaymentLevelClass;
 
 import javax.xml.bind.annotation.XmlElement;
 
 public class FeatureClassMemberPost2025 {
   @XmlElement(name = "Katutoiden_maksuluokat_2025", namespace = VariablesUtils.HELSINKI_NAMESPACE)
-  public PaymentLevelClass2025 paymentLevelClass;
+  public PaymentLevelClass paymentLevelClass;
 
-  public PaymentLevelInfo getPaymentLevelInfo() {
-    return paymentLevelClass.getPaymentLevelInfo();
+  public PaymentLevelClass getPaymentLevelClass() {
+    return paymentLevelClass;
   }
 
   @Override
   public String toString() {
     return "FeatureClassMemberPost2025{" +
-      "paymentClass=" + paymentLevelClass +
+      "paymentLevelClass=" + paymentLevelClass +
       '}';
   }
 }
--- backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/paymentlevel/PaymentLevelZone.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/paymentlevel/PaymentLevelZone.java
@@ -4,21 +4,23 @@ import fi.hel.allu.servicecore.service.geocode.VariablesUtils;
 
 import javax.xml.bind.annotation.XmlElement;
 
-public class PaymentLevelZone extends AbstractPaymentLevel {
+public class PaymentLevelZone {
 
   @XmlElement(name = "maksuvyohyke", namespace = VariablesUtils.HELSINKI_NAMESPACE)
-  public String payment;
+  public String paymentLevelZone;
 
-  public String getPayment() {
-    return payment;
+  @XmlElement(name = "tietopalvelu_id", namespace = VariablesUtils.HELSINKI_NAMESPACE)
+  public int dataServiceId;
+
+  public String getPaymentLevelZone() {
+    return paymentLevelZone;
   }
-
 
   @Override
   public String toString() {
-    return "HelsinkiAlluMaksuvuohyke{" +
-      "id=" + id +
-      ", paymentZone ='" + payment + '\'' +
+    return "PaymentLevelZone{" +
+      "dataServiceId=" + dataServiceId +
+      ", paymentLevelZone='" + paymentLevelZone + '\'' +
       '}';
   }
 }
