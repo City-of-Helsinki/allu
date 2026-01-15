e87d8c77ec6ab8b87591c0119ae87e97767e101d
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2026-01-15 16:28:36 +0200

    refactor: Unify feature class member classes

--- backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/paymentclass/PaymentClassXmlPost2022.java
+++ backend/service-core/src/main/java/fi/hel/allu/servicecore/service/geocode/paymentclass/PaymentClassXmlPost2022.java
@@ -2,21 +2,19 @@ package fi.hel.allu.servicecore.service.geocode.paymentclass;
 
 
 import fi.hel.allu.servicecore.service.geocode.featuremember.FeatureClassMember;
-import fi.hel.allu.servicecore.service.geocode.featuremember.FeatureClassMemberPost2022;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @XmlRootElement(name = "FeatureCollection", namespace = "http://www.opengis.net/wfs")
 public class PaymentClassXmlPost2022 implements PaymentClassXml {
 
   @XmlElement(name = "featureMember", namespace = "http://www.opengis.net/gml")
-  public List<FeatureClassMemberPost2022> featureMember;
+  public List<FeatureClassMember> featureMember;
 
   @Override
   public List<FeatureClassMember> getFeatureMemeber() {
-    return featureMember.stream().map(FeatureClassMember.class::cast).collect(Collectors.toList());
+    return featureMember;
   }
 }
