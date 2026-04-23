53b7e3baf1c2085e6b99e29000d530808b97922c
Ira Lindholm <ira.lindholm@gofore.com>
2026-04-23 09:39:57 +0300

    ALLU-120: Updated prune-data effects, improved type safety, and adjusted endpoint formatting

--- frontend/src/app/feature/admin/prune-data/store/prune-data.effects.ts
+++ frontend/src/app/feature/admin/prune-data/store/prune-data.effects.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap, switchMap, withLatestFrom } from 'rxjs/operators';
 import * as PruneDataActions from './prune-data.actions';
-import { Store } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { selectCurrentTab } from './prune-data.selectors';
 
 @Injectable()
 export class PruneDataEffects {
   private applicationsAnonymizableEndpoint  = '/api/applications/anonymizable';
   private applicationsAnonymizeEndpoint  = '/api/applications/anonymize';
-  private customersEndpoint = '/api/customers';
+  private customersEndpoint = '/api/customers/';
   private customersDeletableEndpoint = '/api/customers/deletable';
 
   constructor(
@@ -61,15 +61,15 @@ export class PruneDataEffects {
     )
   );
 
-  deleteData$ = createEffect(() =>
+  deleteData$ = createEffect((): Observable<Action> =>
     this.actions$.pipe(
       ofType(PruneDataActions.deleteData),
       withLatestFrom(this.store.select(selectCurrentTab)),
       mergeMap(([action, currentTab]) => {
         const endpoint =
           currentTab === 'user_data' ? this.customersEndpoint : this.applicationsAnonymizeEndpoint;
-        const request$ = currentTab === 'user_data'
-          ? this.http.delete<void>(endpoint, { body: action.ids })
+        const request$: Observable<unknown> = currentTab === 'user_data'
+          ? this.http.delete(endpoint, { body: action.ids })
           : this.http.patch<void>(endpoint, action.ids);
         return request$.pipe(
           map(() => PruneDataActions.deleteDataSuccess({ ids: action.ids })),
